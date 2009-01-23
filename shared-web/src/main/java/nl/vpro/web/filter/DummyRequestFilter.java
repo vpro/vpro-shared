452df45d5d54f4247d54ff9bef172d4d08a4f28b
Nils Breunese <nils@breun.nl>
2009-01-23 14:13:22 +0000

    SplashPageFilter naar shared-web.

---
+++ shared-web/src/main/java/nl/vpro/web/filter/DummyRequestFilter.java
@@ -0,0 +1,20 @@
+package nl.vpro.mmbase.applications.portalettes;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * dummy implementation that passes all requests
+ * @author ernst
+ *
+ */
+public class DummyRequestFilter implements RequestFilter {
+
+    public boolean pass(HttpServletRequest request) {
+        return true;
+    }
+
+    public void setServletContext(ServletContext servletContext) {
+    }
+
+}
