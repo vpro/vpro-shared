454652aad5a7eeb061f1a49c592a31f6f69b8025
Rico Jansen <rico.jansen@gmail.com>
2011-02-01 15:03:22 +0000

    removed superflous

--- shared-util/src/test/java/nl/vpro/util/HTMLStripperTest.java
+++ shared-util/src/test/java/nl/vpro/util/HTMLStripperTest.java
@@ -17,23 +17,4 @@ public class HTMLStripperTest {
 		assertEquals(expected, output);
 	}
 
-	@Test
-	public void testStripperComplex() {
-		HTMLStripper stripper = new HTMLStripper();
-
-		String input = "<p>De 22q11DS poli<br />In het Universitair Medisch Centrum Utrecht bestaat een multidisciplinaire <a onclick=\"window.open(this.href);return false;\" title=\"22q11-Poli UMC Utrecht\" href=\"http://www.umcutrecht.nl/subsite/vcfs/\">polikliniek</a> voor kinderen met het 22q11DS/velocardiofaciaal syndroom.&#160;&nbsp;</p><p>In samenwerking met het Albert Schweitzer ziekenhuis in Dordrecht is halverwege 2004 een nieuw bestralingstoestel aangeschaft, de <a onclick=\"window.open(this.href);return false;\" title=\"Cyberknife in Erasmus MC Rotterdam\" href=\"http://www.erasmusmc.nl/radiotherapie/patientenzorg/behandelmethoden/uitwendigebestraling/128774/\">Cyberknife</a>, een robot met een vrij kleine versnellerbuis aan zijn robotarm. Sinds maart 2005 worden patiënten hiermee behandeld. Na Vicenza (Italië) is dit de tweede Cyberknife die in Europa in gebruik is genomen. </p><p>Onderwerpen die in de tv-serie Het Academisch Ziekenhuis aan bod komen zijn onder meer baanbrekende  behandelmethodes voor kanker en andere levensbedreigende aandoeningen, geavanceerde diagnostiek bij onder meer tumoren, healing environments, nieuwe operatietechnieken op het gebied van kijkoperaties en transplantatiechirurgie.</p>";
-
-		String expected = "De 22q11DS poli\n"
-				+ "In het Universitair Medisch Centrum Utrecht bestaat een multidisciplinaire  polikliniek voor kinderen met het 22q11DS/velocardiofaciaal syndroom.  \n\n"
-				+
-
-				"In samenwerking met het Albert Schweitzer ziekenhuis in Dordrecht is halverwege 2004 een nieuw bestralingstoestel aangeschaft, de  Cyberknife, een robot met een vrij kleine versnellerbuis aan zijn robotarm. Sinds maart 2005 worden patiënten hiermee behandeld. Na Vicenza (Italië) is dit de tweede Cyberknife die in Europa in gebruik is genomen.\n\n"
-				+
-
-				"Onderwerpen die in de tv-serie Het Academisch Ziekenhuis aan bod komen zijn onder meer baanbrekende behandelmethodes voor kanker en andere levensbedreigende aandoeningen, geavanceerde diagnostiek bij onder meer tumoren, healing environments, nieuwe operatietechnieken op het gebied van kijkoperaties en transplantatiechirurgie.";
-
-		String output = stripper.StripHTML(input);
-		assertEquals(expected, output);
-
-	}
 }
