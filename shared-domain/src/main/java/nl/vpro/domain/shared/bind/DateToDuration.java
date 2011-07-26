186a16ab0ed206e068f79fcaf8d48b60b79cad6c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2011-07-26 17:30:31 +0000

    Fixed Ignored and failing test-case

--- shared-domain/src/main/java/nl/vpro/domain/shared/bind/DateToDuration.java
+++ shared-domain/src/main/java/nl/vpro/domain/shared/bind/DateToDuration.java
@@ -7,19 +7,34 @@ package nl.vpro.domain.shared.bind;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.Duration;
-import java.util.Date;
+import java.util.*;
 
 public class DateToDuration extends XmlAdapter<Duration, Date> {
+
+    protected Calendar normalizeCalendar() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"), Locale.US);
+        return cal;
+    }
 
     @Override
     public Duration marshal(Date date) throws Exception {
         DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
-        return datatypeFactory.newDuration(date.getTime());
+        Duration dur;
+        if (date.getTime() < 30l * 24 * 60 * 60 * 1000) {
+            dur = datatypeFactory.newDurationDayTime(date.getTime());
+        } else {
+            dur = datatypeFactory.newDuration(date.getTime());
+
+        }
+
+        dur.normalizeWith(normalizeCalendar());
+        return dur;
     }
 
     @Override
     public Date unmarshal(Duration duration) throws Exception {
         Date date = new Date(0);
+        duration.normalizeWith(normalizeCalendar());
         duration.addTo(date);
         return date;
     }
