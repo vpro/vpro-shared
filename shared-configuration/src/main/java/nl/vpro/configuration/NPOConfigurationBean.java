ce3fe16f3f7487d21faf2304355a8510c4ba2653
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2011-09-01 16:00:17 +0000

    Made this thing implement Resource itself too, so you can use it in a PropertyPlaceholderConfigurer

--- shared-configuration/src/main/java/nl/vpro/configuration/NPOConfigurationBean.java
+++ shared-configuration/src/main/java/nl/vpro/configuration/NPOConfigurationBean.java
@@ -1,6 +1,8 @@
 package nl.vpro.configuration;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -9,43 +11,51 @@ import org.apache.log4j.Logger;
 import org.springframework.web.context.ServletContextAware;
 
 import javax.servlet.ServletContext;
+import org.springframework.core.io.*;
 
 /**
- * TODO move to
  * @author Michiel Meeuwissen
  * @version $Id$
  */
-public class NPOConfigurationBean extends AbstractConfiguration implements ServletContextAware {
+public class NPOConfigurationBean extends AbstractConfiguration implements ServletContextAware, Resource {
 
     private static final Logger LOG = Logger.getLogger(NPOConfigurationBean.class);
 
     private final String name;
     private final Properties properties = new Properties();
-
+    private ServletContext sx;
+    private File file = null;
     public NPOConfigurationBean(String name) {
         this.name = name;
     }
 
 
     @Override
-    public void setServletContext(ServletContext servletContext) {
-        File dir = new File(servletContext.getRealPath("/"));
-        if (dir.exists()) {
+    public File getFile() {
+        if (file == null) {
+            File dir = new File(sx.getRealPath("/"));
+            if (dir == null) dir = File.listRoots()[0];
             File parent = dir.getParentFile();
+            if (parent == null) parent = dir;
             File configDir = new File(parent, "config");
-            File configFile = new File(configDir, name);
-            if (configFile.exists()) {
-                try {
-                    properties.load(new FileInputStream(configFile));
-                    LOG.info("Loaded from " + configFile + " " + properties.keySet());
-                } catch (IOException e) {
-                    LOG.error(e.getMessage(), e);
-                }
-            } else {
-                LOG.info("File " + configFile + " not found");
+            file = new File(configDir, name);
+        }
+        return file;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        sx = servletContext;
+        getFile();
+        if (file.exists()) {
+            try {
+                properties.load(new FileInputStream(file));
+                LOG.info("Loaded from " + file + " " + properties.keySet());
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
             }
         } else {
-            LOG.info("No " + dir);
+            LOG.info("File " + file+ " not found");
         }
     }
 
@@ -74,4 +84,55 @@ public class NPOConfigurationBean extends AbstractConfiguration implements Servl
         return properties.isEmpty();
     }
 
+    @Override
+    public boolean exists() {
+        return ! properties.isEmpty();
+    }
+
+    @Override
+    public boolean isReadable() {
+        return !properties.isEmpty();
+    }
+
+    @Override
+    public boolean isOpen() {
+        return false;
+    }
+
+    @Override
+    public URL getURL() throws IOException {
+        return getFile().toURI().toURL();
+    }
+
+    @Override
+    public URI getURI() throws IOException {
+        return getFile().toURI();
+    }
+
+
+
+    @Override
+    public long lastModified() throws IOException {
+        return getFile().lastModified();
+    }
+
+    @Override
+    public Resource createRelative(String relativePath) throws IOException {
+        return new FileSystemResource(new File(getFile(), relativePath));
+    }
+
+    @Override
+    public String getFilename() {
+        return getFile().getName();
+    }
+
+    @Override
+    public String getDescription() {
+        return getFilename();
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new FileInputStream(getFile());
+    }
 }
