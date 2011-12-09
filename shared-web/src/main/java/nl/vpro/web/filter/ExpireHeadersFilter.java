976b549ff05176dfcae58aba3d29a59f7c9ab6c9
Rico Jansen <rico.jansen@gmail.com>
2011-12-09 10:25:24 +0000

    Expires header filter toegevoegd.

--- shared-web/src/main/java/nl/vpro/web/filter/ExpireHeadersFilter.java
+++ shared-web/src/main/java/nl/vpro/web/filter/ExpireHeadersFilter.java
@@ -25,24 +25,41 @@ import org.apache.log4j.Logger;
  * @author Rob Vermeulen (VPRO)
  */
 public class ExpireHeadersFilter implements Filter {
+    private static final int DEFAULTTTL=300;
+
     private static final Logger log = Logger.getLogger(ExpireHeadersFilter.class);
+
+    private FilterConfig filterConfig = null;
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
 
         // Set the expire header
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MINUTE, 5);
+        cal.add(Calendar.SECOND, getTTL());
         response.setDateHeader("Expires" , cal.getTimeInMillis());
 
         // Set the max-age header.
-        response.setHeader("Cache-Control", "max-age=300");
+        response.setHeader("Cache-Control", "max-age="+getTTL());
 
-        // log.error("Setting expire headers ");
         chain.doFilter(req, res);
     }
 
+    private int getTTL() {
+        int ttl=DEFAULTTTL;
+        String value=filterConfig.getInitParameter("ttl");
+        if (value!=null && value.length()>0) {
+            try {
+                ttl=Integer.valueOf(value);
+            } catch (NumberFormatException n) {
+                ttl=DEFAULTTTL;
+            }
+        }
+        return ttl;
+    }
+
     public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
         log.info("Starting ExpireHeadersFilter");
     }
 
