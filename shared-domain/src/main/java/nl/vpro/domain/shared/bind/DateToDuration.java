b19a1c315e2377fccb5ae9893e5f4ce6701a079b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2012-01-19 17:42:23 +0000

    A straigh-forward test-case for DateToDuration

--- shared-domain/src/main/java/nl/vpro/domain/shared/bind/DateToDuration.java
+++ shared-domain/src/main/java/nl/vpro/domain/shared/bind/DateToDuration.java
@@ -7,6 +7,7 @@ package nl.vpro.domain.shared.bind;
 import java.util.Date;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.Duration;
 
@@ -15,16 +16,22 @@ public class DateToDuration extends XmlAdapter<Duration, Date> {
 
     @Override
     public Duration marshal(Date date) throws Exception {
-        DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
         long time = date.getTime();
-        Duration dur;
         if (time < 30l * 24 * 60 * 60 * 1000) {
-            dur = datatypeFactory.newDurationDayTime(time);
+            return marshalDayTime(time);
         } else {
-            dur = datatypeFactory.newDuration(time);
+            return marshal(time);
         }
+    }
 
-        return dur;
+    protected Duration marshalDayTime(long time) throws DatatypeConfigurationException {
+        DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
+        return datatypeFactory.newDurationDayTime(time);
+    }
+
+    protected Duration marshal(long time) throws DatatypeConfigurationException {
+        DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
+        return datatypeFactory.newDuration(time);
     }
 
     @Override
--- shared-domain/src/test/java/nl/vpro/domain/shared/bind/DateToDurationTest.java
+++ shared-domain/src/test/java/nl/vpro/domain/shared/bind/DateToDurationTest.java
@@ -12,11 +12,29 @@ import static junit.framework.Assert.assertEquals;
 public class DateToDurationTest {
 
     @Test
+    public void testDayTime() throws Exception {
+        DateToDuration dateToDuration = new DateToDuration();
+        assertEquals("P0DT0H0M0.000S", dateToDuration.marshalDayTime(0l).toString());
+        assertEquals("P0DT0H1M40.000S", dateToDuration.marshalDayTime(100000l).toString());
+        assertEquals("P30DT0H0M0.000S", dateToDuration.marshalDayTime(30l * 24 * 60 * 60 * 1000).toString());
+    }
+
+    @Test
+    public void testTime() throws Exception {
+        DateToDuration dateToDuration = new DateToDuration();
+        assertEquals("P0Y0M0DT0H0M0.000S", dateToDuration.marshal(0l).toString());
+        assertEquals("P0Y0M0DT0H1M40.000S", dateToDuration.marshal(100000l).toString());
+        assertEquals("P0Y0M30DT0H0M0.000S", dateToDuration.marshal(30l * 24 * 60 * 60 * 1000).toString());
+        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(60l * 24 * 60 * 60 * 1000).toString());
+    }
+
+    @Test
     public void test() throws Exception {
         DateToDuration dateToDuration = new DateToDuration();
         assertEquals("P0DT0H0M0.000S", dateToDuration.marshal(new Date(0)).toString());
         assertEquals("P0DT0H1M40.000S", dateToDuration.marshal(new Date(100000l)).toString());
         assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(new Date(2l * 30 * 24 * 60 * 60 * 1000)).toString());
+        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(new Date(60l * 24 * 60 * 60 * 1000)).toString());
     }
 
 }
