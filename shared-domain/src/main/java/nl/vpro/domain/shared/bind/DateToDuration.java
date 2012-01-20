953f963c856495ae179221b9ba28b22b16c026aa
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2012-01-20 12:41:39 +0000

    Hack hack.

--- shared-domain/src/main/java/nl/vpro/domain/shared/bind/DateToDuration.java
+++ shared-domain/src/main/java/nl/vpro/domain/shared/bind/DateToDuration.java
@@ -4,10 +4,13 @@
  */
 package nl.vpro.domain.shared.bind;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeConstants;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.Duration;
 
@@ -26,7 +29,7 @@ public class DateToDuration extends XmlAdapter<Duration, Date> {
 
     protected Duration marshalDayTime(long time) throws DatatypeConfigurationException {
         DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
-        return datatypeFactory.newDurationDayTime(time);
+        return new TimeDuration(datatypeFactory.newDurationDayTime(time));
     }
 
     protected Duration marshal(long time) throws DatatypeConfigurationException {
@@ -39,5 +42,72 @@ public class DateToDuration extends XmlAdapter<Duration, Date> {
         Date date = new Date(0);
         duration.addTo(date);
         return date;
+    }
+
+
+    /**
+     * This should be superflous, but test-cases are failing on jenkins.
+     * implemented just to correct toString
+     */
+    private class TimeDuration extends Duration {
+        private final Duration dur;
+        TimeDuration(Duration dur) {
+            this.dur = dur;
+        }
+        @Override
+        public int getSign() {
+            return dur.getSign();
+        }
+
+        @Override
+        public Number getField(DatatypeConstants.Field field) {
+            return dur.getField(field);
+        }
+
+        @Override
+        public boolean isSet(DatatypeConstants.Field field) {
+            return dur.isSet(field);
+        }
+
+        @Override
+        public Duration add(Duration duration) {
+            dur.add(duration);
+            return this;
+        }
+
+        @Override
+        public void addTo(Calendar calendar) {
+            dur.addTo(calendar);
+        }
+
+        @Override
+        public Duration multiply(BigDecimal bigDecimal) {
+            return dur.multiply(bigDecimal);
+        }
+
+        @Override
+        public Duration negate() {
+            return dur.negate();
+        }
+
+        @Override
+        public Duration normalizeWith(Calendar calendar) {
+            dur.normalizeWith(calendar);
+            return this;
+        }
+
+        @Override
+        public int compare(Duration duration) {
+            return dur.compare(duration);
+        }
+
+        @Override
+        public int hashCode() {
+            return dur.hashCode();
+        }
+        @Override
+        public String toString() {
+            return "P" + dur.getDays() + "DT" + dur.getHours() + "H" + dur.getMinutes() + "M" + dur.getSeconds() + ".000" + "S";
+        }
     }
 }
