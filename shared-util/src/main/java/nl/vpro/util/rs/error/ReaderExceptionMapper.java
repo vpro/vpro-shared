fca8db03788be5c9fef3f94da1f5a4f0c389e432
Ernst Bunders <e.bunders@vpro.nl>
2012-05-07 10:31:45 +0000

    Moved to fix circular dependency

---
+++ shared-util/src/main/java/nl/vpro/util/rs/error/ReaderExceptionMapper.java
@@ -0,0 +1,22 @@
+package nl.vpro.util.rs.error;
+
+import org.jboss.resteasy.spi.ReaderException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Date: 2-5-12
+ * Time: 15:16
+ *
+ * @author Ernst Bunders
+ */
+@Provider
+public class ReaderExceptionMapper extends ApiErrorMapper implements ExceptionMapper<ReaderException> {
+
+    @Override
+    public Response toResponse(ReaderException exception) {
+        return createResponse(DataError.INVALIDDATA, 500, "Something went wrong unmarshalling the data");
+    }
+}
---
+++ shared-util/src/main/java/nl/vpro/util/rs/transfer/ErrorResponse.java
@@ -0,0 +1,51 @@
+package nl.vpro.util.rs.transfer;
+
+import nl.vpro.util.rs.error.DataError;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Date: 23-4-12
+ * Time: 10:45
+ *
+ * @author Ernst Bunders
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class ErrorResponse {
+
+    private Integer status;
+    private String message;
+    private DataError dataError;
+
+    public ErrorResponse() { }
+
+    public ErrorResponse(String message, Integer status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public DataError getDataError() {
+        return dataError;
+    }
+
+    public void setDataError(DataError dataError) {
+        this.dataError = dataError;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
