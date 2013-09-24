13804a0c6dad27edb039425fead7c9b37cb669d9
soarten <soarten@vpro.nl>
2013-09-24 14:03:28 +0000

    MSE-2093 Test case

---
+++ shared-util/src/test/java/nl/vpro/util/DateUtilsTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.util;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.1
+ */
+public class DateUtilsTest {
+
+    @Test
+    public void testLowest() throws Exception {
+        assertThat(DateUtils.lowest(null,null)).isNull();
+        assertThat(DateUtils.lowest(new Date(0),null)).isEqualTo(new Date(0));
+        assertThat(DateUtils.lowest(new Date(0),new Date(1))).isEqualTo(new Date(0));
+        assertThat(DateUtils.lowest(null,new Date(0))).isEqualTo(new Date(0));
+        assertThat(DateUtils.lowest(new Date(1),new Date(0))).isEqualTo(new Date(0));
+    }
+
+    @Test
+    public void testHighest() throws Exception {
+        assertThat(DateUtils.highest(null,null)).isNull();
+        assertThat(DateUtils.highest(new Date(0),null)).isEqualTo(new Date(0));
+        assertThat(DateUtils.highest(new Date(0),new Date(1))).isEqualTo(new Date(1));
+        assertThat(DateUtils.highest(null,new Date(0))).isEqualTo(new Date(0));
+        assertThat(DateUtils.highest(new Date(1),new Date(0))).isEqualTo(new Date(1));
+    }
+}
