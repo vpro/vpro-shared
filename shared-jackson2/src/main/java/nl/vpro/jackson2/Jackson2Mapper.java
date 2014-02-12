3bada8450bd42ca00ee4d1c811c21b4acf781989
soarten <soarten@vpro.nl>
2014-02-12 09:48:19 +0000

    NPA-1 Separate module with Jackson2Mapper

---
+++ shared-jackson2/src/main/java/nl/vpro/jackson2/Jackson2Mapper.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2012 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.jackson2;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+
+/**
+ * @author Rico
+ * @author Michiel Meeuwissen
+ */
+public class Jackson2Mapper extends ObjectMapper {
+    public static Jackson2Mapper INSTANCE = new Jackson2Mapper();
+
+    public static Jackson2Mapper getInstance() {
+        return INSTANCE;
+    }
+
+    private Jackson2Mapper() {
+
+        AnnotationIntrospector introspector = new AnnotationIntrospectorPair(
+            new JacksonAnnotationIntrospector(),
+            new JaxbAnnotationIntrospector());
+
+        setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        setAnnotationIntrospector(introspector);
+
+        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+}
