198e6a07a84700f2c2ea886ec17ff8f7e2fef34f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-02-28 16:14:06 +0000

    test were failing with java 8.

--- shared-test/src/main/java/nl/vpro/test/util/jaxb/JAXBTestUtil.java
+++ shared-test/src/main/java/nl/vpro/test/util/jaxb/JAXBTestUtil.java
@@ -4,10 +4,15 @@
  */
 package nl.vpro.test.util.jaxb;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXB;
+
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.xml.sax.SAXException;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -32,10 +37,21 @@ public class JAXBTestUtil {
         return (T)JAXB.unmarshal(new StringReader(marshal(input)), input.getClass());
     }
 
+    /**
+     * @deprecated  unfeasible for different java versions. (tests which used this where often failing with java 8). Use e.g {#link roundTripAndSimilar}
+     */
     @SuppressWarnings("unchecked")
     public static  <T> T roundTrip(T input, String contains) {
         String xml = marshal(input);
         assertThat(xml).contains(contains);
         return (T)JAXB.unmarshal(new StringReader(xml), input.getClass());
+    }
+
+
+    public static <T> T roundTripAndSimilar(T input, String expected) throws IOException, SAXException {
+        String xml = marshal(input);
+        Diff diff = XMLUnit.compareXML(expected, xml);
+        assertThat(diff.identical()).isTrue();
+        return (T) JAXB.unmarshal(new StringReader(xml), input.getClass());
     }
 }
