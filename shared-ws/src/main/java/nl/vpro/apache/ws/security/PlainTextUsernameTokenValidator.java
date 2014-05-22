e7ba8284fca4ff89a578195b7ff38f72c32228f4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-05-22 14:30:34 +0000

    better error handling.

--- shared-ws/src/main/java/nl/vpro/apache/ws/security/PlainTextUsernameTokenValidator.java
+++ shared-ws/src/main/java/nl/vpro/apache/ws/security/PlainTextUsernameTokenValidator.java
@@ -10,12 +10,18 @@ import org.apache.ws.security.WSSecurityException;
 import org.apache.ws.security.handler.RequestData;
 import org.apache.ws.security.message.token.UsernameToken;
 import org.apache.ws.security.validate.UsernameTokenValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class PlainTextUsernameTokenValidator extends UsernameTokenValidator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PlainTextUsernameTokenValidator.class);
+
 
     @Resource(name = "authenticationManager")
     AuthenticationManager manager;
@@ -26,8 +32,15 @@ public class PlainTextUsernameTokenValidator extends UsernameTokenValidator {
         final String password = usernameToken.getPassword();
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(name, password);
-        authentication = manager.authenticate(authentication);
+        try {
+            authentication = manager.authenticate(authentication);
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (AuthenticationException ae) {
+            throw new WSSecurityException(name + ":" + ae.getMessage(), ae);
+        } catch (RuntimeException t) {
+            LOG.error(t.getMessage(), t);
+            throw t;
+        }
     }
 }
