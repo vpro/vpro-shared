cbe4fe955584a5df1dd74b61339d76e031470e58
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-09-16 09:23:47 +0000

    try to automaticly determing location of config-file

--- shared-configuration/src/main/java/nl/vpro/configuration/PropertyConfigurationFactoryBean.java
+++ shared-configuration/src/main/java/nl/vpro/configuration/PropertyConfigurationFactoryBean.java
@@ -14,20 +14,22 @@ import org.apache.commons.configuration.ConfigurationUtils;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.commons.configuration.reloading.ReloadingStrategy;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 
 /**
  * Factorybean to create a configuration of properties when the resource exists.
- * 
+ *
  * @author arne
  * @version $Id$
  */
 public class PropertyConfigurationFactoryBean implements InitializingBean, FactoryBean {
 
-    private static final Logger log = Logger.getLogger(PropertyConfigurationFactoryBean.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PropertyConfigurationFactoryBean.class);
+
 
     private Configuration configuration;
 
@@ -52,13 +54,13 @@ public class PropertyConfigurationFactoryBean implements InitializingBean, Facto
     /** {@inheritDoc} */
     public void afterPropertiesSet() throws Exception {
         if (location.exists()) {
-            log.info(String.format("resource %s does exists.", location.getDescription()));
+            LOG.info(String.format("resource %s does exists.", location.getDescription()));
             // If it is a file, use reloading strategy.
             PropertiesConfiguration propconfig = new PropertiesConfiguration(location.getURL());
             if (reload) {
                 File f = ConfigurationUtils.fileFromURL(location.getURL());
                 if (f != null && f.exists()) {
-                    log.info(String.format("resource %s will be configured with file reloading strategy.", location
+                    LOG.info(String.format("resource %s will be configured with file reloading strategy.", location
                             .getDescription()));
                     ReloadingStrategy strategy = new FileChangedReloadingStrategy();
                     propconfig.setReloadingStrategy(strategy);
@@ -66,7 +68,7 @@ public class PropertyConfigurationFactoryBean implements InitializingBean, Facto
             }
             configuration = propconfig;
         } else {
-            log.info(String.format("resource %s does not exist, using base config.", location.getDescription()));
+            LOG.info(String.format("resource %s does not exist, using base config.", location.getDescription()));
             this.configuration = new BaseConfiguration();
         }
     }
