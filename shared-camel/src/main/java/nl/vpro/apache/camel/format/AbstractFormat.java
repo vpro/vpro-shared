6760abfd5fb5f02e597cb415567c53689b14aa30
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-09-18 15:51:13 +0000

    API-159

---
+++ shared-camel/src/main/java/nl/vpro/apache/camel/format/AbstractFormat.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2011 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.apache.camel.format;
+
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import org.apache.camel.spi.DataFormat;
+
+import nl.vpro.jackson2.Jackson2Mapper;
+
+public abstract class AbstractFormat {
+
+    protected static DataFormat getJaxb(String contextPath) {
+        return getJaxb(contextPath, false);
+    }
+
+    protected static DataFormat getJaxb(String contextPath, boolean prettyPrint) {
+        JaxbDataFormat jaxb = new JaxbDataFormat(contextPath);
+        jaxb.setPrettyPrint(prettyPrint);
+        return jaxb;
+    }
+
+    protected static DataFormat getJson(Class clazz) {
+        final JacksonDataFormat mediaMapper = new JacksonDataFormat(Jackson2Mapper.getInstance(), clazz);
+        return mediaMapper;
+    }
+}
