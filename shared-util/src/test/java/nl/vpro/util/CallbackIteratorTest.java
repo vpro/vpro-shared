d2df386e4a18342ad24a10db17fbc3356f12d440
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-01-20 16:31:44 +0000

    callback iterator

---
+++ shared-util/src/test/java/nl/vpro/util/CallbackIteratorTest.java
@@ -0,0 +1,29 @@
+package nl.vpro.util;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+public class CallbackIteratorTest {
+
+
+    @Test
+    public void test() {
+        Runnable runnable = mock(Runnable.class);
+        CallbackIterator<String> i = new CallbackIterator<>(Arrays.asList("A", "B").iterator(), runnable);
+        verifyZeroInteractions(runnable);
+        i.next();
+        verifyZeroInteractions(runnable);
+        i.next();
+        verify(runnable).run();
+        assertThat(i.hasNext()).isFalse();
+    }
+
+
+
+}
