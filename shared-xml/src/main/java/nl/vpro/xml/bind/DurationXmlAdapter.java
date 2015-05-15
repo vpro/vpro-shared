30649e1bddd827175f206671ecec343f8e71eaea
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-05-15 12:04:44 +0000

    moved xml related stuff to its own module

---
+++ shared-xml/src/main/java/nl/vpro/xml/bind/DurationXmlAdapter.java
@@ -0,0 +1,22 @@
+package nl.vpro.xml.bind;
+
+import java.time.Duration;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.21
+ */
+public class DurationXmlAdapter extends XmlAdapter<String, Duration> {
+
+    @Override
+    public Duration unmarshal(String stringValue) {
+        return stringValue != null ? Duration.parse(stringValue) : null;
+    }
+
+    @Override
+    public String marshal(Duration value) {
+        return value != null ? value.toString() : null;
+    }
+}
