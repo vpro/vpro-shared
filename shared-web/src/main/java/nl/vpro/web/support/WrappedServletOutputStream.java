8c918d2ef46555c902d867c13992f184371259f4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-05-18 19:01:32 +0000

    Made api.basePath setting superflous.

--- shared-web/src/main/java/nl/vpro/web/support/WrappedServletOutputStream.java
+++ shared-web/src/main/java/nl/vpro/web/support/WrappedServletOutputStream.java
@@ -4,30 +4,34 @@
  */
 package nl.vpro.web.support;
 
-import javax.servlet.ServletOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+
+import javax.servlet.ServletOutputStream;
 
 /**
  * To wrap the output stream we need an implementation of a ServletOutputStream
  */
 public class WrappedServletOutputStream extends ServletOutputStream {
-    
+
     private DataOutputStream stream;
 
     public WrappedServletOutputStream(OutputStream stream) {
         this.stream = new DataOutputStream(stream);
     }
 
+    @Override
     public void write(int b) throws IOException {
         stream.write(b);
     }
-    
+
+    @Override
     public void write(byte[] b) throws IOException {
         stream.write(b);
     }
 
+    @Override
     public void write(byte[] b, int off, int len) throws IOException {
         stream.write(b, off, len);
     }
