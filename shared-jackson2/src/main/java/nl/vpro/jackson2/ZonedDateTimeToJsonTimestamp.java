9616dc0f6fd81da58fa23af4cb4fd57c0a9a71ca
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-05-19 17:08:32 +0000

    VGA-6

---
+++ shared-jackson2/src/main/java/nl/vpro/jackson2/ZonedDateTimeToJsonTimestamp.java
@@ -0,0 +1,45 @@
+package nl.vpro.jackson2;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+
+import static nl.vpro.jackson2.DateModule.ZONE;
+
+
+public class ZonedDateTimeToJsonTimestamp {
+
+    public static class Serializer extends JsonSerializer<ZonedDateTime> {
+
+
+        public static Serializer INSTANCE = new Serializer();
+
+        @Override
+        public void serialize(ZonedDateTime value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            if (value == null) {
+                jgen.writeNull();
+            } else {
+                jgen.writeNumber(value.toInstant().toEpochMilli());
+            }
+        }
+    }
+
+
+    public static class Deserializer extends JsonDeserializer<ZonedDateTime> {
+
+        public static Deserializer INSTANCE = new Deserializer();
+        @Override
+        public ZonedDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            return Instant.ofEpochMilli(jp.getLongValue()).atZone(ZONE);
+        }
+    }
+}
