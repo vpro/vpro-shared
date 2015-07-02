6ffe397d81e491a322adc9caacd35c92a3085844
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-07-02 09:34:14 +0000

    MSE-2775

--- shared-jackson2/src/main/java/nl/vpro/jackson2/DateToJsonTimestamp.java
+++ shared-jackson2/src/main/java/nl/vpro/jackson2/DateToJsonTimestamp.java
@@ -5,11 +5,10 @@ import java.util.Date;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 
 public class DateToJsonTimestamp {
@@ -30,12 +29,17 @@ public class DateToJsonTimestamp {
     }
 
 
-    public static class Deserializer extends JsonDeserializer<Date> {
+    public static class Deserializer extends StdDeserializer<Date> {
 
-        public static Deserializer INSTANCE = new Deserializer();
+        public static Deserializer INSTANCE = new Deserializer(Date.class);
+
+        protected Deserializer(Class<?> vc) {
+            super(vc);
+        }
+
         @Override
-        public Date deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            return new Date(jp.getLongValue());
+        public Date deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+            return _parseDate(jp, ctxt);
         }
     }
 }
