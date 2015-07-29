80523803f25bad4e444196a579b56baeff856067
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-07-29 11:41:14 +0000

    NPA-223 made interface a bit clearer, and javadocced.

--- shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/IndexHelper.java
+++ shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/IndexHelper.java
@@ -44,7 +44,7 @@ public class IndexHelper {
     }
 
     private Client client() {
-        return client.buildClient(IndexHelper.class.getName() + "." + indexName);
+        return client.client(IndexHelper.class.getName() + "." + indexName);
     }
 
     public  void createIndex() throws ExecutionException, InterruptedException, IOException {
