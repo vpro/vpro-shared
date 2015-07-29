72ce91b6da75aa47b0d2ce5d077c63bbaa4d29db
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-07-29 11:38:24 +0000

    NPA-223 made interface a bit clearer, and javadocced.

--- shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/ESClientFactoryImpl.java
+++ shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/ESClientFactoryImpl.java
@@ -3,6 +3,8 @@ package nl.vpro.elasticsearch;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.PreDestroy;
+
 import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
@@ -45,26 +47,36 @@ public class ESClientFactoryImpl implements  ESClientFactory {
      */
     private Integer pingIntervalInSeconds;
 
+    private Client client = null;
 
     Client buildClient() {
-        return buildClient("test");
+        return client("test");
     }
 
     @Override
-    public Client buildClient(String logName){
+    public Client client(String logName){
+        if (client == null) {
+            client = buildClient(logName);
+        }
+        return client;
+
+
+    }
+
+    private Client buildClient(String logName) {
         ImmutableSettings.Builder builder = ImmutableSettings.settingsBuilder()
             .put("client.transport.ignore_cluster_name", ignoreClusterName)
             .put("client.transport.sniff", sniffCluster)
             .put("discovery.zen.ping.multicast.enabled", "false");
 
-        if(StringUtils.isNotBlank(clusterName)) {
+        if (StringUtils.isNotBlank(clusterName)) {
             builder.put("cluster.name", clusterName);
         }
-        if(pingTimeoutInSeconds != null) {
+        if (pingTimeoutInSeconds != null) {
             builder.put("client.transport.ping_timeout", pingTimeoutInSeconds + "s");
         }
-        if(pingIntervalInSeconds!= null) {
-            builder.put("client.transport.nodes_sampler_interval", pingIntervalInSeconds +  "s");
+        if (pingIntervalInSeconds != null) {
+            builder.put("client.transport.nodes_sampler_interval", pingIntervalInSeconds + "s");
         }
 
         TransportClient transportClient = new TransportClient(builder.build());
@@ -74,41 +86,55 @@ public class ESClientFactoryImpl implements  ESClientFactory {
         LOG.info("Build es client {} {} ({})", logName, transportAddresses, clusterName);
 
         return transportClient;
-
     }
 
 
 
-
     public void setTransportAddresses(List<UrlProvider> transportAddresses) {
+        reset();
         this.transportAddresses = transportAddresses;
     }
 
 
     public void setClusterName(String clusterName) {
+        reset();
         this.clusterName = clusterName;
     }
 
 
 
     public void setSniffCluster(boolean sniffCluster) {
+        reset();
         this.sniffCluster = sniffCluster;
     }
 
 
     public void setIgnoreClusterName(boolean ignoreClusterName) {
+        reset();
         this.ignoreClusterName = ignoreClusterName;
     }
 
 
     public void setPingTimeoutInSeconds(Integer pingTimeoutInSeconds) {
+        reset();
         this.pingTimeoutInSeconds = pingTimeoutInSeconds;
     }
 
 
 
     public void setPingIntervalInSeconds(Integer pingIntervalInSeconds) {
+        reset();
         this.pingIntervalInSeconds = pingIntervalInSeconds;
     }
 
+    private void reset() {
+        shutdown();
+        client = null;
+    }
+    @PreDestroy
+    public void shutdown() {
+        if (client != null) {
+            client.close();
+        }
+    }
 }
