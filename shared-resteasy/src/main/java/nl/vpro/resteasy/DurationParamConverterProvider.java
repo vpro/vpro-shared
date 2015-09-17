16393fc228e2dd875f4629d5f0a9ba43cbd000e4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-09-17 12:19:17 +0000

    MTO-21

---
+++ shared-resteasy/src/main/java/nl/vpro/resteasy/DurationParamConverterProvider.java
@@ -0,0 +1,26 @@
+package nl.vpro.resteasy;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.time.Duration;
+
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.28
+ */
+@Provider
+public class DurationParamConverterProvider implements ParamConverterProvider {
+    @Override
+    public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
+        if (Duration.class.isAssignableFrom(rawType)) {
+            return (ParamConverter<T>) DurationParamConverter.INSTANCE;
+        }
+
+        return null;
+
+    }
+}
