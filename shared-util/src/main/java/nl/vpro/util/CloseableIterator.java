4142648730fef629ae241ee6fd7f0ec2b88cc331
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-10-02 12:45:48 +0000

    AutoClosable is a bit more generic.

--- shared-util/src/main/java/nl/vpro/util/CloseableIterator.java
+++ shared-util/src/main/java/nl/vpro/util/CloseableIterator.java
@@ -1,12 +1,11 @@
 package nl.vpro.util;
 
-import java.io.Closeable;
 import java.util.Iterator;
 
 /**
  * @author Michiel Meeuwissen
  * @since 1.1
  */
-public interface CloseableIterator<T> extends Iterator<T>, Closeable {
+public interface CloseableIterator<T> extends Iterator<T>, AutoCloseable {
 
 }
