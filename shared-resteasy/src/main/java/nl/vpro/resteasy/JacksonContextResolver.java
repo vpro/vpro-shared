a3382a6b0ce81e381a434c6cbb03ce2aa4aa7d77
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-11-17 11:43:35 +0000

    MGNL-12797

--- shared-resteasy/src/main/java/nl/vpro/resteasy/JacksonContextResolver.java
+++ shared-resteasy/src/main/java/nl/vpro/resteasy/JacksonContextResolver.java
@@ -1,10 +1,12 @@
 package nl.vpro.resteasy;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 import nl.vpro.jackson2.Jackson2Mapper;
 
@@ -13,8 +15,9 @@ import nl.vpro.jackson2.Jackson2Mapper;
  * @since 2.0
  */
 @Provider
-@Produces(MediaType.APPLICATION_JSON)
-public class JacksonContextResolver implements ContextResolver<ObjectMapper> {
+@Consumes({"application/*+json", "text/json"})
+@Produces({"application/*+json", "text/json"})
+public class JacksonContextResolver extends JacksonJaxbJsonProvider implements ContextResolver<ObjectMapper> {
 
     public JacksonContextResolver() throws Exception {
     }
