7140b4590bd4d61af6ced4cfee3a4b133822eeba
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-12-09 16:44:08 +0000

    CIN-2175

---
+++ shared-xml/src/test/java/nl/vpro/xml/bind/LocalDateXmlAdapterTest.java
@@ -0,0 +1,29 @@
+package nl.vpro.xml.bind;
+
+import java.time.LocalDate;
+import java.time.Year;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.30
+ */
+public class LocalDateXmlAdapterTest {
+
+    private LocalDateXmlAdapter instance = new LocalDateXmlAdapter();
+
+    @Test
+    public void testUnmarshalLocalDate() throws Exception {
+        assertThat(instance.unmarshal("2015-12-09")).isEqualTo(LocalDate.of(2015, 12, 9));
+        assertThat(instance.unmarshal("2015")).isEqualTo(Year.of(2015));
+
+    }
+
+    @Test
+    public void testMarshal() throws Exception {
+
+    }
+}
