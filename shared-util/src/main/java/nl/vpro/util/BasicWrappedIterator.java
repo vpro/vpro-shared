0d5d98d89044dee6b1069ac0d5bd2ece63bf8b5c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-12-18 16:14:55 +0000

    test case failure.

--- shared-util/src/main/java/nl/vpro/util/BasicWrappedIterator.java
+++ shared-util/src/main/java/nl/vpro/util/BasicWrappedIterator.java
@@ -1,6 +1,8 @@
 package nl.vpro.util;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Optional;
 
 /**
  * @author Michiel Meeuwissen
@@ -8,8 +10,21 @@ import java.util.Iterator;
  */
 public class BasicWrappedIterator<T> extends WrappedIterator<T, T> {
 
+    private final Optional<Long> size;
     public BasicWrappedIterator(Iterator<T> wrapped) {
         super(wrapped);
+        size = Optional.empty();
+    }
+
+    public BasicWrappedIterator(Long size, Iterator<T> wrapped) {
+        super(wrapped);
+        this.size = Optional.ofNullable(size);
+    }
+
+
+    public BasicWrappedIterator(Collection<T> wrapped) {
+        super(wrapped.iterator());
+        this.size = Optional.of((long) wrapped.size());
     }
 
     @Override
