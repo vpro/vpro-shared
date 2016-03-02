6955ef73172d511bb1006ffdc2257fdd544c3c59
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-03-02 09:37:29 +0000

    MSE-3151

--- shared-xml/src/main/java/nl/vpro/xml/util/XmlUtils.java
+++ shared-xml/src/main/java/nl/vpro/xml/util/XmlUtils.java
@@ -2,7 +2,10 @@ package nl.vpro.xml.util;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
@@ -11,11 +14,36 @@ import javax.xml.datatype.XMLGregorianCalendar;
  */
 public class XmlUtils {
 
+    static final DatatypeFactory FACTORY;
+    static {
+        try {
+            FACTORY = DatatypeFactory.newInstance();
+        } catch (DatatypeConfigurationException e) {
+            throw new IllegalStateException();
+        }
+    }
+
     public static Date toDate(XMLGregorianCalendar in) {
         return in == null ? null : in.toGregorianCalendar().getTime();
     }
 
     public static Instant toInstant(XMLGregorianCalendar in) {
         return in == null ? null : in.toGregorianCalendar().getTime().toInstant();
+    }
+
+    public static XMLGregorianCalendar toXml(Date date) {
+        if (date == null) {
+            return null;
+        }
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTime(date);
+        return FACTORY.newXMLGregorianCalendar(c);
+    }
+
+    public static XMLGregorianCalendar toXml(Instant date) {
+        if (date == null) {
+            return null;
+        }
+        return toXml(Date.from(date));
     }
 }
