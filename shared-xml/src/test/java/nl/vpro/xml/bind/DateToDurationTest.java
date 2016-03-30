2c83671cb4b698be9488d7558145c4d0c704e844
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-03-30 10:39:35 +0000

    deprecation.

--- shared-xml/src/test/java/nl/vpro/xml/bind/DateToDurationTest.java
+++ shared-xml/src/test/java/nl/vpro/xml/bind/DateToDurationTest.java
@@ -7,7 +7,8 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * @author Michiel Meeuwissen
@@ -18,27 +19,27 @@ public class DateToDurationTest {
     @Test
     public void testDayTime() throws Exception {
         DateToDuration dateToDuration = new DateToDuration();
-        assertEquals("P0DT0H0M0.000S", dateToDuration.marshalDayTime(0l).toString());
-        assertEquals("P0DT0H1M40.000S", dateToDuration.marshalDayTime(100000l).toString());
-        assertEquals("P30DT0H0M0.000S", dateToDuration.marshalDayTime(30l * 24 * 60 * 60 * 1000).toString());
+        assertEquals("P0DT0H0M0.000S", dateToDuration.marshalDayTime(0L).toString());
+        assertEquals("P0DT0H1M40.000S", dateToDuration.marshalDayTime(100000L).toString());
+        assertEquals("P30DT0H0M0.000S", dateToDuration.marshalDayTime(30L * 24 * 60 * 60 * 1000).toString());
     }
 
     @Test
     public void testTime() throws Exception {
         DateToDuration dateToDuration = new DateToDuration();
-        assertEquals("P0Y0M0DT0H0M0.000S", dateToDuration.marshal(0l).toString());
-        assertEquals("P0Y0M0DT0H1M40.000S", dateToDuration.marshal(100000l).toString());
-        assertEquals("P0Y0M30DT0H0M0.000S", dateToDuration.marshal(30l * 24 * 60 * 60 * 1000).toString());
-        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(60l * 24 * 60 * 60 * 1000).toString());
+        assertEquals("P0Y0M0DT0H0M0.000S", dateToDuration.marshal(0L).toString());
+        assertEquals("P0Y0M0DT0H1M40.000S", dateToDuration.marshal(100000L).toString());
+        assertEquals("P0Y0M30DT0H0M0.000S", dateToDuration.marshal(30L * 24 * 60 * 60 * 1000).toString());
+        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(60L * 24 * 60 * 60 * 1000).toString());
     }
 
     @Test
     public void test() throws Exception {
         DateToDuration dateToDuration = new DateToDuration();
         assertEquals("P0DT0H0M0.000S", dateToDuration.marshal(new Date(0)).toString());
-        assertEquals("P0DT0H1M40.000S", dateToDuration.marshal(new Date(100000l)).toString());
-        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(new Date(2l * 30 * 24 * 60 * 60 * 1000)).toString());
-        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(new Date(60l * 24 * 60 * 60 * 1000)).toString());
+        assertEquals("P0DT0H1M40.000S", dateToDuration.marshal(new Date(100000L)).toString());
+        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(new Date(2L * 30 * 24 * 60 * 60 * 1000)).toString());
+        assertEquals("P0Y2M1DT0H0M0.000S", dateToDuration.marshal(new Date(60L * 24 * 60 * 60 * 1000)).toString());
     }
 
     @Test
