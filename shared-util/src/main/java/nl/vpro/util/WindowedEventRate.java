81f024a865e0cfc60882c4c5590b1b7a4c9f2ad6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-04-13 14:39:54 +0000

    Fixed all kind of exceptions, using an integration test.

---
+++ shared-util/src/main/java/nl/vpro/util/WindowedEventRate.java
@@ -0,0 +1,64 @@
+package nl.vpro.util;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.38
+ */
+public class WindowedEventRate {
+
+    protected final int bucketCount;
+    private final long[] buckets;
+    private final long windowSize;
+    private long currentBucketTime = System.currentTimeMillis();
+    private int currentBucket = 0;
+
+    public WindowedEventRate(int unit, TimeUnit timeUnit, int bucketCount) {
+        buckets = new long[bucketCount];
+        Arrays.fill(buckets, 0L);
+        windowSize = TimeUnit.MILLISECONDS.convert(unit, timeUnit);
+        this.bucketCount = bucketCount;
+    }
+    public WindowedEventRate(int unit, TimeUnit timeUnit) {
+        this(unit, timeUnit, 100);
+    }
+
+    public WindowedEventRate(TimeUnit timeUnit) {
+        this(1, timeUnit);
+    }
+
+
+    public void newEvent() {
+        shiftBuckets();
+        buckets[currentBucket]++;
+    }
+
+    public double getRate(TimeUnit unit) {
+        shiftBuckets();
+        long totalCount = 0;
+        for (long bucket : buckets) {
+            totalCount += bucket;
+        }
+        return ((double) totalCount * TimeUnit.MILLISECONDS.convert(1, unit)) / (buckets.length * windowSize);
+    }
+
+    private void shiftBuckets() {
+        long currentTime = System.currentTimeMillis();
+        long afterBucketBegin = currentTime - currentBucketTime;
+        int i = 0;
+        while (afterBucketBegin > windowSize && (i++) < buckets.length) {
+            currentBucket++;
+            currentBucket %= buckets.length;
+            buckets[currentBucket] = 0;
+            afterBucketBegin -= windowSize;
+            currentBucketTime = currentTime;
+        }
+    }
+
+    public String toString() {
+        return "" + getRate(TimeUnit.SECONDS) + " /s";
+    }
+
+}
