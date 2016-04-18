04fb13e835c4ef90f23a2b5bf7042c12652d552d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-04-18 11:53:50 +0000

    More a bit more robust.

--- shared-jackson2/src/main/java/nl/vpro/jackson2/StringInstantToJsonTimestamp.java
+++ shared-jackson2/src/main/java/nl/vpro/jackson2/StringInstantToJsonTimestamp.java
@@ -9,9 +9,12 @@ import java.time.Instant;
 
 import javax.xml.bind.DatatypeConverter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonTokenId;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -23,6 +26,8 @@ import com.fasterxml.jackson.databind.SerializerProvider;
  */
 public class StringInstantToJsonTimestamp {
 
+    private static final Logger LOG = LoggerFactory.getLogger(StringInstantToJsonTimestamp.class);
+
     public static class Serializer extends JsonSerializer<String> {
         public static StringInstantToJsonTimestamp.Serializer INSTANCE = new StringInstantToJsonTimestamp.Serializer();
 
@@ -31,7 +36,12 @@ public class StringInstantToJsonTimestamp {
             if (value == null) {
                 jgen.writeNull();
             } else {
-                jgen.writeNumber(DatatypeConverter.parseTime(value).toInstant().toEpochMilli());
+                try {
+                    jgen.writeNumber(DatatypeConverter.parseTime(value).toInstant().toEpochMilli());
+                } catch (IllegalArgumentException iae) {
+                    LOG.warn("Could not parse {}. Writing null to json", value);
+                    jgen.writeNull();
+                }
             }
         }
     }
@@ -42,8 +52,23 @@ public class StringInstantToJsonTimestamp {
         public static StringInstantToJsonTimestamp.Deserializer INSTANCE = new StringInstantToJsonTimestamp.Deserializer();
 
         @Override
-        public Instant deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            return Instant.ofEpochMilli(jp.getLongValue());
+        public Instant deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+            switch(jp.getCurrentTokenId()) {
+                case JsonTokenId.ID_NUMBER_INT:
+                    return Instant.ofEpochMilli(jp.getLongValue());
+                case JsonTokenId.ID_NULL:
+                    return null;
+                case JsonTokenId.ID_STRING:
+                    try {
+                        return DatatypeConverter.parseTime(jp.getText()).toInstant();
+                    } catch (IllegalArgumentException iae) {
+                        LOG.warn("Could not parse {}. Writing null to json", jp.getText());
+                        return null;
+                    }
+                default:
+                    LOG.warn("Could not parse {} to instant. Returing null", jp.toString());
+                    return null;
+            }
         }
     }
 }
