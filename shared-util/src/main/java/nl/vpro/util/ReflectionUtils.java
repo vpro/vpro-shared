a7a5e8e1b91b51da02cf9ed7e2ea6e722b766ad9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-05-11 10:32:44 +0000

    Details.

--- shared-util/src/main/java/nl/vpro/util/ReflectionUtils.java
+++ shared-util/src/main/java/nl/vpro/util/ReflectionUtils.java
@@ -3,6 +3,7 @@ package nl.vpro.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Properties;
@@ -54,16 +55,57 @@ public class ReflectionUtils {
     }
 
     public static void configured(Object instance, String... configFiles) throws IOException {
+        configured(null, instance, configFiles);
+    }
+
+    public static void configured(String env, Object instance, String... configFiles) throws IOException {
+        Properties properties = filtered(env, getProperties(configFiles));
+        LOG.debug("Configuring with {}", properties);
+        properties.forEach((k, v) -> ReflectionUtils.setProperty(instance, k, v));
+    }
+
+    public static Properties getProperties(String... configFiles) throws IOException {
         Properties properties = new Properties();
         for (String configFile : configFiles) {
+            if (configFile.startsWith("classpath:")) {
+                InputStream in = ReflectionUtils.class.getResourceAsStream(configFile.substring("classpath:".length()));
+                if (in != null) {
+                    LOG.info("Reading properties from classpath {}", configFile);
+                    properties.load(in);
+                    continue;
+                }
+            }
             File file = new File(configFile);
             if (!file.canRead()) {
                 LOG.info("The file {} cannot be read", file);
             } else {
                 LOG.info("Reading properties from {}", file);
                 properties.load(new FileInputStream(file));
-                properties.forEach((k, v) -> ReflectionUtils.setProperty(instance, k, v));
             }
         }
+        return properties;
+    }
+    public static  Properties filtered(String env, Properties properties) {
+        Properties result = new Properties();
+        properties.forEach((k, v) -> {
+            String key = (String) k;
+            String value = (String) v;
+            String[] split = key.split("\\.", 2);
+            if (split.length == 1) {
+                String explicitValue = value;
+                if (env != null) {
+                    String envValue = properties.getProperty(key + "." + env);
+                    if (envValue != null) {
+                        explicitValue = envValue;
+                    }
+                }
+                result.put(key, explicitValue);
+            } else {
+                if ((env == null && split[1].equals("test")) || split[1].equals(env)) {
+                    result.put(split[0], value);
+                }
+            }
+        });
+        return result;
     }
 }
