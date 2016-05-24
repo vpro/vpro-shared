4584905681c637df56c890b4999c3d2db870b19a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-05-24 14:03:20 +0000

    Deprecation.

--- shared-hibernate/src/test/java/nl/vpro/hibernate/Location.java
+++ shared-hibernate/src/test/java/nl/vpro/hibernate/Location.java
@@ -1,27 +1,29 @@
 package nl.vpro.hibernate;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.*;
-import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import nl.vpro.jackson2.XMLDurationToJsonTimestamp;
 
- 
+
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "locationType",
-    propOrder = {"programUrl",
-     
+    propOrder = {
+        "programUrl",
         "subtitles",
         "offset",
         "duration"})
 public class Location  {
 
-    
+
     @Column(nullable = false)
     @XmlElement
     protected String programUrl;
@@ -32,32 +34,34 @@ public class Location  {
 
     @Temporal(TemporalType.TIME)
     @Column(name = "start_offset")
-    @JsonSerialize(using = XMLDurationToJsonTimestamp.Serializer.class, include = JsonSerialize.Inclusion.NON_NULL)
+    @JsonSerialize(using = XMLDurationToJsonTimestamp.Serializer.class)
     @JsonDeserialize(using = XMLDurationToJsonTimestamp.DeserializerDate.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     protected Date offset;
 
     @Temporal(TemporalType.TIME)
     @XmlElement
-    @JsonSerialize(using = XMLDurationToJsonTimestamp.Serializer.class, include = JsonSerialize.Inclusion.NON_NULL)
+    @JsonSerialize(using = XMLDurationToJsonTimestamp.Serializer.class)
     @JsonDeserialize(using = XMLDurationToJsonTimestamp.DeserializerDate.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     protected Date duration;
 
-    
+
 
     @Column(nullable = true)
     @XmlTransient
     protected Long neboId;
 
-   
+
 
     @XmlTransient
     private boolean ceresUpdate = false;
 
     public Location() {
     }
-    
 
-    
+
+
     public String getProgramUrl() {
         return programUrl;
     }
@@ -66,7 +70,7 @@ public class Location  {
         this.programUrl = url == null ? null : url.trim();
         return this;
     }
-  
+
 
     public String getSubtitles() {
         return subtitles;
@@ -94,7 +98,7 @@ public class Location  {
         this.duration = duration;
         return this;
     }
-  
+
     public boolean isCeresUpdate() {
         return ceresUpdate;
     }
@@ -103,9 +107,9 @@ public class Location  {
         this.ceresUpdate = ceresUpdate;
     }
 
-    
- 
-  
+
+
+
     @Override
     public int hashCode() {
         int result = super.hashCode();
@@ -120,6 +124,6 @@ public class Location  {
     public void setNeboId(Long neboId) {
         this.neboId = neboId;
     }
-   
+
 
 }
