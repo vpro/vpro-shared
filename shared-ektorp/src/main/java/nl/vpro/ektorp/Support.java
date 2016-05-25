79cbef60a0636177b4716474290a0e4246f69a12
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-05-25 09:54:18 +0000

    NPA-302

--- shared-ektorp/src/main/java/nl/vpro/ektorp/Support.java
+++ shared-ektorp/src/main/java/nl/vpro/ektorp/Support.java
@@ -2,6 +2,7 @@ package nl.vpro.ektorp;
 
 import org.ektorp.CouchDbConnector;
 import org.ektorp.DbAccessException;
+import org.ektorp.impl.NameConventions;
 import org.ektorp.support.CouchDbRepositorySupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,15 +15,20 @@ public class Support<T> extends CouchDbRepositorySupport<T> {
     private static final Logger LOG = LoggerFactory.getLogger(Support.class);
 
 
-    public static <T> Support<T> getInstance(String logInfo, Class<T> type, CouchDbConnector db) {
+    public static <T> Support<T> getInstance(String logInfo, Class<T> type, CouchDbConnector db, boolean createIfNotExists) {
         try {
-            return new Support<>(type, db);
+            return new Support<>(type, db, createIfNotExists);
         } catch (DbAccessException dbe) {
             LOG.error(logInfo + " " + db.getDatabaseName() + ": " + dbe.getClass().getName() + " " + dbe.getMessage());
             LOG.info("Will create a connector to couchdb without trying to check and create the database (supposing it is temporary down now");
             return new Support<>(type, db, false);
         }
     }
+
+    public static <T> Support<T> getInstance(String logInfo, Class<T> type, CouchDbConnector db) {
+        return getInstance(logInfo, type, db, true);
+    }
+
 
     protected Support(Class<T> type, CouchDbConnector db) {
         super(type, db);
@@ -38,6 +44,10 @@ public class Support<T> extends CouchDbRepositorySupport<T> {
 
     public String getPath() {
         return getConnector().path();
+    }
+
+    public String getDesignDocumentId() {
+        return NameConventions.designDocName(type);
     }
 
 }
