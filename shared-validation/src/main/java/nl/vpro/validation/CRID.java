3147011afee45e8815a6100d25c2ed531a849235
Daan Debie <debie.daan@gmail.com>
2016-05-26 14:36:29 +0000

    CRID validation should also work for parameters

--- shared-validation/src/main/java/nl/vpro/validation/CRID.java
+++ shared-validation/src/main/java/nl/vpro/validation/CRID.java
@@ -13,9 +13,10 @@ import javax.validation.Payload;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({METHOD, FIELD})
+@Target({METHOD, FIELD, PARAMETER})
 @Retention(RUNTIME)
 @Constraint(validatedBy = CRIDValidator.class)
 @Documented
