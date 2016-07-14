162c79e9491d17ea582ae62ac2f97dcd38c7ee84
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-07-14 11:33:04 +0000

    Simple util.

---
+++ shared-client-resteasy/src/main/java/nl/vpro/api/client/resteasy/Utils.java
@@ -0,0 +1,28 @@
+package nl.vpro.api.client.resteasy;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import javax.ws.rs.NotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.47
+ */
+public class Utils {
+    private static Logger LOG = LoggerFactory.getLogger(Utils.class);
+
+
+    static <T> Optional<T> wrapNotFound(Supplier<T> t) {
+
+        try {
+            return Optional.ofNullable(t.get());
+        } catch (NotFoundException nfe) {
+            LOG.debug(nfe.getMessage());
+            return Optional.empty();
+        }
+    }
+}
