1961dd01294f0de42683f5986687a167b1236d5e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-07-16 20:39:28 +0000

    Tested ElasticSearchIterator

--- shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/ElasticSearchIterator.java
+++ shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/ElasticSearchIterator.java
@@ -2,41 +2,58 @@ package nl.vpro.elasticsearch;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.function.Function;
 
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.search.SearchHit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.vpro.util.CountedIterator;
 
 /**
- * TODO untested.
+ * A wrapper around the Elastic Search scroll interface.
+ * 
+ * See 
  * @author Michiel Meeuwissen
  * @since 0.47
  */
-public class ElasticSearchIterator<T>  implements Iterator<T> {
+public class ElasticSearchIterator<T>  implements CountedIterator<T> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ElasticSearchIterator.class);
+
 
     final Function<SearchHit, T> adapt;
-    final SearchResponse response;
     final Client client;
 
+    SearchRequestBuilder builder;
+    SearchResponse response;
     SearchHit[] hits;
+    private long count = -1;
+    
     boolean hasNext;
     int i = -1;
     T next;
     boolean needsNext = true;
+    
 
-    public ElasticSearchIterator(SearchResponse response, Client client, Function<SearchHit, T> adapt) {
-        this.response = response;
-        this.hits = response.getHits().getHits();
+    public ElasticSearchIterator(Client client, Function<SearchHit, T> adapt) {
         this.adapt = adapt;
         this.client = client;
-        if (this.hits.length == 0) {
-            needsNext = false;
-            hasNext = false;
-
-        }
+        needsNext = true;
     }
+
+    public SearchRequestBuilder prepareSearch(String... indices) {
+        builder = client.prepareSearch(indices);
+        builder.setScroll(TimeValue.timeValueSeconds(60));
+        builder.setSize(1000);
+        return builder;
+    }
+
     @Override
     public boolean hasNext() {
         findNext();
@@ -45,20 +62,42 @@ public class ElasticSearchIterator<T>  implements Iterator<T> {
     }
 
     protected void findNext() {
+        if (response == null) {
+            // first call only.
+            if (builder == null) {
+                throw new IllegalStateException("prepareSearch not called");
+            }
+            response = builder.get();
+            hits = response.getHits().getHits();
+            if (hits.length == 0) {
+                hasNext = false;
+                needsNext = false;
+            }
+        }
         if (needsNext) {
             i++;
             boolean newHasNext = i < hits.length;
             if (!newHasNext) {
-                hits = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet().getHits().getHits();
-                if (hits.length == 0) {
-                    hasNext = false;
+                if (response.getScrollId() != null) {
+                    response = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet();
+                    hits = response.getHits().getHits();
+                    i = 0;
+                    if (hits.length == 0) {
+                        hasNext = false;
+                    } else {
+                        hasNext = true;
+                    }
                 } else {
-                    hasNext = true;
+                    LOG.warn("No scroll id found, so not possible to scroll next batch");
+                    hasNext = false;
                 }
             } else {
                 hasNext = true;
             }
-            next = adapt.apply(hits[i]);
+            if (hasNext) {
+                next = adapt.apply(hits[i]);
+            }
+            needsNext = false;
         }
 
     }
@@ -68,8 +107,19 @@ public class ElasticSearchIterator<T>  implements Iterator<T> {
         if (!hasNext) {
             throw new NoSuchElementException();
         }
+        count++;
         needsNext = true;
         return next;
     }
 
+    @Override
+    public Optional<Long> getSize() {
+        return Optional.of(response.getHits().getTotalHits());
+    }
+
+    @Override
+    public Long getCount() {
+        return count;
+    }
+ 
 }
