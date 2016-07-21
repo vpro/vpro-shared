e7122542636fb96c769bad0208dd560cfdbbeb1f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-07-21 15:22:21 +0000

    NPA-318

--- shared-resteasy/src/main/java/nl/vpro/resteasy/InstantParamConverter.java
+++ shared-resteasy/src/main/java/nl/vpro/resteasy/InstantParamConverter.java
@@ -1,13 +1,13 @@
 package nl.vpro.resteasy;
 
 import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeParseException;
+import java.time.ZoneId;
 import java.util.regex.Pattern;
 
 import javax.ws.rs.ext.ParamConverter;
 import javax.ws.rs.ext.Provider;
+
+import nl.vpro.util.TimeUtils;
 
 
 /**
@@ -17,6 +17,8 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class InstantParamConverter implements ParamConverter<Instant> {
 
+    private static ZoneId ZONE_ID = ZoneId.of("Europe/Amsterdam");
+
     static InstantParamConverter INSTANCE = new InstantParamConverter();
     private static final Pattern NUMERIC = Pattern.compile("\\d+");
 
@@ -25,27 +27,8 @@ public class InstantParamConverter implements ParamConverter<Instant> {
         if (value == null || value.length() == 0) {
             return null;
         }
-        if (NUMERIC.matcher(value).matches()) {
-            return Instant.ofEpochMilli(Long.valueOf(value));
-        }
-        try {
-            return Instant.parse(value);
-        } catch (DateTimeParseException dte) {
-            try {
-                return OffsetDateTime.parse(value).toInstant();
-            } catch (DateTimeParseException dte2) {
-                try {
-                    return ZonedDateTime.parse(value).toInstant();
-                } catch (DateTimeParseException dte3) {
-                    if (value.contains(" ")) {
-                        value = value.replace(" ", "+");
-                        return fromString(value);
-                    } else {
-                        throw dte;
-                    }
-                }
-            }
-        }
+
+        return TimeUtils.parse(value).orElse(null);
 
 
     }
