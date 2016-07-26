b977d3d5e2b3766ec1bc2407084ee7c9fa3a42dd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-07-26 11:40:09 +0000

    test case failure.s

---
+++ shared-util/src/main/java/nl/vpro/util/LinkedProperties.java
@@ -0,0 +1,60 @@
+package nl.vpro.util;
+
+import java.util.*;
+
+/**
+ * Properties with predictable iteration order.
+ * @author Michiel Meeuwissen
+ * @since 0.47
+ */
+public class LinkedProperties extends Properties {
+    private final HashSet<Object> keys = new LinkedHashSet<Object>();
+
+    public LinkedProperties() {
+    }
+
+    public Iterable<Object> orderedKeys() {
+        return Collections.list(keys());
+    }
+
+    @Override
+    public Enumeration<Object> keys() {
+        return Collections.enumeration(keys);
+    }
+
+    @Override
+    public Set<Map.Entry<Object, Object>> entrySet() {
+        return new AbstractSet<Map.Entry<Object, Object>>() {
+            @Override
+            public Iterator<Map.Entry<Object, Object>> iterator() {
+                Iterator<Object> keyIterator = keys.iterator();
+                return new Iterator<Map.Entry<Object, Object>>() {
+                    @Override
+                    public boolean hasNext() {
+                        return keyIterator.hasNext();
+
+                    }
+
+                    @Override
+                    public Map.Entry<Object, Object> next() {
+                        Object key = keyIterator.next();
+                        return new AbstractMap.SimpleEntry<>(key, get(key));
+                    }
+                };
+
+            }
+
+            @Override
+            public int size() {
+                return keys.size();
+            }
+        };
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        keys.add(key);
+        return super.put(key, value);
+    }
+
+}
