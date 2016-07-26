484957955a628ff88d7b947b4853a077854ee9aa
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-07-26 15:40:03 +0000

    MSE-3345

---
+++ shared-xml/src/test/java/nl/vpro/xml/bind/LocaleAdapterTest.java
@@ -0,0 +1,36 @@
+package nl.vpro.xml.bind;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.47
+ */
+public class LocaleAdapterTest {
+
+
+    @Test
+    public void adapt() throws Exception {
+        LocaleAdapter adapter = new LocaleAdapter();
+        assertThat(adapter.marshal(new Locale("nl", "NL"))).isEqualTo("nl-NL");
+        assertThat(adapter.marshal(new Locale("nl", "NL", "slang"))).isEqualTo("nl-NL-slang");
+
+
+    }
+
+
+    @Test
+    public void unadapt() throws Exception {
+        LocaleAdapter adapter = new LocaleAdapter();
+        assertThat(adapter.unmarshal("nl")).isEqualTo(new Locale("nl"));
+        assertThat(adapter.unmarshal("nl-NL")).isEqualTo(new Locale("nl", "NL"));
+        assertThat(adapter.unmarshal("nl-NL-slang-xy")).isEqualTo(new Locale("nl", "NL", "slang-xy"));
+
+
+    }
+
+}
