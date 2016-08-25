e2a0186707ee5a6186c41904de604115b1e9b514
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-08-25 14:47:26 +0000

    test case failure.

--- shared-test/src/test/java/nl/vpro/test/util/jackson2/Jackson2TestUtilTest.java
+++ shared-test/src/test/java/nl/vpro/test/util/jackson2/Jackson2TestUtilTest.java
@@ -22,9 +22,24 @@ public class Jackson2TestUtilTest {
     public static class A {
         private String a = "a";
         public A() {
-            
+
         }
-      
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            A a1 = (A) o;
+
+            return a != null ? a.equals(a1.a) : a1.a == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return a != null ? a.hashCode() : 0;
+        }
     }
     @Test
     public void roundTrip() throws Exception {
