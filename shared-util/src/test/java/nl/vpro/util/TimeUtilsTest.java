6532bf2e3013dd0de6ffd675e6ccfc4e8aaef51e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-09-14 13:54:27 +0000

    dammit.

--- shared-util/src/test/java/nl/vpro/util/TimeUtilsTest.java
+++ shared-util/src/test/java/nl/vpro/util/TimeUtilsTest.java
@@ -20,5 +20,12 @@ public class TimeUtilsTest {
 
     }
 
+    @Test
+    public void parse() throws Exception {
+
+        assertThat(TimeUtils.parse("2000-07-11T14:00:33.556+02:00").get()).isEqualTo(ZonedDateTime.of(LocalDate.of(2000, 7, 11), LocalTime.of(14, 0, 33, 556000000), TimeUtils.ZONE_ID).toInstant());
+
+    }
+
 
 }
