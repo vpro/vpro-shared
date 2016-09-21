cdc3b99551213c1f5f7e566464bf8ca7f1c0dc54
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-09-21 08:22:43 +0000

    serializetestutil

---
+++ shared-test/src/main/java/nl/vpro/test/util/serialize/SerializeTestUtil.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2012 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.test.util.serialize;
+
+import java.io.*;
+
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @since 0.50
+ */
+public class SerializeTestUtil {
+
+    public static <T extends Serializable> byte[] serialize(T object) throws IOException {
+
+        ByteArrayOutputStream bytes  = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(bytes);
+        out.writeObject(object);
+        out.close();
+        return bytes.toByteArray();
+
+    }
+
+    public static <T extends Serializable> T deserialize(byte[] bytes, Class<T> clazz) throws IOException {
+        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes));
+        T result = null;
+        try {
+            result = (T) in.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        in.close();
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static  <T extends Serializable> T roundTrip(T input) throws IOException {
+        return (T) deserialize(serialize(input), input.getClass());
+    }
+
+
+}
