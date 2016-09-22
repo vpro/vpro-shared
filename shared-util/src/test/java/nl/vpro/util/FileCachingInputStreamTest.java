1a7735475366ecf598ca517bd197de7933e146fd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-09-22 20:06:38 +0000

    lombok@Builder

---
+++ shared-util/src/test/java/nl/vpro/util/FileCachingInputStreamTest.java
@@ -0,0 +1,34 @@
+package nl.vpro.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 0.50
+ */
+public class FileCachingInputStreamTest {
+	
+	@Test
+	public void test() throws IOException {
+	    byte[] in = new byte[] {1,2,3,4,5,6,7,8};
+		FileCachingInputStream inputStream  = FileCachingInputStream.builder()
+            .bufferSize(2)
+            .input(new ByteArrayInputStream(in))
+            .build();
+        
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        IOUtils.copy(inputStream, out);
+        
+        assertThat(out.toByteArray()).containsExactly(in);
+	}
+
+}
