81cee5931a29fadd285a417517a368cdb417e94d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-09-23 11:48:24 +0000

    javadoc, log parameter

--- shared-util/src/main/java/nl/vpro/util/FileCachingInputStream.java
+++ shared-util/src/main/java/nl/vpro/util/FileCachingInputStream.java
@@ -1,7 +1,6 @@
 package nl.vpro.util;
 
 import lombok.Builder;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -9,17 +8,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * When wrapping this around your inputstream, it will be read as fast a possible, but you can consume from it slower.
  *
- * It will buffer the result to memory, until that becomes too large, then a file buffer will be created.
+ * It will buffer the result to a temporary file.
+ *
+ * Use this if you want to consume an inputstream as fast as possible, while handing it at a slower pace. The cost is the creation of the temporary file.
+ * 
  *
  * @author Michiel Meeuwissen
  * @since 0.50
  */
 
-@Slf4j
 public class FileCachingInputStream extends InputStream {
 
     private static final int EOF = -1;
@@ -28,13 +31,18 @@ public class FileCachingInputStream extends InputStream {
     private final InputStream file;
     private int count = 0;
 
+    private Logger log = LoggerFactory.getLogger(FileCachingInputStream.class);
+
+
     @Builder
     private FileCachingInputStream(
         InputStream input,
         Path path,
         String filePrefix,
         long  batchSize,
-        int outputBuffer) throws IOException {
+        int outputBuffer,
+        Logger logger
+        ) throws IOException {
 
         super();
         tempFile = Files.createTempFile(
@@ -43,6 +51,9 @@ public class FileCachingInputStream extends InputStream {
             null);
 
         OutputStream out = new BufferedOutputStream(Files.newOutputStream(tempFile), outputBuffer == 0 ? 8192 : outputBuffer);
+        if (logger != null) {
+            this.log = logger;
+        }
 
         copier = Copier.builder()
             .input(input)
