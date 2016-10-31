ced622e0f95948074fd0a9f744bea9c3e854d282
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-10-31 09:49:56 +0000

    Tracking a bug.

--- shared-hibernate/src/test/java/nl/vpro/hibernate/JsonBridgeTest.java
+++ shared-hibernate/src/test/java/nl/vpro/hibernate/JsonBridgeTest.java
@@ -1,10 +1,11 @@
 package nl.vpro.hibernate;
 
-import nl.vpro.test.util.jackson2.Jackson2TestUtil;
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.Test;
+
+import nl.vpro.test.util.jackson2.Jackson2TestUtil;
 
 /**
  * @author Michiel Meeuwissen
@@ -14,7 +15,7 @@ public class JsonBridgeTest {
 
     @Test
     public void testObjectToString() throws Exception {
-        String string = "[{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/320x180_180.m4v\"},{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/640x360_1000.m4v\"},{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/854x480_1500.m4v\"}]\n" 
+        String string = "[{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/320x180_180.m4v\"},{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/640x360_1000.m4v\"},{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/854x480_1500.m4v\"}]\n"
             ;
 
         JsonBridge bridge = new JsonBridge();
@@ -24,7 +25,25 @@ public class JsonBridgeTest {
         bridge.setParameterValues(params);
 
         Location[] o = (Location[]) bridge.stringToObject(string);
-        
+
+
+        Jackson2TestUtil.roundTripAndSimilar(o, string);
+
+
+    }
+
+    @Test
+    public void testObjectToStringWithDuration() throws Exception {
+        String string = "[{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/320x180_180.m4v\"},{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/640x360_1000.m4v\"},{\"programUrl\":\"http://content.omroep.nl/avrotros/transcoding/communicatie/1422371188061/854x480_1500.m4v\"}]\n";
+
+        JsonBridge bridge = new JsonBridge();
+        Map<String, String> params = new HashMap<>();
+        params.put("class", LocationWithDurationField[].class.getName());
+
+        bridge.setParameterValues(params);
+
+        LocationWithDurationField[] o = (LocationWithDurationField []) bridge.stringToObject(string);
+
 
         Jackson2TestUtil.roundTripAndSimilar(o, string);
 
