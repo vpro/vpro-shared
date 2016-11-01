bb859605a124135e39daa66e156c099f26616787
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-01 14:39:25 +0000

    serializeAndEquals.

--- shared-test/src/main/java/nl/vpro/test/util/serialize/SerializeTestUtil.java
+++ shared-test/src/main/java/nl/vpro/test/util/serialize/SerializeTestUtil.java
@@ -6,6 +6,11 @@ package nl.vpro.test.util.serialize;
 
 import java.io.*;
 
+import org.assertj.core.api.AbstractObjectAssert;
+import org.assertj.core.api.Assertions;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 
 /**
  *
@@ -32,9 +37,46 @@ public class SerializeTestUtil {
         }
     }
 
-    public static  <T> T roundTrip(T input) throws IOException {
+    public static <T> T roundTrip(T input) throws IOException {
         return (T) deserialize(serialize(input), input.getClass());
     }
 
+    public static <T> T roundTripAndEquals(T input) throws IOException {
+        T newT = (T) deserialize(serialize(input), input.getClass());
+        assertThat(newT).isEqualTo(input);
+        return newT;
+
+    }
+
+    public static <T> SerializeTestUtil.ObjectAssert assertThatSerialized(T o) {
+        return new SerializeTestUtil.ObjectAssert<>(o);
+    }
+
+
+    public static class ObjectAssert<S extends SerializeTestUtil.ObjectAssert<S, A>, A> extends AbstractObjectAssert<S, A> {
+
+        A rounded;
+
+        protected ObjectAssert(A actual) {
+            super(actual, SerializeTestUtil.ObjectAssert.class);
+        }
+
+
+
+        public AbstractObjectAssert<?, A> andRounded() throws IOException {
+            if (rounded == null) {
+                rounded = roundTrip(actual);
+            }
+            return Assertions.assertThat(rounded);
+        }
+
+        public A get() throws IOException {
+            if (rounded == null) {
+                rounded = roundTrip(actual);
+            }
+            return rounded;
+        }
+
+    }
 
 }
