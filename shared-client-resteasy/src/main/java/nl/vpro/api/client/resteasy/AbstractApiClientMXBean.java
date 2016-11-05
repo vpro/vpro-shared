40292976567f71a8c2668b463b043ed5e1e8bb00
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-05 16:40:54 +0000

    Added total rate too.

--- shared-client-resteasy/src/main/java/nl/vpro/api/client/resteasy/AbstractApiClientMXBean.java
+++ shared-client-resteasy/src/main/java/nl/vpro/api/client/resteasy/AbstractApiClientMXBean.java
@@ -1,21 +1,27 @@
 package nl.vpro.api.client.resteasy;
 
+import javax.management.MXBean;
+
+import nl.vpro.jmx.Units;
+
 /**
  * @author Michiel Meeuwissen
  * @since 0.51
  */
-public interface AbstractApiClientMBean {
+@MXBean
+public interface AbstractApiClientMXBean {
+
+    @Units("duration")
     String getConnectionRequestTimeout();
     void setConnectionRequestTimeout(String connectionRequestTimeout);
     String getConnectTimeout();
     void setConnectTimeout(String connectTimeout);
     String getSocketTimeout();
     void setSocketTimeout(String socketTimeout);
-
     String getCounts();
-
     long getCount(String method);
-
     long getTotalCount();
+    @Units("events/minute")
+    double getRate();
 
 }
