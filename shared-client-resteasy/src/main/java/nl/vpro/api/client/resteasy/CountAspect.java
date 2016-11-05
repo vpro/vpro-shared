78f30dbbea0febe7c8000dc989f5ead6f14a17c4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-05 11:50:07 +0000

    Better separation of concern. More MBeans.

---
+++ shared-client-resteasy/src/main/java/nl/vpro/api/client/resteasy/CountAspect.java
@@ -0,0 +1,54 @@
+package nl.vpro.api.client.resteasy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+/**
+ * Wraps all calls to log client errors, and to register some statistics.
+ *
+ * @author Michiel Meeuwissen
+ * @since 1.57
+ */
+
+public class CountAspect<T> implements InvocationHandler {
+    private final T proxied;
+
+
+    private final Map<Method, Counter> counts;
+    private final ObjectName name;
+
+    CountAspect(T proxied, Map<Method, Counter> counter, ObjectName name) {
+        this.proxied = proxied;
+        this.counts = counter;
+        this.name = name;
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        counts.computeIfAbsent(method, (m) -> new Counter(getObjectName(m))).incrementAndGet();
+        return method.invoke(proxied, args);
+    }
+
+    ObjectName getObjectName(Method m) {
+        try {
+            return new ObjectName(name.toString() + ",name=" + AbstractApiClient.methodToString(m));
+        } catch (MalformedObjectNameException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static <T> T proxyCounter(Map<Method, Counter> counter, ObjectName name, Class<T> restInterface, T service) {
+        return (T) Proxy.newProxyInstance(CountAspect.class.getClassLoader(),
+            new Class[]{restInterface},
+            new CountAspect<T>(service, counter, name));
+    }
+
+}
+
---
+++ shared-client-resteasy/src/main/java/nl/vpro/api/client/resteasy/CounterMBean.java
@@ -0,0 +1,10 @@
+package nl.vpro.api.client.resteasy;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.57
+ */
+public interface CounterMBean {
+
+    long getCount();
+}
