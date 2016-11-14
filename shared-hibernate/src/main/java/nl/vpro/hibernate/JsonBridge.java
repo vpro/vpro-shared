a04decdf643dcef4448c4d08a1151723dd59666f
Jeroen van Vianen <jeroen@vanvianen.nl>
2016-11-14 16:20:04 +0000

    MSE-3485 Make Lucene JSON Bridge smarter when serializing large objects. If it's an array, serialize smaller slices until it's less than max size (32K). If it still doesn't fit, return "{}"

--- shared-hibernate/src/main/java/nl/vpro/hibernate/JsonBridge.java
+++ shared-hibernate/src/main/java/nl/vpro/hibernate/JsonBridge.java
@@ -5,6 +5,7 @@
 package nl.vpro.hibernate;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.hibernate.search.bridge.ParameterizedBridge;
@@ -13,6 +14,8 @@ import org.hibernate.search.bridge.TwoWayStringBridge;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import nl.vpro.jackson2.Jackson2Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -20,6 +23,10 @@ import nl.vpro.jackson2.Jackson2Mapper;
  * @since 3.5
  */
 public class JsonBridge implements TwoWayStringBridge, ParameterizedBridge {
+
+    private final static Logger LOG = LoggerFactory.getLogger(JsonBridge.class);
+
+    public final static int MAX_LENGTH = 32000;
 
     private Class clazz;
 
@@ -44,7 +51,30 @@ public class JsonBridge implements TwoWayStringBridge, ParameterizedBridge {
             return null;
         }
         try {
-            return Jackson2Mapper.getInstance().writeValueAsString(object);
+            String ret = Jackson2Mapper.getInstance().writeValueAsString(object);
+
+            int len = ret.length();
+            if (len > MAX_LENGTH) {
+                if (object instanceof Object[]) {
+                    LOG.warn("Cannot store JSON representation of object type " + object.getClass().getName() + ": trying shorter version");
+                    int size = ((Object[]) object).length;
+                    while (len > MAX_LENGTH && size > 0) {
+                        object = Arrays.copyOfRange((Object[]) object, 0, --size);
+                        ret = Jackson2Mapper.getInstance().writeValueAsString(object);
+                        len = ret.length();
+                    }
+
+                    if (size == 0) {
+                        LOG.warn("Cannot store JSON representation of object type " + object.getClass().getName() + ": even first item in array already too large (maxlength = " + MAX_LENGTH + ")");
+                        return "{}";
+                    }
+                } else {
+                    LOG.warn("Cannot store JSON representation of object type " + object.getClass().getName() + ": " + object + " (maxlength = " + MAX_LENGTH + ")");
+                    return "{}";
+                }
+            }
+
+            return ret;
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
