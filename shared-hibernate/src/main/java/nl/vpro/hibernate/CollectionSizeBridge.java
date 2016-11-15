ed17f4e5a06006057ca32f1cd327371d8d3e7004
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-11-15 15:02:19 +0000

    MSE-3485

--- shared-hibernate/src/main/java/nl/vpro/hibernate/CollectionSizeBridge.java
+++ shared-hibernate/src/main/java/nl/vpro/hibernate/CollectionSizeBridge.java
@@ -6,9 +6,15 @@ package nl.vpro.hibernate;
 
 import java.util.Collection;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.hibernate.search.bridge.LuceneOptions;
 import org.hibernate.search.bridge.StringBridge;
+import org.hibernate.search.bridge.TwoWayFieldBridge;
 
-public class CollectionSizeBridge implements StringBridge {
+public class CollectionSizeBridge implements StringBridge, TwoWayFieldBridge {
+
 
     @Override
     public String objectToString(Object object) {
@@ -21,5 +27,17 @@ public class CollectionSizeBridge implements StringBridge {
         }
         int size = ((Collection)object).size();
         return String.format("%03d",  size);
+    }
+
+
+    @Override
+    public Object get(String name, Document document) {
+        return Integer.parseInt(document.get(name));
+
+    }
+    @Override
+    public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
+        Field field = new StringField(name, objectToString(value), luceneOptions.getStore());
+        document.add(field);
     }
 }
