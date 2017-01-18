16574cbbf25c6462c967246719f5ac8289efad88
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-01-18 13:12:59 +0000

    Some efforts to make transforming collections work better.

--- vpro-shared-util/src/main/java/nl/vpro/util/TransformingList.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/TransformingList.java
@@ -1,22 +1,22 @@
 package nl.vpro.util;
 
-import java.util.AbstractList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 
 /**
  * @author Michiel Meeuwissen
  * @since 4.3
  */
-public class TransformingList<T, S> extends AbstractList<T> implements TransformingCollection<T, S> {
-
+public class TransformingList<T, S> extends AbstractList<T> implements TransformingCollection<T, S, List<T>, List<S>> {
 
     private List<S> wrapped;
+    private final List<Optional<T>> transformed;
     private final Function<S, T> transformer;
     private final Function<T, S> producer;
 
     public TransformingList(List<S> wrapped, Function<S, T> transformer, Function<T, S> producer) {
         this.wrapped = wrapped;
+        this.transformed = new ArrayList<>(Collections.nCopies(wrapped.size(), null));
         this.transformer = transformer;
         this.producer = producer;
     }
@@ -24,34 +24,74 @@ public class TransformingList<T, S> extends AbstractList<T> implements Transform
 
     @Override
     public T get(int index) {
-        return transformer.apply(wrapped.get(index));
+        return transform(index, wrapped.get(index));
     }
 
     @Override
     public int size() {
-        return wrapped.size();
+        return TransformingCollection.super.size();
     }
 
+    @Override
+    public boolean remove(Object o) {
+        return TransformingCollection.super.remove(o);
+    }
 
     @Override
     public T set(int i, T toSet) {
-        S newObject = wrapped.set(i, producer.apply(toSet));
-        return transformer.apply(newObject);
+        S newObject = wrapped.set(i, produce(toSet));
+        return transform(i, newObject);
     }
 
     @Override
     public void add(int i, T toAdd) {
-        wrapped.add(i, producer.apply(toAdd));
+        transformed.add(i, null);
+        wrapped.add(i, produce(toAdd));
     }
 
     @Override
     public T remove(int i) {
         S removed = wrapped.remove(i);
-        return transformer.apply(removed);
+        transformed.remove(i);
+        return transform(removed);
+    }
+
+    @Override
+    public T transform(S entry) {
+        return transformer.apply(entry);
+    }
+
+    @Override
+    public T transform(int i, S entry) {
+        Optional<T> trans = transformed.get(i);
+        if (trans == null) {
+            trans = Optional.ofNullable(transformer.apply(entry));
+            transformed.set(i, trans);
+        }
+        return trans.orElse(null);
+    }
+
+    @Override
+    public S produce(T entry) {
+        return producer.apply(entry);
+
+    }
+
+    @Override
+    public List<S> newWrap() {
+        return new ArrayList<S>();
+
+    }
+
+    @Override
+    public List<T> newFiltered() {
+        return new ArrayList<T>();
+
     }
 
     @Override
     public List<S> unwrap() {
         return wrapped;
     }
+
 }
--- vpro-shared-util/src/main/java/nl/vpro/util/TransformingSortedSet.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/TransformingSortedSet.java
@@ -8,8 +8,9 @@ import java.util.function.Predicate;
  * @author Michiel Meeuwissen
  * @since 4.3
  */
-public class TransformingSortedSet<T, S> extends AbstractSet<T> implements SortedSet<T>, TransformingCollection<T, S> {
+public class TransformingSortedSet<T, S> extends AbstractSet<T> implements SortedSet<T>, TransformingCollection<T, S, SortedSet<T>, SortedSet<S>> {
     private final SortedSet<S> wrapped;
+    private final Map<S, Optional<T>> transformed;
     private final Function<S, T> transformer;
     private final Function<T, S> producer;
 
@@ -20,10 +21,9 @@ public class TransformingSortedSet<T, S> extends AbstractSet<T> implements Sorte
         }
     };
 
-
-
     public TransformingSortedSet(SortedSet<S> wrapped, Function<S, T> transformer, Function<T, S> producer) {
         this.wrapped = wrapped;
+        transformed = new HashMap<>(wrapped.size());
         this.transformer = transformer;
         this.producer = producer;
     }
@@ -32,33 +32,22 @@ public class TransformingSortedSet<T, S> extends AbstractSet<T> implements Sorte
 
     @Override
     public Iterator<T> iterator() {
-        final Iterator<S> i = wrapped.iterator();
-        return new Iterator<T>() {
-            @Override
-            public boolean hasNext() {
-                return i.hasNext();
-            }
-
-            @Override
-            public T next() {
-                return transformer.apply(i.next());
-            }
-            @Override
-            public void remove() {
-                i.remove();
-            }
-        };
+        return TransformingCollection.super.iterator();
     }
 
     @Override
     public int size() {
-        return wrapped.size();
+        return TransformingCollection.super.size();
     }
-
 
     @Override
     public boolean add(T toAdd) {
-        return wrapped.add(producer.apply(toAdd));
+        return TransformingCollection.super.add(toAdd);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return TransformingCollection.super.remove(o);
     }
 
     @Override
@@ -68,28 +57,28 @@ public class TransformingSortedSet<T, S> extends AbstractSet<T> implements Sorte
 
     @Override
     public SortedSet<T> subSet(T fromElement, T toElement) {
-        return new TransformingSortedSet<>(wrapped.subSet(producer.apply(fromElement), producer.apply(toElement)), transformer, producer);
+        return new TransformingSortedSet<>(wrapped.subSet(produce(fromElement), produce(toElement)), transformer, producer);
 
     }
 
     @Override
     public SortedSet<T> headSet(T toElement) {
-        return new TransformingSortedSet<>(wrapped.headSet(producer.apply(toElement)), transformer, producer);
+        return new TransformingSortedSet<>(wrapped.headSet(produce(toElement)), transformer, producer);
     }
 
     @Override
     public SortedSet<T> tailSet(T fromElement) {
-        return new TransformingSortedSet<>(wrapped.tailSet(producer.apply(fromElement)), transformer, producer);
+        return new TransformingSortedSet<>(wrapped.tailSet(produce(fromElement)), transformer, producer);
     }
 
     @Override
     public T first() {
-        return transformer.apply(wrapped.first());
+        return transform(wrapped.first());
     }
 
     @Override
     public T last() {
-        return transformer.apply(wrapped.last());
+        return transform(wrapped.last());
     }
 
     @Override
@@ -97,18 +86,41 @@ public class TransformingSortedSet<T, S> extends AbstractSet<T> implements Sorte
         return wrapped;
     }
 
-    public SortedSet<T> filter() {
-        SortedSet<T> result = new TreeSet<>();
-        result.addAll(this);
-        return result;
+
+
+    @Override
+    public T transform(S entry) {
+        return transformed.computeIfAbsent(entry, e -> Optional.ofNullable(transformer.apply(e))).orElse(null);
     }
+
+    @Override
+    public S produce(T entry) {
+        return producer.apply(entry);
+
+    }
+
+    @Override
+    public SortedSet<S> newWrap() {
+        return new TreeSet<S>();
+
+    }
+
+    @Override
+    public SortedSet<T> newFiltered() {
+        return new TreeSet<T>();
+
+    }
+
+
     public SortedSet<T> filter(Predicate<S> s) {
         SortedSet<T> result = new TreeSet<>();
-        for (S in : wrapped) {
+        for (S in : unwrap()) {
             if (s.test(in)) {
-                result.add(transformer.apply(in));
+                result.add(transform(in));
             }
         }
         return result;
     }
+
+
 }
