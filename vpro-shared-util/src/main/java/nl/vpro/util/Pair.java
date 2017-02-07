ae6c7a3233db174af48b431e4482d1ea29976f29
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-02-07 10:00:31 +0000

    Using builder/toString.

--- vpro-shared-util/src/main/java/nl/vpro/util/Pair.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/Pair.java
@@ -1,6 +1,7 @@
 package nl.vpro.util;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -9,8 +10,17 @@ import lombok.Data;
  */
 @Data
 @AllArgsConstructor(staticName = "of")
+@Builder
 public class Pair<F, S> {
     private F first;
+    private String firstDescription = "first";
+
     private S second;
+    private String secondDescription = "second";
+
+    @Override
+    public String toString() {
+        return "(" + firstDescription + "=" + first + ", " + secondDescription + "=" + second + ")";
+    }
 }
 
