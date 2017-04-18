58a0cc8db4db5fc4d77c695adf3319bc36b860bc
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-18 18:37:20 +0000

    ResortedSortedSet was not entirely well behaved. Fixed that.

--- vpro-shared-util/src/test/java/nl/vpro/util/ResortedSortedSetTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/ResortedSortedSetTest.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 /**
  * @author Michiel Meeuwissen
@@ -35,6 +36,68 @@ public class ResortedSortedSetTest {
 
         }
 
+
+    }
+
+
+    @Test
+    public void testAdd() {
+        Collection<String> test = new ArrayList<>();
+        test.addAll(Arrays.asList("b", "a"));
+
+        SortedSet<String> resorted = new ResortedSortedSet<>(test, String::compareTo);
+
+        assertThat(resorted.contains("a")).isTrue();
+        assertThat(resorted.add("a")).isFalse();
+        assertThat(resorted).containsExactly("a", "b");
+
+    }
+
+    @Test
+    public void testRemove() {
+        Collection<String> test = new ArrayList<>();
+        test.addAll(Arrays.asList("b", "a"));
+
+        SortedSet<String> resorted = new ResortedSortedSet<>(test, String::compareTo);
+
+        assertThat(resorted.contains("a")).isTrue();
+        assertThat(resorted.remove("a")).isTrue();
+        assertThat(resorted).containsExactly("b");
+
+    }
+
+    @Test
+    public void testIteratorRemove() {
+        Collection<String> test = new ArrayList<>();
+        test.addAll(Arrays.asList("b", "a"));
+
+        SortedSet<String> resorted = new ResortedSortedSet<>(test, String::compareTo);
+
+        Iterator<String> i = resorted.iterator();
+        String a = i.next();
+        assertThat(a).isEqualTo("a");
+        i.remove();
+
+        assertThat(resorted).containsExactly("b");
+
+        try {
+            i.remove();
+            fail("Should have thrown IllegalStateEcxception");
+        } catch (IllegalStateException ignore) {
+
+        }
+        String b = i.next();
+        assertThat(b).isEqualTo("b");
+        i.remove();
+        assertThat(resorted).isEmpty();
+        assertThat(i.hasNext()).isFalse();
+
+        try {
+            i.next();
+            fail("Should have thrown NoSuchElementException");
+        } catch (NoSuchElementException ignore) {
+
+        }
 
     }
 }
