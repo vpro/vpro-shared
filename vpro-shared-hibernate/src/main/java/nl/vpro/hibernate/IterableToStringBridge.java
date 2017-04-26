527d5f5b79750cf1edf12306d947ed8556f22374
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-26 19:35:02 +0000

    MSE-3666

--- vpro-shared-hibernate/src/main/java/nl/vpro/hibernate/IterableToStringBridge.java
+++ vpro-shared-hibernate/src/main/java/nl/vpro/hibernate/IterableToStringBridge.java
@@ -1,5 +1,7 @@
 package nl.vpro.hibernate;
 
+import java.util.Objects;
+
 import org.apache.lucene.document.Document;
 import org.hibernate.search.bridge.FieldBridge;
 import org.hibernate.search.bridge.LuceneOptions;
@@ -8,16 +10,19 @@ import org.hibernate.search.bridge.LuceneOptions;
  * @author Michiel Meeuwissen
  * @since 1.10
  */
-public class IterableToStringBridge implements FieldBridge {
+public class IterableToStringBridge<T> implements FieldBridge {
 
     @Override
     public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
         if (value != null) {
-            for (Object object : (Iterable<?>) value) {
+            for (T object : (Iterable<T>) value) {
                 if (object != null) {
-                    luceneOptions.addFieldToDocument(name, object.toString(), document);
+                    luceneOptions.addFieldToDocument(name, toString(object), document);
                 }
             }
         }
+    }
+    protected String toString(T object) {
+        return Objects.toString(object, null);
     }
 }
