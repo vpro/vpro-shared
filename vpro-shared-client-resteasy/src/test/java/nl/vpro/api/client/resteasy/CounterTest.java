a1ffdbe40a151fa8ae566c633eebba71e0188f3a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-28 12:26:18 +0000

    Made possible to register more than one duration per event.

--- vpro-shared-client-resteasy/src/test/java/nl/vpro/api/client/resteasy/CounterTest.java
+++ vpro-shared-client-resteasy/src/test/java/nl/vpro/api/client/resteasy/CounterTest.java
@@ -32,4 +32,24 @@ public class CounterTest {
 
     }
 
+
+    @Test
+    public void testMultipleDuration() throws InterruptedException {
+        Counter counter = Counter.builder()
+            .bucketCount(10)
+            .countWindow(Duration.ofSeconds(10))
+            .build();
+
+        counter.eventAndDuration(Duration.ofMillis(500), Duration.ofMillis(200));
+        Thread.sleep(500);
+
+        // 1 event in .5 seconds is about 120 /min.
+        assertThat(counter.getRate()).isCloseTo(120, Percentage.withPercentage(30));
+
+        assertThat(counter.getAverageDuration()).isEqualTo("PT0.5S");
+
+        assertThat(counter.getDurationStatistics(1).getCombined().getAverage()).isCloseTo(200.0, Percentage.withPercentage(1));
+
+
+    }
 }
