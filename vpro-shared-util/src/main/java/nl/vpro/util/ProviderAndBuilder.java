0783da85ef4c16117f907ddd7607dfc173524d96
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-04-28 15:21:52 +0000

    Provider stuff.

--- vpro-shared-util/src/main/java/nl/vpro/util/ProviderAndBuilder.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/ProviderAndBuilder.java
@@ -1,7 +1,17 @@
 package nl.vpro.util;
 
-import java.lang.reflect.*;
+import lombok.AllArgsConstructor;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import javax.inject.Provider;
 
@@ -13,6 +23,14 @@ import javax.inject.Provider;
  * @since 1.69
  */
 public class ProviderAndBuilder {
+
+    private static final List<Converter<?, ?>> CONVERTERS = new ArrayList<>();
+    static {
+        CONVERTERS.add(new Converter<>(s -> s, (o, c) -> c.isInstance(o)));
+        CONVERTERS.add(new Converter<String, Duration>(
+            s -> TimeUtils.parseDuration(s).orElse(null),
+            (o, c) -> (String.class.isInstance(o) && c.equals(Duration.class))));
+    }
 
 
     public static <T, S> S fill(Provider<T> provider, S builder) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
@@ -26,8 +44,16 @@ public class ProviderAndBuilder {
                 fieldType = Class.forName(((ParameterizedType) f.getGenericType()).getActualTypeArguments()[0].getTypeName());
                 fieldValue = fieldValue == null ? null : ((Optional) fieldValue).orElse(null);
             }
-            Method m = builderClass.getMethod(f.getName(), fieldType);
-            m.invoke(builder, fieldValue);
+            MethodAndConverter methodAndConverter = find
+            while(true) {
+                try {
+                    Method m = builderClass.getMethod(f.getName(), fieldType);
+                    m.invoke(builder, fieldValue);
+                    break;
+                } catch (NoSuchMethodException nsm) {
+
+                }
+            }
 
         }
 
@@ -41,6 +67,36 @@ public class ProviderAndBuilder {
             return fill(provider, builder);
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private MethodAndConverter findMethod(Class clazz, String name, Class fieldType) throws NoSuchMethodException {
+        Method m = clazz.getMethod(name, fieldType);
+        return new MethodAndConverter(m, CONVERTERS.get(0));
+
+    }
+
+    @AllArgsConstructor
+    private static class MethodAndConverter {
+        final Method method;
+        final Converter<?, ?> convertor;
+    }
+
+    @AllArgsConstructor
+    public static class Converter<F, T> implements Function<F, T>, BiPredicate<Object, Class<?>> {
+        private final Function<F, T> converter;
+        private final BiPredicate<Object, Class<?>> applies;
+
+        @Override
+        public T apply(F f) {
+            return converter.apply(f);
+
+        }
+
+        @Override
+        public boolean test(Object o, Class<?> toClass) {
+            return applies.test(o, toClass);
+
         }
     }
 }
