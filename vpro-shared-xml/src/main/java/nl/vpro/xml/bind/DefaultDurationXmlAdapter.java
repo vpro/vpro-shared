f13587b893d619fefceab6b443fd27b97a03df56
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-05-26 06:22:21 +0000

    ported from branch

--- vpro-shared-xml/src/main/java/nl/vpro/xml/bind/DefaultDurationXmlAdapter.java
+++ vpro-shared-xml/src/main/java/nl/vpro/xml/bind/DefaultDurationXmlAdapter.java
@@ -1,8 +1,13 @@
 package nl.vpro.xml.bind;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.Duration;
+import java.time.format.DateTimeParseException;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import nl.vpro.util.TimeUtils;
 
 /**
  * This simpler version of {@link DurationXmlAdapter} simply uses {@link Duration#toString()}
@@ -10,11 +15,17 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  * @author Michiel Meeuwissen
  * @since 0.36
  */
+@Slf4j
 public class DefaultDurationXmlAdapter extends XmlAdapter<String, Duration> {
 
     @Override
     public Duration unmarshal(String stringValue) {
-        return stringValue != null ? Duration.parse(stringValue) : null;
+        try {
+            return stringValue != null ? Duration.parse(stringValue) : null;
+        } catch (DateTimeParseException dtpe) {
+            return Duration.ofMillis(TimeUtils.parse(stringValue)
+                .orElseThrow(() -> new IllegalArgumentException(stringValue + " cannot be parse to duration")).toEpochMilli());
+        }
     }
 
     @Override
