1d373d3fa6944b02c627b2b319752b84950b4dc8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-07 14:31:15 +0000

    cleaned up a bit

--- vpro-shared-logging/src/test/java/nl/vpro/logging/LoggerHelperTest.java
+++ vpro-shared-logging/src/test/java/nl/vpro/logging/LoggerHelperTest.java
@@ -14,7 +14,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 public class LoggerHelperTest {
     private static final Logger log = LoggerFactory.getLogger(LoggerHelperTest.class);
