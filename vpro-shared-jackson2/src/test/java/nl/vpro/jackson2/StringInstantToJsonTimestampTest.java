61dee0c1c6c59af752ae8150f26b72f663b91828
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-24 16:27:20 +0000

    Test case failures.

--- vpro-shared-jackson2/src/test/java/nl/vpro/jackson2/StringInstantToJsonTimestampTest.java
+++ vpro-shared-jackson2/src/test/java/nl/vpro/jackson2/StringInstantToJsonTimestampTest.java
@@ -1,8 +1,12 @@
 package nl.vpro.jackson2;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 /**
  * @author Michiel Meeuwissen
@@ -14,10 +18,14 @@ public class StringInstantToJsonTimestampTest {
     public void testOk() {
         Instant instant = StringInstantToJsonTimestamp.parseDateTime("2017-05-24T16:30:00+02:00");
     }
-    @Test(expected = IllegalArgumentException.class)
-    // Of course, that is a very odd timezone
+
+    //@Test(expected = IllegalArgumentException.class)
+    @Test // natty does succeed in parsing this
     public void test() {
-        Instant instant = StringInstantToJsonTimestamp.parseDateTime("0737-05-22T14:35:55+00:19:32");
+        Instant instant = StringInstantToJsonTimestamp.parseDateTime("0737-05-22T14:35:55+00:19:32");     // Of course, that is a very odd timezone
+
+        assertThat(instant).isEqualTo(LocalDateTime.of(737, 5, 26, 14, 16, 55).atZone(ZoneId.of("UTC")).toInstant());
+
     }
 
 
