5cfcdeaf2b5a09ec6529dd889ca5c3d06543fd28
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-13 10:08:28 +0000

    NPA-334

--- vpro-shared-elasticsearch5/src/main/java/nl/vpro/elasticsearch/IndexHelper.java
+++ vpro-shared-elasticsearch5/src/main/java/nl/vpro/elasticsearch/IndexHelper.java
@@ -10,11 +10,13 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.io.IOUtils;
+import org.elasticsearch.ResourceAlreadyExistsException;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
 import org.elasticsearch.action.admin.indices.refresh.RefreshResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.NoNodeAvailableException;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 
@@ -56,25 +58,27 @@ public class IndexHelper {
     }
 
     public  void createIndex() throws ExecutionException, InterruptedException, IOException {
-        CreateIndexResponse response = client().admin().indices().prepareCreate(indexName).setSettings(read(settings)).execute().actionGet();
-        if (response.isAcknowledged()) {
-            log.info("Created index {}", indexName);
-        } else {
-            log.warn("Could not create index {}", indexName);
+
+        try {
+            CreateIndexRequestBuilder createIndexRequestBuilder = client().admin().indices()
+                .prepareCreate(indexName)
+                .setSettings(settings, XContentType.JSON);
+            for (Map.Entry<String, String> e : mappings.entrySet()) {
+                createIndexRequestBuilder.addMapping(e.getKey(), e.getValue(), XContentType.JSON);
+            }
+            CreateIndexResponse response = createIndexRequestBuilder.execute()
+                .actionGet();
+            if (response.isAcknowledged()) {
+                log.info("Created index {}", indexName);
+            } else {
+                log.warn("Could not create index {}", indexName);
+            }
+        } catch (ResourceAlreadyExistsException e) {
+            log.info("Index exists");
         }
-        putMappings();
+        CreateIndexResponse response = client().admin().indices().prepareCreate(indexName).setSettings(read(settings), XContentType.JSON).execute().actionGet();
     }
 
-    public void putMappings() throws ExecutionException, InterruptedException, IOException {
-        for (Map.Entry<String, String> e : mappings.entrySet()) {
-            PutMappingResponse a = client().admin().indices().preparePutMapping(indexName).setType(e.getKey()).setSource(read(e.getValue())).execute().actionGet();
-            if (a.isAcknowledged()) {
-                log.info("Put mapping {}/{}", indexName, e.getKey());
-            } else {
-                log.warn("Could not put mapping {}/", indexName, e.getKey());
-            }
-        }
-    }
 
     public void prepareIndex() {
         try {
@@ -88,13 +92,6 @@ public class IndexHelper {
                     log.error(e.getMessage() + c);
                 }
             } else {
-                try {
-                    putMappings();
-                } catch (Exception e) {
-                    String c = (e.getCause() != null ? (" " + e.getCause().getMessage()) : "");
-                    log.error(e.getMessage() + c);
-
-                }
                 log.info("Found {} objects in '{}' of {}", count(), indexName, client);
             }
         } catch( NoNodeAvailableException noNodeAvailableException) {
