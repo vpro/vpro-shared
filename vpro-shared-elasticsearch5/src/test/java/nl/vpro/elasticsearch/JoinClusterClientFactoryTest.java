242b24ee2ac322c921420bea26eb56c628ca3a5a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-09-13 09:49:47 +0000

    NPA-334

--- vpro-shared-elasticsearch5/src/test/java/nl/vpro/elasticsearch/JoinClusterClientFactoryTest.java
+++ vpro-shared-elasticsearch5/src/test/java/nl/vpro/elasticsearch/JoinClusterClientFactoryTest.java
@@ -1,5 +1,7 @@
 package nl.vpro.elasticsearch;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.elasticsearch.client.Client;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -7,10 +9,11 @@ import org.junit.Test;
 import nl.vpro.util.UrlProvider;
 
 
-@Ignore("required running es")
+@Ignore("requires running es")
+@Slf4j
 public class JoinClusterClientFactoryTest {
 
-    int port = 9202;
+    int port = 9300;
     //String clusterName = "elasticsearch_acceptatie";
     //String clusterName = "poms10aas";
     String clusterName = "elasticsearch";
@@ -19,8 +22,9 @@ public class JoinClusterClientFactoryTest {
     public void joinUnicast() {
         JoinClusterClientFactory factory = new JoinClusterClientFactory();
         factory.setClusterName(clusterName);
-        factory.setUnicastHosts("localhost[" + port + "]");
+        factory.setUnicastHosts("localhost:" + port);
         Client client = factory.client("media");
+        log.info("{}", client.prepareSearch().get().getHits().getTotalHits());
         //System.out.println(client.prepareCount("test").get().getCount());
     }
 
