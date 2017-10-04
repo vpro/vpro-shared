83c62c0000205757385837d6d0708ab59ea79a0f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-10-04 15:43:46 +0000

    NPA-334

--- vpro-shared-ektorp/src/main/java/nl/vpro/ektorp/PublisherJackson2MapperFactory.java
+++ vpro-shared-ektorp/src/main/java/nl/vpro/ektorp/PublisherJackson2MapperFactory.java
@@ -6,24 +6,23 @@ package nl.vpro.ektorp;
 
 import org.ektorp.CouchDbConnector;
 import org.ektorp.impl.ObjectMapperFactory;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import nl.vpro.jackson2.Jackson2Mapper;
 
 /**
- * @author Roelof Jan Koekoek
- * @since 3.0
+ * @author Michiel Meeuwissen
+ * @since 5.5
  */
-public class Jackson2MapperFactory implements ObjectMapperFactory {
+public class PublisherJackson2MapperFactory implements ObjectMapperFactory {
 
     @Override
     public ObjectMapper createObjectMapper() {
-        return Jackson2Mapper.getLenientInstance();
+        return Jackson2Mapper.getPublisherInstance();
     }
 
     @Override
     public ObjectMapper createObjectMapper(CouchDbConnector connector) {
-        return Jackson2Mapper.getLenientInstance();
+        return Jackson2Mapper.getPublisherInstance();
     }
 }
