70cd56a2046d7708ad9746c29df8f53449244170
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-11-19 18:31:47 +0000

    javadoc.

--- vpro-shared-configuration/src/main/java/nl/vpro/util/ShowCatalinaBase.java
+++ vpro-shared-configuration/src/main/java/nl/vpro/util/ShowCatalinaBase.java
@@ -3,6 +3,8 @@ package nl.vpro.util;
 import org.apache.commons.lang.StringUtils;
 
 /**
+ * Sometimes it is hard to know what currently is 'catalina.base' (intellij...) what we often are
+ * interested in, because logs are to be found relative to this.
  * @author Michiel Meeuwissen
  * @since 1.74
  */
