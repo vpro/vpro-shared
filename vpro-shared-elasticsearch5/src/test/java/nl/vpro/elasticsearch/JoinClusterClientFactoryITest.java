0eb52cf3720dfdb7c50f34227e1151fafa2aacef
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-11-29 15:22:30 +0000

    API-316

--- vpro-shared-elasticsearch5/src/test/java/nl/vpro/elasticsearch/JoinClusterClientFactoryITest.java
+++ vpro-shared-elasticsearch5/src/test/java/nl/vpro/elasticsearch/JoinClusterClientFactoryITest.java
@@ -2,58 +2,47 @@ package nl.vpro.elasticsearch;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.elasticsearch.client.Client;
-import org.junit.Test;
+import java.util.concurrent.ExecutionException;
 
-import nl.vpro.util.UrlProvider;
+import org.elasticsearch.client.Client;
+import org.junit.Ignore;
+import org.junit.Test;
 
 
 @Slf4j
 public class JoinClusterClientFactoryITest {
 
+
+    String clientName = JoinClusterClientFactoryITest.class.getName();
     int port = 9300;
     //String clusterName = "elasticsearch_acceptatie";
     //String clusterName = "poms10aas";
     String clusterName = System.getProperty("integ.cluster.name", "elasticsearch");
 
     @Test
-    public void joinUnicast() {
+    public void joinUnicast() throws ExecutionException, InterruptedException {
         JoinClusterClientFactory factory = new JoinClusterClientFactory();
         factory.setClusterName(clusterName);
         factory.setUnicastHosts("localhost:" + port);
-        Client client = factory.client("media");
+        factory.setNodeName("Test-" + System.currentTimeMillis());
+        Client client = factory.client(clientName);
         log.info("{}", client.prepareSearch().get().getHits().getTotalHits());
-        //System.out.println(client.prepareCount("test").get().getCount());
+        log.info("{}", client.admin().cluster().prepareClusterStats().execute().get());
     }
 
 
     @Test
-    public void joinMulticast() {
+    @Ignore("Doesn't work?")
+    public void joinMulticast() throws ExecutionException, InterruptedException {
         JoinClusterClientFactory factory = new JoinClusterClientFactory();
         factory.setClusterName(clusterName);
         factory.setUnicastHosts("");
-        Client client = factory.client("media");
+        factory.setNodeName("Test-" + System.currentTimeMillis());
+        Client client = factory.client(clientName);
+        log.info("{}", client.prepareSearch().get().getHits().getTotalHits());
+        log.info("{}", client.admin().cluster().prepareClusterStats().execute().get());
         //System.out.println(client.prepareCount("media").get().getCount());
     }
 
 
-    @Test
-    public void join2() {
-        TransportClientFactory factory = new TransportClientFactory();
-        factory.setClusterName(clusterName);
-        factory.setTransportAddresses(new UrlProvider("localhost", port));
-
-        Client client = factory.client("test");
-
-        //System.out.println(client.prepareCount("test").get().getCount());
-    }
-
-
-    @Test
-    public void join3() {
-        TransportClientFactory factory = new TransportClientFactory();
-        factory.setTransportAddresses(new UrlProvider("localhost", port));
-
-        Client client = factory.client("test");
-    }
 }
