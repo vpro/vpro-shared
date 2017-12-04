a424c6c8d6152348f90a40ee6ef8d5c9855fbc01
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-12-04 19:23:23 +0000

    Refactored ReflectionUtils/ConfigUtils a bit.

--- vpro-shared-util/src/main/java/nl/vpro/util/ReflectionUtils.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/ReflectionUtils.java
@@ -4,9 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.lang.reflect.*;
+import java.time.Duration;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.LocaleUtils;
 
 /**
  * @author Michiel Meeuwissen
@@ -19,68 +22,47 @@ public class ReflectionUtils {
     public static Function<String, String> IDENTITY = k -> k;
 
 
+    /**
+     * Sets a certain property value in an object using reflection
+     */
+
     public static void setProperty(Object instance, String key, Object value) {
         setProperty(instance, Arrays.asList(SETTER.apply(key), IDENTITY.apply(key)), value);
     }
 
-    public static <T> T configured(T instance, String... configFiles) {
-        return configured(null, instance, configFiles);
-    }
-
-    public static <T> T configuredInHome(T instance, String... configFiles) {
-        return configuredInHome(null, instance, configFiles);
-    }
-
-    public static <T> T configuredInHome(Env env, T instance, String... configFiles) {
-        return configured(
-            env,
-            instance,
-            ConfigUtils.getConfigFilesInHome(configFiles)
-        );
-    }
-
-    @Deprecated
-    public static String[] getConfigFilesInHome(String... configFiles) {
-        return ConfigUtils.getConfigFilesInHome(configFiles);
-    }
-
-    public static <T> T configured(Env env, T instance, String... configFiles) {
-        try {
-            Map<String, String> properties = ConfigUtils.getProperties(configFiles);
-            if (env == null) {
-                env = ConfigUtils.getEnv(properties);
-            }
-            return configured(env, instance, properties);
-        } catch (IOException ioe) {
-            throw new RuntimeException(ioe);
-        }
-    }
-
-
-    public static <T> T  configured(Env env, T instance, Map<String, String> properties, Collection<Function<String, String>> setterName) {
-        Map<String, String> filtered = ConfigUtils.filtered(env, null, properties);
-        log.debug("Configuring with {}", filtered);
-        filtered.forEach((k, v) -> ReflectionUtils.setProperty(instance,
+    /**
+     * Configure an instance using a map of properties.
+     * @param setterName How, given a property name the setter methods must be calculated.
+     */
+    public static <T> T  configured(T instance, Map<String, String> properties, Collection<Function<String, String>> setterName) {
+        log.debug("Configuring with {}", properties);
+        properties.forEach((k, v) -> setProperty(instance,
             setterName.stream().map(f -> f.apply(String.valueOf(k))).collect(Collectors.toList()), v));
         return instance;
     }
 
-    public static <T> T configured(Env env, T instance, Map<String, String> properties) {
-        return configured(env, instance, properties, Arrays.asList(SETTER, IDENTITY));
-    }
 
+    /**
+     * Defaulting version of {@link #configured(Object, Map, Collection)}. Using {@link #SETTER}, {@link #IDENTITY} for the setter discovery.
 
+     */
     public static <T> T configured(T instance, Map<String, String> properties) {
-        return configured(ConfigUtils.getEnv(properties), instance, properties, Arrays.asList(SETTER, IDENTITY));
+        return configured(instance, properties, Arrays.asList(SETTER, IDENTITY));
     }
 
-
-    public static <T> T configured(Env env, Class<T> clazz, Map<String, String> properties) {
+    /**
+     * We can also create the instance itself (supporting the Builder pattern of lombok)
+     * @param clazz
+     * @param properties
+     * @param <T>
+     * @return
+     */
+    public static <T> T configured(Class<T> clazz, Map<String, String> properties) {
         try {
             Method builderMethod = clazz.getDeclaredMethod("builder");
             if (Modifier.isStatic(builderMethod.getModifiers())) {
                 Object builder = builderMethod.invoke(null);
-                configured(env, builder, properties, Collections.singletonList(IDENTITY));
+                configured(builder, properties, Collections.singletonList(IDENTITY));
                 Method objectMethod = builder.getClass().getMethod("build");
                 return (T) objectMethod.invoke(builder);
             }
@@ -92,7 +74,7 @@ public class ReflectionUtils {
             Constructor<T> constructor = clazz.getConstructor();
             constructor.setAccessible(true);
             T object = constructor.newInstance();
-            configured(env, object, properties);
+            configured(object, properties);
             return object;
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
             log.error(e.getMessage(), e);
@@ -100,32 +82,27 @@ public class ReflectionUtils {
         }
     }
 
+
+    @Deprecated
     public static <T> T configured(Env env, Class<T> clazz, String... configFiles) {
-        try {
-            return configured(env, clazz, ConfigUtils.getProperties(configFiles));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+        return ConfigUtils.configured(env, clazz, configFiles);
 
-    public static <T> T configured(Class<T> clazz, Map<String, String> properties) {
-        return configured(ConfigUtils.getEnv(properties), clazz, properties);
-    }
-
-    public static <T> T configured(Class<T> clazz, String... configfiles) {
-        try {
-            Map<String, String> properties = ConfigUtils.getProperties(configfiles);
-            return configured(ConfigUtils.getEnv(properties), clazz, properties);
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
     }
 
 
     @Deprecated
+    public static void configured(Env env, Object instance, String... configFiles) {
+        ConfigUtils.getProperties();
+    }
+
+    @Deprecated
+    public static <T> T configured(Class<T> clazz, String... configfiles) {
+        return ConfigUtils.configured(clazz, configfiles);
+    }
+
+    @Deprecated
     public static Map<String, String> getProperties(String... configFiles) throws IOException {
-        return ConfigUtils.getProperties(new HashMap<>(), configFiles);
+        return ConfigUtils.getProperties(configFiles);
     }
 
     @Deprecated
@@ -135,57 +112,46 @@ public class ReflectionUtils {
 
     @Deprecated
     public static void substitute(Map<String, String> map) {
-        ConfigUtils.substitute(map);
+        ConfigUtils.substitute(map, map);
     }
 
+    @Deprecated
     public static Map<String, String> filtered(Env e, Map<String, String> properties) {
-        return filtered(e, null, properties);
+        return ConfigUtils.filtered(e, properties);
     }
 
+    @Deprecated
     public static Map<String, String> filtered(Env e, String prefix,  Map<String, String> properties) {
-        if (e == null) {
-            if (System.getProperty("env") != null) {
-                e = Env.valueOf(System.getProperty("env").toUpperCase());
-            }
-        }
-        final Env env = e;
-        log.debug("Filtering{}for {}", prefix == null ? "" : prefix + " ", e);
-        Map<String, String> result = new HashMap<>();
-        properties.forEach((key, value) -> {
-            String[] split = key.split("\\.", 3);
-            if (split.length == 1) {
-                String explicitValue = value;
-                if (env != null) {
-                    String envValue = properties.get(key + "." + env);
-                    if (envValue != null) {
-                        explicitValue = envValue;
-                    }
-                }
-                result.put(key, explicitValue);
-            }
-        });
-        properties.forEach((key, value) -> {
-            String[] split = key.split("\\.", 3);
-            if (split.length == 3) {
-                if (!Objects.equals(prefix, split[0])) {
-                    return;
-                }
-                split = Arrays.copyOfRange(split, 1, 3);
-            }
-            if (split.length > 1 ) {
-                if ((env == null && split[1].equals("test" ) && !result.containsKey(split[0])) || (env != null && split[1].toUpperCase().equals(env.name()))) {
-                    result.put(split[0], value);
-                }
-            }
+        return ConfigUtils.filtered(e, prefix, properties);
 
-        });
-        return result;
     }
 
 
-    private static Env getEnv(Map<String, String> properties) {
-        return Env.valueOf(System.getProperty("env", properties.getOrDefault("env", "test")).toUpperCase());
+    @Deprecated
+    public static <T> T configured(T instance, String... configFiles) {
+        return ConfigUtils.configured(instance, configFiles);
     }
+
+    @Deprecated
+    public static <T> T configuredInHome(T instance, String... configFiles) {
+        return ConfigUtils.configured(instance, ConfigUtils.getConfigFilesInHome(configFiles));
+    }
+
+    @Deprecated
+    public static <T> T configuredInHome(Env env, T instance, String... configFiles) {
+        return ConfigUtils.configuredInHome(env, instance, configFiles);
+    }
+    @Deprecated
+    public static <T> T configured(Env env, Class<T> clazz, Map<String, String> config) {
+        return ConfigUtils.configured(env, clazz, config);
+    }
+
+    @Deprecated
+    public static <T> T configured(Env env, T instance, Map<String, String> config) {
+        return ConfigUtils.configured(env, instance, config);
+    }
+
+
 
     private static boolean setProperty(Object instance, Collection<String> setterNames, Object value) {
         Method[] methods = instance.getClass().getMethods();
@@ -195,7 +161,7 @@ public class ReflectionUtils {
             if (setterNames.contains(m.getName()) && m.getParameterCount() == 1) {
                 try {
                     parameterClass = m.getParameters()[0];
-                    m.invoke(instance, ConfigUtils.convert(v, parameterClass));
+                    m.invoke(instance, convert(v, parameterClass));
                     log.debug("Set {}#{} to {} from config file", instance, m.getName(), v);
                     return true;
                 } catch (IllegalAccessException | InvocationTargetException e) {
@@ -210,5 +176,51 @@ public class ReflectionUtils {
         return false;
     }
 
+    static <T> Object convert(String o, Parameter parameter) {
+        return convert(o, parameter.getParameterizedType());
+    }
+
+    private static <T> Object convert(String o, Type parameterType) {
+        Class<?> parameterClass;
+        if (parameterType instanceof Class) {
+            parameterClass = (Class) parameterType;
+        } else if (parameterType instanceof ParameterizedType) {
+            parameterClass = (Class) ((ParameterizedType) parameterType).getRawType();
+        } else if (parameterType instanceof WildcardType) {
+            parameterClass = (Class) ((WildcardType) parameterType).getUpperBounds()[0];
+        } else {
+            throw new UnsupportedOperationException("Cannot convert " + o + " to " + parameterType);
+        }
+        if (String.class.isAssignableFrom(parameterClass)) {
+            return o;
+        } else if (boolean.class.equals(parameterClass) || parameterClass.isAssignableFrom(Boolean.class)) {
+            return Boolean.valueOf(o);
+        } else if (int.class.equals(parameterClass) || parameterClass.isAssignableFrom(Integer.class)) {
+            return Integer.valueOf(o);
+        } else if (long.class.equals(parameterClass) || parameterClass.isAssignableFrom(Long.class)) {
+            return Long.valueOf(o);
+        } else if (float.class.equals(parameterClass) || parameterClass.isAssignableFrom(Float.class)) {
+            return Float.valueOf(o);
+        } else if (double.class.equals(parameterClass) || parameterClass.isAssignableFrom(Double.class)) {
+            return Double.valueOf(o);
+        } else if (Enum.class.isAssignableFrom(parameterClass)) {
+            try {
+                return Enum.valueOf((Class<? extends Enum>) parameterClass, o);
+            } catch (IllegalArgumentException iae) {
+                return Enum.valueOf((Class<? extends Enum>) parameterClass, o.toUpperCase());
+            }
+        } else if (parameterClass.isAssignableFrom(Locale.class)) {
+            return LocaleUtils.toLocale(o);
+        } else if (parameterClass.isAssignableFrom(Duration.class)) {
+            return TimeUtils.parseDuration(o).orElse(null);
+        } else if (parameterClass.isAssignableFrom(List.class)) {
+            ParameterizedType parameterizedType = (ParameterizedType) parameterType;
+            return Arrays.stream(o.split("\\s*,\\s*"))
+                .map(s -> convert(s, parameterizedType.getActualTypeArguments()[0]))
+                .collect(Collectors.toList());
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
 
 }
