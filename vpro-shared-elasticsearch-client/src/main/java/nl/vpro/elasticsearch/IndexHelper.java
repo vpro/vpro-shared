4f05434d1a6ade3ecaaf8df96c948c894e2df465
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-12-20 13:39:48 +0000

    NPA-432

--- vpro-shared-elasticsearch-client/src/main/java/nl/vpro/elasticsearch/IndexHelper.java
+++ vpro-shared-elasticsearch-client/src/main/java/nl/vpro/elasticsearch/IndexHelper.java
@@ -7,11 +7,12 @@ import lombok.ToString;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.function.Supplier;
 
 import org.apache.commons.io.IOUtils;
@@ -19,12 +20,16 @@ import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.client.ResponseListener;
 import org.elasticsearch.client.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import nl.vpro.jackson2.Jackson2Mapper;
+import nl.vpro.util.Pair;
 
 /**
  * Some tools to automaticly create indices and put mappings and stuff.
@@ -41,6 +46,7 @@ public class IndexHelper {
     private Supplier<String> settings;
     private ESClientFactory clientFactory;
     private final Map<String, Supplier<String>> mappings = new HashMap<>();
+
 
     public static class Builder {
         private final Map<String, Supplier<String>> mappings = new HashMap<>();
@@ -134,7 +140,7 @@ public class IndexHelper {
         return clientFactory.client(IndexHelper.class.getName() + "." + indexNameSupplier.get());
     }
 
-    public  void createIndex() throws ExecutionException, InterruptedException, IOException {
+    public  void createIndex() throws IOException {
 
         if (indexNameSupplier == null){
             throw new IllegalStateException("No index name configured");
@@ -150,8 +156,7 @@ public class IndexHelper {
         HttpEntity entity = new NStringEntity(request.toString(), ContentType.APPLICATION_JSON);
 
         log.info("Creating index {} with mappings {}: {}", indexNameSupplier, mappings.keySet(), request.toString());
-        ObjectNode response = Jackson2Mapper.getLenientInstance().readerFor(ObjectNode.class).readValue(
-            client().performRequest("PUT", indexNameSupplier.get(), Collections.emptyMap(), entity).getEntity().getContent());
+        ObjectNode response = read(client().performRequest("PUT", indexNameSupplier.get(), Collections.emptyMap(), entity).getEntity());
 
 
         if (response.get("acknowledged").booleanValue()) {
@@ -194,13 +199,23 @@ public class IndexHelper {
     }
 
 
-    public void deleteIndex() throws ExecutionException, InterruptedException, IOException {
-        client().performRequest("DELETE", getIndexName());
+    public void deleteIndex()  {
+        try {
+            client().performRequest("DELETE", getIndexName());
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
-    public boolean refresh() throws ExecutionException, InterruptedException, IOException {
-        Response response = client().performRequest("GET", "_refresh");
-        return response != null;
+    public boolean refresh() {
+
+        try {
+            Response response = client().performRequest("GET", "_refresh");
+            return response != null;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return false;
+        }
     }
 
     public ObjectNode search(ObjectNode request) {
@@ -210,15 +225,144 @@ public class IndexHelper {
 
     public ObjectNode post(String path, ObjectNode request) {
         try {
-            return
-            Jackson2Mapper.getLenientInstance().readerFor(ObjectNode.class).readValue(
+            return read(
                 client().performRequest("POST", path, Collections.emptyMap(), new NStringEntity(request.toString(), ContentType.APPLICATION_JSON))
-                    .getEntity().getContent()
+                    .getEntity()
             );
         } catch (IOException e) {
             log.error(e.getMessage(), e);
             return null;
         }
+    }
+
+
+    public Future<ObjectNode> postAsync(String path, ObjectNode request) {
+        final CompletableFuture<ObjectNode> future = new CompletableFuture<>();
+        client().performRequestAsync("POST", path, Collections.emptyMap(), new NStringEntity(request.toString(), ContentType.APPLICATION_JSON), listen(future));
+        return future;
+    }
+
+    protected ResponseListener listen(final CompletableFuture<ObjectNode> future) {
+        return new ResponseListener() {
+            @Override
+            public void onSuccess(Response response) {
+                future.complete(read(response.getEntity()));
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                future.completeExceptionally(exception);
+            }
+        };
+    }
+
+    public ObjectNode index(String type, String id, Object o) {
+        return post(indexNameSupplier.get() + "/" + type + "/" + encode(id), Jackson2Mapper.getPublisherInstance().valueToTree(o));
+    }
+
+    public Future<ObjectNode> indexAsync(String type, String id, Object o) {
+        return postAsync(indexNameSupplier.get() + "/" + type + "/" + encode(id), Jackson2Mapper.getPublisherInstance().valueToTree(o));
+    }
+
+
+    public ObjectNode delete(String type, String id) {
+        try {
+            client().performRequest("DELETE", indexNameSupplier.get() + "/" + type + "/" + encode(id));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+
+    public Future<ObjectNode> deleteAsync(String type, String id) {
+        final CompletableFuture<ObjectNode> future = new CompletableFuture<>();
+
+        client().performRequestAsync("DELETE", indexNameSupplier.get() + "/" + type + "/" + encode(id), listen(future));
+        return future;
+    }
+
+    public Optional<JsonNode> get(String types, String id){
+        try {
+            Response response = client().performRequest("GET", indexNameSupplier.get() + "/" + types + "/" + encode(id));
+            return Optional.of(read(response.getEntity()));
+        } catch (ResponseException re) {
+            return Optional.empty();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    ObjectNode read(HttpEntity entity) {
+        try {
+            return Jackson2Mapper.getLenientInstance().readerFor(ObjectNode.class).readValue(entity.getContent());
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return null;
+        }
+    }
+
+    String encode(String id) {
+        try {
+            return URLEncoder.encode(id, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage(), e);
+            return id;
+        }
+    }
+
+    public Pair<JsonNode, JsonNode> indexRequest(String type, String id, Object o) {
+        ObjectNode actionLine = Jackson2Mapper.getInstance().createObjectNode();
+        ObjectNode index = actionLine.with("index");
+        index.put("_type", type);
+        index.put("_id", id);
+        index.put("_index", getIndexName());
+        return Pair.of(actionLine, Jackson2Mapper.getPublisherInstance().valueToTree(o));
+    }
+
+    public Pair<JsonNode, JsonNode> deleteRequest(String type, String id) {
+        ObjectNode actionLine = Jackson2Mapper.getInstance().createObjectNode();
+        ObjectNode index = actionLine.with("delete");
+        index.put("_type", type);
+        index.put("_id", id);
+        index.put("_index", getIndexName());
+        return Pair.of(actionLine, null);
+    }
+
+    public ObjectNode bulk(List<Pair<JsonNode, JsonNode>> request) {
+        try {
+            return read(
+                client().performRequest("POST", "_bulk", Collections.emptyMap(), bulkEntity(request))
+                    .getEntity()
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return null;
+        }
+    }
+
+
+    public Future<ObjectNode> bulkAsync(List<Pair<JsonNode, JsonNode>> request) {
+        final CompletableFuture<ObjectNode> future = new CompletableFuture<>();
+
+        client().performRequestAsync("POST", "_bulk", Collections.emptyMap(), bulkEntity(request), listen(future));
+        return future;
+    }
+
+    protected HttpEntity bulkEntity(List<Pair<JsonNode, JsonNode>> request) {
+        StringBuilder builder = new StringBuilder();
+        for (Pair<JsonNode, JsonNode> n : request) {
+            builder.append(n.getFirst());
+            builder.append("\n");
+            if (n.getSecond() != null) {
+                builder.append(n.getSecond());
+                builder.append("\n");
+            }
+        }
+        return new NStringEntity(builder.toString(), ContentType.APPLICATION_JSON);
     }
 
     public long count() {
