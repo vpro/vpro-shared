7db572bf304637b5de39224d645142f880a5f756
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-01-12 10:47:30 +0000

    PAGEPUB-14

---
+++ vpro-shared-elasticsearch-client/src/main/java/nl/vpro/elasticsearchclient/AsyncESClientFactory.java
@@ -0,0 +1,35 @@
+package nl.vpro.elasticsearchclient;
+
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
+
+import org.elasticsearch.client.RestClient;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.75
+ */
+public interface AsyncESClientFactory extends ESClientFactory {
+
+    /**
+     * Returns a client. The client may be cached by the factory.
+     */
+
+    @Override
+    default RestClient client(String logName) {
+        try {
+            return clientAsync(logName, rc -> {}).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns client, after that checks (e.g. on clustername) are performed asynchronously.
+     */
+    Future<RestClient> clientAsync(String logName, Consumer<RestClient> callback);
+
+
+}
