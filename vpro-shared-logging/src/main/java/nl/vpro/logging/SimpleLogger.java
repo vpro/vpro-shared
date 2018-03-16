58d73a69a407d4bf0cc4ae06bddabbb7b7cb1349
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-16 11:00:01 +0000

    Improved logging.

---
+++ vpro-shared-logging/src/main/java/nl/vpro/logging/SimpleLogger.java
@@ -0,0 +1,43 @@
+package nl.vpro.logging;
+
+import java.util.function.BiConsumer;
+
+import org.slf4j.event.Level;
+import org.slf4j.helpers.FormattingTuple;
+import org.slf4j.helpers.MessageFormatter;
+
+
+/**
+ * A very simplified Logger. This can be used as messaging system. It was made to use in conjuction with @{link ChainedSimpleLogger} to be able to programmatticly 'tee' logging.
+ *
+ * The goal was to log to slf4j but also send corresponding messages to users via websockets.
+ *
+ *
+ * @author Michiel Meeuwissen
+ * @since 1.76
+ */
+public interface  SimpleLogger extends  BiConsumer<Level, String> {
+
+
+     default void info(String format, Object... arg) {
+         FormattingTuple ft = MessageFormatter.arrayFormat(format, arg);
+         String message = ft.getMessage();
+         accept(Level.INFO, message);
+     }
+
+
+     default void error(String format, Object... arg) {
+         FormattingTuple ft = MessageFormatter.arrayFormat(format, arg);
+         String message = ft.getMessage();
+         accept(Level.ERROR, message);
+     }
+
+    @Override
+    default void accept(Level level, String message) {
+        accept(level, message, null);
+    }
+
+    void accept(Level level, String message, Throwable t);
+
+
+}
