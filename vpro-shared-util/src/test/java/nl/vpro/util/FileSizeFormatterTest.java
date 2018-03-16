c4cf5265807368b0090144ebf8cbb9bbb8ffcdea
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-03-16 16:05:27 +0000

    Details.

---
+++ vpro-shared-util/src/test/java/nl/vpro/util/FileSizeFormatterTest.java
@@ -0,0 +1,32 @@
+package nl.vpro.util;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.76
+ */
+public class FileSizeFormatterTest {
+
+    @Test
+    public void mebi() {
+        FileSizeFormatter formatter = FileSizeFormatter.builder()
+            .build();
+        assertThat(formatter.format(1000L)).isEqualTo("1000 B");
+        assertThat(formatter.format(221400200L)).isEqualTo("211 MiB");
+
+    }
+
+     @Test
+    public void si() {
+        FileSizeFormatter formatter = FileSizeFormatter.builder()
+            .mebi(false)
+            .build();
+        assertThat(formatter.format(1000L)).isEqualTo("1000 B");
+        assertThat(formatter.format(221400200L)).isEqualTo("221 MB");
+
+    }
+}
