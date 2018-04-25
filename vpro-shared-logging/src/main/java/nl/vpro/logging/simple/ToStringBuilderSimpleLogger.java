8d6fc4da0d10d85cad2e8243444df71e94e71c37
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-04-25 20:08:06 +0000

    Limit the size of the StringBuilder.

--- vpro-shared-logging/src/main/java/nl/vpro/logging/simple/ToStringBuilderSimpleLogger.java
+++ vpro-shared-logging/src/main/java/nl/vpro/logging/simple/ToStringBuilderSimpleLogger.java
@@ -2,6 +2,7 @@ package nl.vpro.logging.simple;
 
 import lombok.Getter;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.event.Level;
 
@@ -11,26 +12,55 @@ import org.slf4j.event.Level;
  */
 public class ToStringBuilderSimpleLogger implements SimpleLogger {
 
+    private static final String TRUNC = "...\n";
+
     @Getter
     final StringBuilder stringBuilder;
 
-    public ToStringBuilderSimpleLogger(StringBuilder stringBuilder) {
-        this.stringBuilder = stringBuilder;
+    @Getter
+    final long maxLength;
+    private long count = 0;
+    private boolean truncated = false;
+
+    @lombok.Builder
+    private ToStringBuilderSimpleLogger(
+        StringBuilder stringBuilder,
+        Long maxLength) {
+        this.stringBuilder = stringBuilder == null ? new StringBuilder() : stringBuilder;
+        this.maxLength = maxLength == null ? 10000L : maxLength;
     }
 
     public ToStringBuilderSimpleLogger() {
-        this(new StringBuilder());
+        this(null, null);
     }
 
     @Override
     public void accept(Level level, String message, Throwable t) {
+
         if (stringBuilder.length() > 0) {
             stringBuilder.append('\n');
+            count++;
         }
         stringBuilder.append(level.name()).append(" ").append(message);
         if (t != null) {
             stringBuilder.append('\n');
-            stringBuilder.append(ExceptionUtils.getStackTrace(t));
+            count++;
+            String stackTrace = ExceptionUtils.getStackTrace(t);
+            count += StringUtils.countMatches(stackTrace, '\n');
+            stringBuilder.append(stackTrace);
+        }
+        trucatedIfNecessary();
+    }
+
+    private void trucatedIfNecessary() {
+        while (count >= maxLength) {
+            if (! truncated) {
+                stringBuilder.insert(0, TRUNC);
+                truncated = true;
+            }
+            int index = stringBuilder.indexOf("\n", TRUNC.length());
+            stringBuilder.delete(TRUNC.length(),  index + 1);
+            count--;
         }
     }
 }
