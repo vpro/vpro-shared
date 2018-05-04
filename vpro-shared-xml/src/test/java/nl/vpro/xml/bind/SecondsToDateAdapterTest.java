e95bd291a5cc184fb57201f91221147af036c3fe
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-04 11:39:14 +0000

    Moved to own module

---
+++ vpro-shared-xml/src/test/java/nl/vpro/xml/bind/SecondsToDateAdapterTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2013 All rights reserved
+ * VPRO The Netherlands
+ */
+package nl.vpro.xml.bind;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Roelof Jan Koekoek
+ * @since 2.1
+ */
+@SuppressWarnings("deprecation")
+public class SecondsToDateAdapterTest {
+
+    @Test
+    public void testUnmarshalOnWHoleNumbers() throws Exception {
+        Date date = new SecondsToDateAdapter().unmarshal("11");
+        assertThat(date.getTime()).isEqualTo(11000l);
+    }
+
+    @Test
+    public void testUnmarshalWithMillisRoundedToTens() throws Exception {
+        Date date = new SecondsToDateAdapter().unmarshal("10.5");
+        assertThat(date.getTime()).isEqualTo(10500l);
+    }
+
+    @Test
+    public void testUnmarshalWithMillisRoundedToHundreds() throws Exception {
+        Date date = new SecondsToDateAdapter().unmarshal("10.55");
+        assertThat(date.getTime()).isEqualTo(10550l);
+    }
+
+    @Test
+    public void testUnmarshalWithMillisFull() throws Exception {
+        Date date = new SecondsToDateAdapter().unmarshal("10.555");
+        assertThat(date.getTime()).isEqualTo(10555l);
+    }
+
+    @Test
+    public void testUnmarshalOmmitHigherPrecision() throws Exception {
+        Date date = new SecondsToDateAdapter().unmarshal("10.5558");
+        assertThat(date.getTime()).isEqualTo(10555l);
+    }
+
+    @Test
+    public void testUnmarshalOnLargeNumbers() throws Exception {
+        Date date = new SecondsToDateAdapter().unmarshal("7777777777777777.999111");
+        assertThat(date.getTime()).isEqualTo(7777777777777777999l);
+    }
+}
