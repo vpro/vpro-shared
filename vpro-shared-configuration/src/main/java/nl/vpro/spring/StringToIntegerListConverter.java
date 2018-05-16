223615dfaa008efe18c47e54e55cfceab7bc4a26
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-05-16 10:01:11 +0000

    Moved to vpro-shared-configuration.

---
+++ vpro-shared-configuration/src/main/java/nl/vpro/spring/StringToIntegerListConverter.java
@@ -0,0 +1,46 @@
+package nl.vpro.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @since 1.78
+ */
+public class StringToIntegerListConverter implements Converter<String, List<Integer>> {
+
+    public static final StringToIntegerListConverter INSTANCE = new StringToIntegerListConverter();
+
+    @Override
+    public List<Integer> convert(String string) {
+        List<Integer> result = new ArrayList<>();
+        for (String s : string.split("\\s*,\\s*")) {
+            String[] split =  s.split("-", 4);
+            if (split.length == 1) {
+                result.add(Integer.parseInt(split[0]));
+            } else {
+                int i = 0;
+                String first = split[i++];
+                if (first.isEmpty()) {
+                    first = "-" + split[i++];
+                }
+                if (split.length == i) {
+                    result.add(Integer.parseInt(first));
+                } else {
+                    String second = split[i++];
+                    if (second.isEmpty()) {
+                        second = "-" + split[i];
+                    }
+                    for (int j = Integer.parseInt(first); j <= Integer.parseInt(second); j++) {
+                        result.add(j);
+                    }
+                }
+            }
+        }
+        return result;
+
+    }
+}
