63efdea236114d0a523badbaff0c220b0ded4e11
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-12 12:54:30 +0000

    More exception utils.

--- vpro-shared-util/src/main/java/nl/vpro/util/ExceptionUtils.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/ExceptionUtils.java
@@ -1,6 +1,10 @@
 package nl.vpro.util;
 
+import lombok.Lombok;
+import lombok.SneakyThrows;
+
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
@@ -24,11 +28,42 @@ public class ExceptionUtils {
         return () -> {
             try {
                 return b.call();
-            } catch (RuntimeException rte) {
-                throw rte;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            } catch (Throwable t) {
+                throw Lombok.sneakyThrow(t);
             }
-     };
+        };
+    }
+
+
+    /**
+     * Wraps a {@link Callable} in a {@link Supplier}.
+     *
+     * You may need a supplier, but have code that throws an exception. Like this:
+     *
+     * {@code
+     *   service.download(file, ExceptionUtils.wrapException(() -> new FileOutputStream(destFile)))
+     * }
+     *
+     * The point is that otherwise this nice lambda gets ugly because of the try/catch block.
+     */
+    public static <T> Consumer<T> wrapConsumer(ThrowingConsumer<T> b) {
+        return b;
+    }
+
+
+
+
+    @FunctionalInterface
+    public interface ThrowingConsumer<T> extends Consumer<T> {
+
+        @Override
+        @SneakyThrows
+        default void accept(final T e) {
+            acceptWithException(e);
+        }
+
+        void acceptWithException(T e) throws Throwable;
+
 }
+
 }
