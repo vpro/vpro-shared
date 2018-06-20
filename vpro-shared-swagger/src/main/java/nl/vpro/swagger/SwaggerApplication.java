88c8ccf9af220e03e025ac400ca7c4a9c2bc99d0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-06-20 13:12:25 +0000

    Rolled back.

--- vpro-shared-swagger/src/main/java/nl/vpro/swagger/SwaggerApplication.java
+++ vpro-shared-swagger/src/main/java/nl/vpro/swagger/SwaggerApplication.java
@@ -23,8 +23,6 @@ import javax.ws.rs.core.Application;
 
 import com.google.common.collect.ImmutableMap;
 
-import nl.vpro.util.XTrustProvider;
-
 
 /**
  * Static helper to expose a dummy JAX-RS Application. Swagger needs such an application to expose the API docs for
@@ -37,7 +35,7 @@ import nl.vpro.util.XTrustProvider;
 public class SwaggerApplication extends Application {
     private static final Set<Object> singletons = new HashSet<>();
     static {
-        XTrustProvider.install();
+        //XTrustProvider.install();
         // swagger sucks a lot.
         //https://github.com/swagger-api/swagger-core/issues/1444
         setExternalTypes(ImmutableMap.<Class, PrimitiveType>builder()
