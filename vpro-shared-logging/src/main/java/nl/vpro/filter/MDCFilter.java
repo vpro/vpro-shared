69bc06fe5ebd791faca1684e131843bfb838bef8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-07-27 13:30:26 +0000

    Logging.

--- vpro-shared-logging/src/main/java/nl/vpro/filter/MDCFilter.java
+++ vpro-shared-logging/src/main/java/nl/vpro/filter/MDCFilter.java
@@ -13,6 +13,8 @@ import org.slf4j.MDC;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import nl.vpro.mdc.MDCConstants;
+
 /**
  * Puts a few things related to authentication and accountability (remote address) on the Mapped Diagnostic Context of SLF4J
  * so that they can easily be added to the log.
@@ -21,10 +23,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
  */
 public class  MDCFilter implements Filter {
 
-    public static final String USER_NAME = "userName";
-    public static final String ONBEHALFOF= "onBehalfOf";
-    public static final String REQUEST = "request";
-    public static final String REMOTE_ADDR= "remoteAddr";
+    @Deprecated
+    public static final String USER_NAME   = MDCConstants.USER_NAME;
+    @Deprecated
+    public static final String REQUEST     = MDCConstants.REQUEST;
+    @Deprecated
+    public static final String REMOTE_ADDR = MDCConstants.REMOTE_ADDR;
 
     boolean clear = false;
 
@@ -43,17 +47,17 @@ public class  MDCFilter implements Filter {
         HttpServletResponse response  = (HttpServletResponse) res;
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
-            MDC.put(USER_NAME, auth.getName());
+            MDC.put(MDCConstants.USER_NAME, auth.getName());
         }
         String query = request.getQueryString();
         String path = request.getRequestURI().substring(request.getContextPath().length());
-        MDC.put(REQUEST, request.getMethod() + " " + path + (StringUtils.isEmpty(query) ? "" : ("?" + query)));
+        MDC.put(MDCConstants.REQUEST, request.getMethod() + " " + path + (StringUtils.isEmpty(query) ? "" : ("?" + query)));
 
         String ipAddress = request.getHeader("X-FORWARDED-FOR");
         if (ipAddress == null) {
             ipAddress = request.getRemoteAddr();
         }
-        MDC.put(REMOTE_ADDR, ipAddress);
+        MDC.put(MDCConstants.REMOTE_ADDR, ipAddress);
         try {
             chain.doFilter(req, res);
         } finally {
@@ -63,9 +67,9 @@ public class  MDCFilter implements Filter {
             if (clear) {
                 MDC.clear();
             } else {
-                MDC.remove(USER_NAME);
-                MDC.remove(REQUEST);
-                MDC.remove(REMOTE_ADDR);
+                MDC.remove(MDCConstants.USER_NAME);
+                MDC.remove(MDCConstants.REQUEST);
+                MDC.remove(MDCConstants.REMOTE_ADDR);
             }
         }
     }
