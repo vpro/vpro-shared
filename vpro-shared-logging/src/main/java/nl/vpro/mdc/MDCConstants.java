9170d805905997ca84e7ad636e64f0c1a77a8e51
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-07-27 13:30:26 +0000

    Logging.

---
+++ vpro-shared-logging/src/main/java/nl/vpro/mdc/MDCConstants.java
@@ -0,0 +1,13 @@
+package nl.vpro.mdc;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.79
+ */
+public class MDCConstants {
+
+    public static final String USER_NAME = "userName";
+    public static final String ONBEHALFOF= "onBehalfOf";
+    public static final String REQUEST = "request";
+    public static final String REMOTE_ADDR= "remoteAddr";
+}
