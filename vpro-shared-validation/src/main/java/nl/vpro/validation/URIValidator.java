77ad225edd261bdfee80a6ecf17a8067748718f7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-08-10 10:53:54 +0000

    MSE-4133

--- vpro-shared-validation/src/main/java/nl/vpro/validation/URIValidator.java
+++ vpro-shared-validation/src/main/java/nl/vpro/validation/URIValidator.java
@@ -5,14 +5,20 @@
 package nl.vpro.validation;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class URIValidator implements ConstraintValidator<URI, String> {
+
+    URI annotation;
 
     @Override
     public void initialize(URI uri) {
+        this.annotation = uri;
     }
 
     @Override
@@ -22,7 +28,16 @@ public class URIValidator implements ConstraintValidator<URI, String> {
         }
 
         try {
-            new java.net.URI(value);
+            java.net.URI uri = new java.net.URI(value);
+            if (annotation.mustHaveScheme()) {
+                return StringUtils.isNotEmpty(uri.getScheme());
+            }
+            if (annotation.schemes().length > 0) {
+                return Arrays.asList(annotation.schemes()).contains(uri.getScheme());
+            }
+            if (annotation.minHostParts() > 0) {
+                return uri.getHost() != null && uri.getHost().split("\\.").length >= annotation.minHostParts();
+            }
         } catch(URISyntaxException e) {
             return false;
         }
