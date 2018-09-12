07e630426fd0c54d1344d54e4eeb5373b6ac580a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-09-12 14:10:04 +0200

    An object to index in I-tests of this.

---
+++ vpro-shared-elasticsearch-client/src/test/java/nl/vpro/elasticsearchclient/TestObject.java
@@ -0,0 +1,17 @@
+package nl.vpro.elasticsearchclient;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 2.0
+ */
+@Getter
+@Setter
+public class TestObject {
+
+    private String id;
+
+    private String title;
+}
