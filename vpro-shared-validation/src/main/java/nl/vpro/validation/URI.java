70cb723489be54a82339e71c36d7d6152b8b5862
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-10-03 12:25:44 +0200

    Javadoc.

--- vpro-shared-validation/src/main/java/nl/vpro/validation/URI.java
+++ vpro-shared-validation/src/main/java/nl/vpro/validation/URI.java
@@ -14,6 +14,14 @@ import javax.validation.Payload;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Marks a {@link String} as representing a valid URI or URL. The basic check is whether
+ * {@code
+ * new URI(<String>)
+ * }
+ * throws an exception
+ */
+
 @Target({METHOD, FIELD, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 @Constraint(validatedBy = URIValidator.class)
@@ -21,10 +29,22 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 public @interface URI {
     String message() default "{nl.vpro.constraints.URI}";
 
+    /**
+     * If true the URI must have a scheme.
+     */
     boolean mustHaveScheme() default false;
 
+     /**
+      * The number of parts (when the string is separated by a dot) the URI must have.
+      * E.g. if this is 2, then the url http://vpro/foobar will not be valid.
+      * (because you'd expect at least something like http://vpro.nl/foobar
+      */
     int  minHostParts() default 0;
 
+
+    /**
+     * If set, the acceptable schemes. E.g. {http, https}
+     */
     String[] schemes() default {};
 
     Class<?>[] groups() default {};
