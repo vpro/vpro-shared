79d5fc92a141484d39316ae51e0b2a082fedfd97
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-01-28 20:56:39 +0100

    Trying to fix javadoc update.

--- .travis-update-javadoc.sh
+++ .travis-update-javadoc.sh
@@ -4,9 +4,9 @@
 echo -e "Publishing javadoc...\n"
 source ./update-javadoc.sh
 
-cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
+
 git add -f doc
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER"
 git push -fq origin  > /dev/null
