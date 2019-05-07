0d12fff144d872aaacc9d3f235b5c1794fa6fd4b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-05-07 23:04:15 +0200

    Fixing test cases.

---
+++ vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/rs/JsonIdAdderBodyReader.java
@@ -0,0 +1,63 @@
+package nl.vpro.jackson2.rs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import nl.vpro.jackson2.Jackson2Mapper;
+
+/**
+ * Sometimes resteasy will not unmarshal an jsoin because there is not type information, but the prototype actully specifies it fully. The message body reader will deal with that, by adding the id implicetely (if it is missing) before the actual unmarshall.
+ *
+ * @author Michiel Meeuwissen
+ * @since 2.7
+ */
+@Provider
+@Consumes(MediaType.APPLICATION_JSON)
+@Slf4j
+public class JsonIdAdderBodyReader implements MessageBodyReader<Object> {
+
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return mediaType.isCompatible(MediaType.APPLICATION_JSON_TYPE);
+
+    }
+
+    @Override
+    public Object readFrom(
+        Class<Object> type,
+        Type genericType,
+        Annotation[] annotations,
+        MediaType mediaType,
+        MultivaluedMap<String, String> httpHeaders,
+        InputStream entityStream) throws WebApplicationException, IOException {
+        Jackson2Mapper mapper = Jackson2Mapper.getLenientInstance(); // TODO somehow get object mapper from resteasy context
+        JavaType javaType = mapper.getTypeFactory().constructType(genericType);
+        ObjectNode jsonNode = (ObjectNode) mapper.readTree(entityStream);
+        TypeDeserializer typeDeserializer = mapper.getDeserializationConfig().findTypeDeserializer(javaType);
+        if (typeDeserializer != null) {
+            String propertyName = typeDeserializer.getPropertyName();
+            String propertyValue = typeDeserializer.getTypeIdResolver().idFromBaseType();
+            if (! jsonNode.has(propertyName)) {
+                jsonNode.put(propertyName, propertyValue);
+            }
+        }
+        return mapper.treeToValue(jsonNode, (Class) genericType);
+
+    }
+}
---
+++ vpro-shared-jackson2/src/test/java/nl/vpro/jackson2/rs/JsonIdAdderInterceptorTest.java
@@ -0,0 +1,73 @@
+package nl.vpro.jackson2.rs;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since ...
+ */
+public class JsonIdAdderInterceptorTest {
+
+    @JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "objectType"
+    )
+    @JsonSubTypes(
+        {
+            @JsonSubTypes.Type(value = A.class, name = "a"),
+            @JsonSubTypes.Type(value = B.class, name = "b")
+        })
+    public interface  BaseI {
+
+    }
+
+    public static class Base implements BaseI {
+
+    }
+
+    @JsonTypeName("a")
+    public static class A extends Base {
+
+    }
+    @JsonTypeName("b")
+    public static class B extends Base {
+
+    }
+    public static class C {
+
+    }
+    JsonIdAdderBodyReader idAdderInterceptor = new JsonIdAdderBodyReader();
+
+    @Test
+    public void testA() throws IOException {
+
+        Object o = idAdderInterceptor.readFrom(Object.class,
+            A.class,
+            null,
+            MediaType.APPLICATION_JSON_TYPE, null, new ByteArrayInputStream("{}".getBytes()));
+        assertThat(o).isInstanceOf(A.class);
+    }
+
+    @Test
+    public void testC() throws IOException {
+
+        Object o = idAdderInterceptor.readFrom(Object.class,
+            C.class,
+            null,
+            MediaType.APPLICATION_JSON_TYPE, null, new ByteArrayInputStream("{}".getBytes()));
+        assertThat(o).isInstanceOf(C.class);
+    }
+
+}
