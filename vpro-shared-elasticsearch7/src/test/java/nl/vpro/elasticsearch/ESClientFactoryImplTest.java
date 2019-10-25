9c005de33a73a8c5726ec6001b3d9a58c2244749
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-10-25 16:30:59 +0200

    Added elasticsearch7 support.

--- vpro-shared-elasticsearch7/src/test/java/nl/vpro/elasticsearch/ESClientFactoryImplTest.java
+++ vpro-shared-elasticsearch7/src/test/java/nl/vpro/elasticsearch/ESClientFactoryImplTest.java
@@ -1,14 +1,13 @@
 package nl.vpro.elasticsearch;
 
-import java.net.InetAddress;
-import java.util.Arrays;
-
+import nl.vpro.util.UrlProvider;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import nl.vpro.util.UrlProvider;
+import java.net.InetAddress;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,7 +23,7 @@ public class ESClientFactoryImplTest {
         TransportClientFactory factory = new TransportClientFactory();
         factory.setTransportAddresses(new UrlProvider("a", 100), new UrlProvider("b", 101));
         TransportClient client = (TransportClient) factory.buildClient();
-        assertEquals(Arrays.asList(new InetSocketTransportAddress(InetAddress.getByName("a"), 100), new InetSocketTransportAddress(InetAddress.getByName("b"), 101)), client.transportAddresses());
+        assertEquals(Arrays.asList(new TransportAddress(InetAddress.getByName("a"), 100), new TransportAddress(InetAddress.getByName("b"), 101)), client.transportAddresses());
     }
 
     @Test
@@ -42,7 +41,7 @@ public class ESClientFactoryImplTest {
         //factory.setSniffCluster(false);
 
         TransportClient client = (TransportClient) factory.buildClient();
-        assertEquals("" + client.settings().getAsMap(), "false", client.settings().get("client.transport.sniff"));
+        assertEquals("" + client.settings(), "false", client.settings().get("client.transport.sniff"));
     }
 
     @Test
@@ -60,7 +59,7 @@ public class ESClientFactoryImplTest {
         //factory.setPingTimeoutInSeconds(10);
 
         TransportClient client = (TransportClient) factory.buildClient();
-        assertEquals("" + client.settings().getAsMap(), "10s", client.settings().get("client.transport.ping_timeout"));
+        assertEquals("" + client.settings(), "10s", client.settings().get("client.transport.ping_timeout"));
     }
 
     @Test
@@ -69,6 +68,6 @@ public class ESClientFactoryImplTest {
         //factory.setPingIntervalInSeconds(100);
 
         TransportClient client = (TransportClient) factory.buildClient();
-        assertEquals("" + client.settings().getAsMap(), "100s", client.settings().get("client.transport.nodes_sampler_interval"));
+        assertEquals("" + client.settings(), "100s", client.settings().get("client.transport.nodes_sampler_interval"));
     }
 }
