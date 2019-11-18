4d7f2ce80a13e45eac3caf24e77fc345ac2d94ae
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-18 22:38:59 +0100

    added for backwards compatibility

---
+++ vpro-shared-elasticsearch-client/src/main/java/nl/vpro/elasticsearchclient/Constants.java
@@ -0,0 +1,13 @@
+package nl.vpro.elasticsearchclient;
+
+/**
+ * @author Michiel Meeuwissen
+ */
+@Deprecated
+public class Constants extends nl.vpro.elasticsearch.Constants {
+
+    @Deprecated
+    public static class Fields extends nl.vpro.elasticsearch.Constants.Fields {
+
+    }
+}
