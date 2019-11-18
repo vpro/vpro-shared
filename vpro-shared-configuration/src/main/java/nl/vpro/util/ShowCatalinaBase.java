b09a049a7a9fca955b272dc40b1296d470e31b1a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2019-11-18 13:59:00 +0100

    commons-lang3

--- vpro-shared-configuration/src/main/java/nl/vpro/util/ShowCatalinaBase.java
+++ vpro-shared-configuration/src/main/java/nl/vpro/util/ShowCatalinaBase.java
@@ -4,7 +4,8 @@ import java.io.File;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+
 
 /**
  * Sometimes it is hard to know what currently is 'catalina.base' (intellij...) what we often are
