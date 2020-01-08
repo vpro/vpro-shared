ed500a2efb80a8a7490c081f5081cbbcd0cb291f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-01-08 09:26:47 +0100

    javadoc.

--- vpro-shared-test/src/main/java/nl/vpro/test/jupiter/NoAbort.java
+++ vpro-shared-test/src/main/java/nl/vpro/test/jupiter/NoAbort.java
@@ -4,8 +4,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
+ * In combination with {@link AbortOnException}. A test annotated with this will not be skipped, even if a previous test failed.
+ *
+ * Sometimes in integrations test some later test mainly perform clean up actions, and you'd better not skip it.
+ *
  * @author Michiel Meeuwissen
- * @since ...
+ * @since 2.10
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NoAbort {
