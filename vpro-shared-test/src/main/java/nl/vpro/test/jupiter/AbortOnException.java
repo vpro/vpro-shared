89b9f701fc9cfb95462d77ee98c9a7aaa03fc2a6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-01-08 21:00:55 +0100

    Rationalized AbortOnException stuff a bit.

--- vpro-shared-test/src/main/java/nl/vpro/test/jupiter/AbortOnException.java
+++ vpro-shared-test/src/main/java/nl/vpro/test/jupiter/AbortOnException.java
@@ -2,6 +2,7 @@ package nl.vpro.test.jupiter;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.annotation.*;
 import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.MethodOrderer;
@@ -19,12 +20,21 @@ import org.opentest4j.TestAbortedException;
 @Slf4j
 public class AbortOnException extends ExceptionCollector implements InvocationInterceptor, BeforeAllCallback {
 
+    protected boolean active = true;
+
     @Override
-    public void beforeAll(ExtensionContext context) throws Exception {
+    public void beforeAll(ExtensionContext context) {
         //store = context.getStore(Namespace.create(context.getRequiredTestClass()));
         TestMethodOrder annotation = context.getRequiredTestClass().getAnnotation(TestMethodOrder.class);
+        OnlyIfOrdered onlyIfOrdered = context.getRequiredTestClass().getAnnotation(OnlyIfOrdered.class);
+
         if (annotation == null || annotation.value().equals(MethodOrderer.Random.class)) {
-            throw new IllegalStateException("The abort on exception extension only makes sense with ordered test methods");
+            if (onlyIfOrdered == null) {
+                throw new IllegalStateException("The abort on exception extension only makes sense with ordered test methods");
+            } else {
+                log.info("Tests are not ordered. Disabled AbortOnException extension");
+                active = false;
+            }
         }
 
     }
@@ -33,14 +43,34 @@ public class AbortOnException extends ExceptionCollector implements InvocationIn
         Invocation<Void> invocation,
         ReflectiveInvocationContext<Method> invocationContext,
         ExtensionContext extensionContext) throws Throwable {
-        boolean hasNoAbortAnnotation =  invocationContext.getExecutable().getAnnotation(NoAbort.class) != null;
-        boolean skip = ! hasNoAbortAnnotation && ! fails.isEmpty();
-        if (skip) {
-             throw new TestAbortedException("An exception occured already " + fails.get(0).getInvocationContext().getExecutable().getName());
-         }
+
+        if (active) {
+            boolean hasNoAbortAnnotation = invocationContext.getExecutable().getAnnotation(NoAbort.class) != null;
+            boolean skip = !hasNoAbortAnnotation && !fails.isEmpty();
+            if (skip) {
+                throw new TestAbortedException("An exception occured already " + fails.get(0).getInvocationContext().getExecutable().getName());
+            }
+        }
         super.interceptTestMethod(invocation, invocationContext, extensionContext);
     }
 
 
-
+    /**
+     * In combination with {@link AbortOnException}. A test annotated with this will not be skipped, even if a previous test failed.
+     *
+     * Sometimes in integrations test some later test mainly perform clean up actions, and you'd better not skip it.
+     * @since 2.10
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.METHOD})
+    public @interface NoAbort {
+    }
+    /**
+     * @since 2.10
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.TYPE})
+    @Inherited
+    public @interface OnlyIfOrdered {
+    }
 }
