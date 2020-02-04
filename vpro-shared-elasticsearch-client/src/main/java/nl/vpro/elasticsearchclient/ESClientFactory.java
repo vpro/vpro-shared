bbe7e22c652b5ec835a6c6e2aae324bf358c7503
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-02-04 13:03:03 +0100

    PMP-41

--- vpro-shared-elasticsearch-client/src/main/java/nl/vpro/elasticsearchclient/ESClientFactory.java
+++ vpro-shared-elasticsearch-client/src/main/java/nl/vpro/elasticsearchclient/ESClientFactory.java
@@ -1,16 +1,17 @@
 package nl.vpro.elasticsearchclient;
 
 
-import org.elasticsearch.client.RestClient;
-
 import java.io.IOException;
+import java.util.function.Supplier;
+
+import org.elasticsearch.client.RestClient;
 
 /**
  * @author Michiel Meeuwissen
  * @since 3.6
  */
 @FunctionalInterface
-public interface ESClientFactory extends AutoCloseable {
+public interface ESClientFactory extends AutoCloseable, Supplier<RestClient> {
 
     /**
      * Returns a client. The client may be cached by the factory.
@@ -18,12 +19,17 @@ public interface ESClientFactory extends AutoCloseable {
 
     RestClient client(String logName);
 
+    @Override
+    default RestClient get() {
+        return client((String) null);
+    }
+
     default RestClient client(Class<?> clazz) {
         return client(clazz.getName());
     }
 
     default void close() throws IOException {
-        client((String) null).close();
+        get().close();
     }
 
 }
