f58fd771a396a705575ca62c19287b5bb25fd3cb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-02-06 14:11:26 +0100

    That actually broke the code.

--- vpro-shared-util/src/test/java/nl/vpro/util/StringsTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/StringsTest.java
@@ -1,12 +1,12 @@
 package nl.vpro.util;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -15,13 +15,11 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class StringsTest {
 
-
     @Test
     public void strings() {
         List<String> result = Strings.strings("a,b", "c,d").collect(Collectors.toList());
         assertThat(result).containsExactly("a", "b", "c", "d");
     }
-
 
     @Test
     public void stringsWithNewlines() {
@@ -33,16 +31,17 @@ public class StringsTest {
     public void stringsFile() throws IOException {
         File tempFile = File.createTempFile("test", "test");
         tempFile.deleteOnExit();
-        Writer writer = new OutputStreamWriter(new FileOutputStream(tempFile), StandardCharsets.UTF_8);
-        writer.append("f1\n");
-        writer.append("f2\n");
-        writer.close();
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(tempFile), UTF_8)) {
+            writer.append("f1\n");
+            writer.append("f2\n");
+        }
         List<String> result = Strings.strings("a,b", tempFile.toString(), "c,d").collect(Collectors.toList());
         assertThat(result).containsExactly("a", "b", "f1", "f2", "c", "d");
 
-		assertThat(Strings.strings("a,b", tempFile.toString(), "c,d")
+        assertThat(Strings.strings("a,b", tempFile.toString(), "c,d")
             .sorted().collect(Collectors.toList()))
-            .containsExactly("a", "b", "c", "d", "f1", "f2");
+                .containsExactly("a", "b", "c", "d", "f1", "f2");
+
 
 	}
 
