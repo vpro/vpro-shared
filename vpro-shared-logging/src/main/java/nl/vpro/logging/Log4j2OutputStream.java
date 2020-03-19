c2be765c6efd9ce8a09bcd6f6273b881808ea1cb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-03-19 16:30:01 +0100

    backporting

--- vpro-shared-logging/src/main/java/nl/vpro/logging/Log4j2OutputStream.java
+++ vpro-shared-logging/src/main/java/nl/vpro/logging/Log4j2OutputStream.java
@@ -6,19 +6,63 @@ import org.apache.logging.log4j.Logger;
  * @author Michiel Meeuwissen
  * @since 2.10
  */
-public abstract class Log4j2OutputStream {
+public abstract class Log4j2OutputStream extends AbstractLoggerOutputStream {
 
-    public static LoggerOutputStream debug(Logger log) {
+    public static Log4j2OutputStream debug(Logger log) {
         return debug(log, false);
     }
 
-    public static LoggerOutputStream debug(Logger log, boolean skipEmptyLines) {
-        return new LoggerOutputStream(skipEmptyLines) {
+    public static Log4j2OutputStream debug(Logger log, boolean skipEmptyLines) {
+        return new Log4j2OutputStream(skipEmptyLines) {
             @Override
             void log(String line) {
                 log.debug(line);
             }
         };
+    }
+
+    public static Log4j2OutputStream info(Logger log) {
+        return info(log, false);
+    }
+
+    public static Log4j2OutputStream info(Logger log, boolean skipEmptyLines) {
+        return new Log4j2OutputStream(skipEmptyLines) {
+            @Override
+            void log(String line) {
+                log.info(line);
+            }
+        };
+    }
+
+
+    public static Log4j2OutputStream warn(Logger log) {
+        return warn(log, false);
+    }
+
+    public static Log4j2OutputStream warn(Logger log, boolean skipEmptyLines) {
+        return new Log4j2OutputStream(skipEmptyLines) {
+            @Override
+            void log(String line) {
+                log.warn(line);
+            }
+        };
+    }
+
+    public static Log4j2OutputStream error(Logger log) {
+        return error(log, false);
+    }
+
+    public static Log4j2OutputStream error(Logger log, boolean skipEmptyLines) {
+        return new Log4j2OutputStream(skipEmptyLines) {
+            @Override
+            void log(String line) {
+                log.error(line);
+            }
+        };
+    }
+
+    Log4j2OutputStream(boolean skipEmptyLines) {
+        super(skipEmptyLines, null);
     }
 
 }
