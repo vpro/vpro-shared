13d8e71a1aef38e8225083c67b6af39ee4647c8c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-04-03 23:30:41 +0200

    Simplified.

--- vpro-shared-configuration/src/test/java/nl/vpro/spring/converters/StringToIntegerListConverterTest.java
+++ vpro-shared-configuration/src/test/java/nl/vpro/spring/converters/StringToIntegerListConverterTest.java
@@ -5,35 +5,36 @@ import java.util.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.convert.TypeDescriptor;
 
+import static nl.vpro.spring.converters.StringToIntegerListConverter.INSTANCE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringToIntegerListConverterTest {
 
     @Test
     public void testConvert() {
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("1,2")).isEqualTo(Arrays.asList(1, 2));
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("1-2")).isEqualTo(Arrays.asList(1, 2));
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("1, 7, 23-25")).isEqualTo(Arrays.asList(1, 7, 23, 24, 25));
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("1, 7, 23-25, -8")).isEqualTo(Arrays.asList(1, 7, 23, 24, 25, -8));
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("-1,1")).isEqualTo(Arrays.asList(-1, 1));
+        assertThat(INSTANCE.convert("1,2")).isEqualTo(Arrays.asList(1, 2));
+        assertThat(INSTANCE.convert("1-2")).isEqualTo(Arrays.asList(1, 2));
+        assertThat(INSTANCE.convert("1, 7, 23-25")).isEqualTo(Arrays.asList(1, 7, 23, 24, 25));
+        assertThat(INSTANCE.convert("1, 7, 23-25, -8")).isEqualTo(Arrays.asList(1, 7, 23, 24, 25, -8));
+        assertThat(INSTANCE.convert("-1,1")).isEqualTo(Arrays.asList(-1, 1));
     }
     @Test
     public void testConvertNegativeFirst() {
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("-1-1")).isEqualTo(Arrays.asList(-1, 0, 1));
+        assertThat(INSTANCE.convert("-1-1")).isEqualTo(Arrays.asList(-1, 0, 1));
     }
 
     @Test
     public void testConvertWithDots() {
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("-1..1")).isEqualTo(Arrays.asList(-1, 0, 1));
+        assertThat(INSTANCE.convert("-1..1")).isEqualTo(Arrays.asList(-1, 0, 1));
     }
       @Test
     public void testConvertWithDotsAndAll() {
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("-20--18,-8..-6,-1..1,3,5, 8-10"))
+        assertThat(INSTANCE.convert("-20--18,-8..-6,-1..1,3,5, 8-10"))
             .isEqualTo(Arrays.asList(-20, -19, -18, -8, -7, -6, -1, 0, 1, 3, 5, 8, 9, 10));
     }
     @Test
     public void testConvertNegativeBoth() {
-        assertThat(StringToIntegerListConverter.INSTANCE.convert("-2--1")).isEqualTo(Arrays.asList(-2, -1));
+        assertThat(INSTANCE.convert("-2--1")).isEqualTo(Arrays.asList(-2, -1));
     }
 
     static class TestClass {
@@ -59,15 +60,12 @@ public class StringToIntegerListConverterTest {
         TypeDescriptor targetNotMatches = new TypeDescriptor(TestClass.class.getField("targetNotMatches"));
         TypeDescriptor targetNotMatches2 = new TypeDescriptor(TestClass.class.getField("targetNotMatches2"));
 
-        StringToIntegerListConverter impl = new StringToIntegerListConverter();
-        assertThat(impl.matches(source, target)).isTrue();
+        assertThat(INSTANCE.matches(source, target)).isTrue();
 
-        assertThat(impl.matches(source, targetNotMatches)).isFalse();
-        assertThat(impl.matches(source, targetNotMatches2)).isFalse();
-
-
-        assertThat(impl.matches(sourceNotMatches, target)).isFalse();
+        assertThat(INSTANCE.matches(source, targetNotMatches)).isFalse();
+        assertThat(INSTANCE.matches(source, targetNotMatches2)).isFalse();
 
 
+        assertThat(INSTANCE.matches(sourceNotMatches, target)).isFalse();
     }
 }
