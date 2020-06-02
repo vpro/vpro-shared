16e8786eb314f263a558fd6eb6c896a03bf285a2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-02 23:11:09 +0200

    Logging.

--- vpro-shared-client-resteasy/src/main/java/nl/vpro/api/client/Utils.java
+++ vpro-shared-client-resteasy/src/main/java/nl/vpro/api/client/Utils.java
@@ -10,39 +10,59 @@ import javax.ws.rs.*;
  * @since 2.12
  */
 public class Utils {
-      
+
+    /**
+     * Creates a string representation of a call to a javax.ws.rs annotated method.
+     * So, it considers annotations like {@link Path}, {@link PathParam} and {@link QueryParam} to
+     * create an accurate yet clear presentation of what happened or will happen.
+     */
     public static String methodCall(Method method, Object[] args) {
-        StringBuilder builder = new StringBuilder();
+        StringBuilder pathBuilder = new StringBuilder();
         {
             Path classPath = method.getClass().getAnnotation(Path.class);
             if ( classPath != null) {
-                builder.append(classPath.value());
+                pathBuilder.append(classPath.value());
             }
         }
         {
             Path path = method.getAnnotation(Path.class);
             if ( path != null) {
-                builder.append(path.value());
+                pathBuilder.append(path.value());
             }
         }
+        StringBuilder queryBuilder = new StringBuilder();
+        StringBuilder bodyBuilder = new StringBuilder();
+
+
         {
             Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+            char sep = '?';
+            OUTER:
             for (int i = 0; i <  parameterAnnotations.length; i++) {
                 for (int j = 0 ; j < parameterAnnotations[i].length; j++) {
                     Annotation a = parameterAnnotations[i][j];
                     if (a instanceof PathParam) {
-                        builder.append('/').append(args[i]).append('/');
+                        pathBuilder.append('/').append(args[i]).append('/');
+                        continue OUTER;
                     }
                     if (a instanceof QueryParam) {
                         if (args[i] != null) {
-                            builder.append('&').append(((QueryParam) a).value()).append('=').append(args[i]);
+                            queryBuilder.append(sep).append(((QueryParam) a).value()).append('=').append(args[i]);
+                            sep = '&';
                         }
+                        continue OUTER;
                     }
+                }
+                if (args[i] != null) {
+                    bodyBuilder.append("\t");
+                    bodyBuilder.append(args[i]);
                 }
 
             }
         }
-        return builder.toString();
+        pathBuilder.append(queryBuilder);
+        pathBuilder.append(bodyBuilder);
+        return pathBuilder.toString();
     }
 
 }
