f8c3315609fe672bdcaa8a3a19adac1093d9addd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-15 20:59:57 +0200

    Added another view.

--- vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/Views.java
+++ vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/Views.java
@@ -1,7 +1,7 @@
 package nl.vpro.jackson2;
 
 /**
- * Several setups at VPRO and at NPO involve a backen system that publishes JSON to ElasticSearch.
+ * Several setups at VPRO and at NPO involve a backend system that publishes JSON to ElasticSearch.
  * In some cases this published json must be somewhat adapted, in contrast to when it not yet published.
  * @author Michiel Meeuwissen
  * @since 1.72
@@ -15,4 +15,12 @@ public class Views {
     public static class Publisher extends Normal {
 
     }
+
+    /**
+     * new field may be temporary marked 'ForwardPublisher'.
+     */
+    public  static class ForwardPublisher extends Publisher {
+
+    }
+
 }
