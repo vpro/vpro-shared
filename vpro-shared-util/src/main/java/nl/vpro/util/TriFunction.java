c33e1f85461224476f2c446a21007b32ce86fd4a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-18 21:31:44 +0200

    javadoc.

--- vpro-shared-util/src/main/java/nl/vpro/util/TriFunction.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/TriFunction.java
@@ -17,14 +17,27 @@ public interface TriFunction <T,U,V,R> {
 
     R apply(T t, U u, V v);
 
+    /**
+     * Morphs this {@link TriFunction} into a {@link BiFunction}, which a certain given value for the first argument.
+     *
+     * See {@link Functions#withArg1(BiFunction, Object)}
+     */
     default BiFunction<U, V, R> withArg1(T value) {
         return (u, v) -> apply(value, u, v);
     }
 
+    /**
+     * Morphs this {@link TriFunction} into a {@link BiFunction}, which a certain given value for the second argument.
+     *
+     * See {@link Functions#withArg2(BiFunction, Object)}
+     */
     default BiFunction<T, V, R> withArg2(U value) {
         return (t, v) -> apply(t, value, v);
     }
 
+    /**
+     * Morphs this {@link TriFunction} into a {@link BiFunction}, which a certain given value for the third argument.
+     */
     default BiFunction<T, U, R> withArg3(V value) {
         return (t, u) -> apply(t, u, value);
     }
