cb87efdff1ad2f1f0ac2ce930ea66341dd2b94f4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-23 09:15:58 +0200

    Spacing only.

--- vpro-shared-util/src/test/java/nl/vpro/util/VersionTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/VersionTest.java
@@ -15,29 +15,21 @@ public class VersionTest {
 
     @Test
     public void compareTo() {
-
         assertThat(parseIntegers("1.2.3")).isLessThan(parseIntegers("1.2.4"));
         assertThat(parseIntegers("1.2.3")).isGreaterThan(parseIntegers("1.2"));
         assertThat(parseIntegers("1.2.3").isAfter(1, 2)).isTrue();
         assertThat(parseIntegers("2.2.3").isAfter(1, 2, 5, 4)).isTrue();
         assertThat(parseIntegers("2.2.3").isNotBefore(2, 2, 3)).isTrue();
         assertThat(parseIntegers("2.2.3").isNotBefore(2, 2, 4)).isFalse();
-
-
-
-
     }
+    
     @Test
     public void parseWithSnaphot() {
         assertThat(parseIntegers("5.8-SNAPSHOT")).isEqualTo(of(5, 8));
     }
     @Test
     public void floatValue() {
-
         assertThat(parseIntegers("2.2.3").toFloat()).isEqualTo(2.002003f);
-
-
-
     }
 
 }
