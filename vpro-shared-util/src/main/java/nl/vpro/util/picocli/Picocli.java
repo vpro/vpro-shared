376b856b40318e12bb518f3a5643578f60a4b182
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-23 09:14:47 +0200

    Details.

--- vpro-shared-util/src/main/java/nl/vpro/util/picocli/Picocli.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/picocli/Picocli.java
@@ -4,13 +4,12 @@ import picocli.CommandLine;
 
 import java.time.Instant;
 
-import nl.vpro.util.Env;
-import nl.vpro.util.TimeUtils;
+import nl.vpro.util.*;
 
 /**
- * Provides some convertors for picocli command line parser, wich wrap some tools which are in this module.
+ * Provides some convertors for the <a href="https://picocli.info/">picocli</a> command line parser, wich wrap some tools which are in this module.
  *
- * The depenency on picocli is optional, so these classes are only useable if you havva picocli anyway.
+ * The dependency on picocli is optional, so these classes are only useable if you havva picocli anyway.
  *
  * @author Michiel Meeuwissen
  * @since 2.12
@@ -19,16 +18,25 @@ public class Picocli {
     private Picocli() {
 
     }
+
     public static class EnvConverter implements CommandLine.ITypeConverter<Env> {
         @Override
         public Env convert(String value) {
             return Env.valueOf(value.toUpperCase());
         }
     }
-    public static class InstantConvert implements CommandLine.ITypeConverter<Instant> {
+
+    public static class InstantConverter implements CommandLine.ITypeConverter<Instant> {
         @Override
         public Instant convert(String value) {
             return TimeUtils.parse(value).orElse(Instant.now());
+        }
+    }
+
+    public static class IntegerVersionConverter implements CommandLine.ITypeConverter<IntegerVersion> {
+        @Override
+        public IntegerVersion convert(String value) {
+            return IntegerVersion.parseIntegers(value);
         }
     }
 }
