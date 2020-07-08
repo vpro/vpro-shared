742f6832044fc73699a9e2fb4e52d5cd8b1f519e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-07-08 22:57:37 +0200

    Promoted mbean stuff to vpro-shared for easier reuse.

---
+++ vpro-shared-configuration/src/main/java/nl/vpro/jmx/AbstractGroovyScript.java
@@ -0,0 +1,56 @@
+package nl.vpro.jmx;
+
+import lombok.Getter;
+
+import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+import org.springframework.context.ApplicationContext;
+
+import nl.vpro.logging.simple.*;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since ...
+ */
+public abstract class AbstractGroovyScript {
+
+    protected SimpleLogger logger;
+    protected ApplicationContext applicationContext;
+
+    protected boolean interrupted = false;
+
+    public AbstractGroovyScript(ApplicationContext applicationContext, SimpleLogger logger) {
+        final Logger log  = LoggerFactory.getLogger(getClass());
+        this.logger = ChainedSimpleLogger.of(Slf4jSimpleLogger.of(log),
+            FileSimpleLogger.builder()
+                .tempFile(getClass().getSimpleName() + "." + Instant.now(), ".log")
+                .level(Level.DEBUG)
+                .build(), logger);
+        log.info("Logging to {}", this.logger);
+        this.applicationContext = applicationContext;
+    }
+
+
+
+    @lombok.Builder
+    @Getter
+    public static class FileAndMeta {
+        final File file;
+        final Long count;
+        final Duration duration;
+
+        @Override
+        public String toString() {
+            return String.valueOf(file) + " (" + count + " lines)";
+        }
+    }
+
+    abstract void run(String... args);
+
+
+}
---
+++ vpro-shared-configuration/src/main/java/nl/vpro/jmx/GroovyMBean.java
@@ -0,0 +1,83 @@
+package nl.vpro.jmx;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.time.Duration;
+
+import javax.inject.Inject;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.jmx.export.annotation.*;
+
+import nl.vpro.logging.simple.SimpleLogger;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 3.0
+ */
+@ManagedResource(description = "Execute groovy script", objectName = "nl.vpro.media:name=groovy")
+@Slf4j
+public class GroovyMBean {
+
+    @Inject
+    ApplicationContext applicationContext;
+
+
+    GroovyObject currentlyRunning;
+
+    @ManagedOperation(description = "Execute Groovy Script", currencyTimeLimit= -1)
+    @ManagedOperationParameters({
+        @ManagedOperationParameter(name = "groovyScript", description = ""),
+        @ManagedOperationParameter(name = "arguments", description = "")}
+    )
+    public String executeGroovySript(String groovyScript, String arguments) {
+        return
+            MBeans.returnString(groovyScript,  MBeans.multiLine(log, groovyScript), Duration.ofSeconds(10), (logger) -> {
+                try {
+                    ClassLoader parent = getClass().getClassLoader();
+                    GroovyClassLoader loader = new GroovyClassLoader(parent);
+
+                    File file = new File(groovyScript);
+                    Class<GroovyObject> groovyClass;
+                    if (file.exists()) {
+                        logger.info("{} is a file, so interpreting the file", groovyScript);
+                        groovyClass = loader.parseClass(file);
+                    } else {
+                        groovyClass = loader.parseClass(groovyScript);
+                    }
+                    GroovyObject groovyObject;
+                    try {
+                        Constructor<GroovyObject> constructor = groovyClass.getConstructor(ApplicationContext.class, SimpleLogger.class);
+
+                        groovyObject = constructor.newInstance(applicationContext, logger);
+                    } catch (NoSuchMethodException nsme) {
+                        groovyObject = groovyClass.newInstance();
+                    }
+                    Object[] args = arguments.split(",");
+                    currentlyRunning = groovyObject;
+                    groovyObject.invokeMethod("run", args);
+
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                } finally {
+                    currentlyRunning = null;
+                }
+            });
+    }
+
+
+    @ManagedOperation(description = "Interrupt Groovy Script")
+    public String interrupt() {
+        if (currentlyRunning != null) {
+            AbstractGroovyScript abstractGroovyScript = (AbstractGroovyScript) currentlyRunning;
+            abstractGroovyScript.interrupted = true;
+            return "Interrupted " + currentlyRunning;
+        } else {
+            return "No current script running";
+        }
+    }
+}
