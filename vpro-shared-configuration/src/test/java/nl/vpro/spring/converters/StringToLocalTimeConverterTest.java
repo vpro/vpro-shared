bd7d91c633949483c0417bcc1955f85a8cc83b46
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-07-17 10:28:16 +0200

    More tests.

--- vpro-shared-configuration/src/test/java/nl/vpro/spring/converters/StringToLocalTimeConverterTest.java
+++ vpro-shared-configuration/src/test/java/nl/vpro/spring/converters/StringToLocalTimeConverterTest.java
@@ -1,10 +1,11 @@
 package nl.vpro.spring.converters;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -12,11 +13,20 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2.13
  */
 class StringToLocalTimeConverterTest {
+    StringToLocalTimeConverter instance = new StringToLocalTimeConverter();
 
     @Test
     public void test() {
-        assertThat(new StringToLocalTimeConverter().convert("8:00")).isEqualTo(LocalTime.of(8, 0));
-        assertThat(new StringToLocalTimeConverter().convert("08:00")).isEqualTo(LocalTime.of(8, 0));
+
+        assertThat(instance.convert("8:00")).isEqualTo(LocalTime.of(8, 0));
+        assertThat(instance.convert("08:00")).isEqualTo(LocalTime.of(8, 0));
+
+        assertThat(instance.convert("08:00").getHour()).isEqualTo(8);
+        assertThat(instance.convert("8:00").getHour()).isEqualTo(8);
+        assertThat(instance.convert("8:01").getMinute()).isEqualTo(1);
+
+        assertThatThrownBy(() -> instance.convert("8:1")).isInstanceOf(DateTimeParseException.class);
+        assertThatThrownBy(() -> instance.convert(("8:01:31"))).isInstanceOf(DateTimeParseException.class);
 
     }
 
