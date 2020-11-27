801e24f0b7046bf10d7ee48d624d20ae6fdf9a10
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-11-27 09:38:38 +0100

    Added a few conversions I'll need.

---
+++ vpro-shared-configuration/src/main/java/nl/vpro/spring/converters/StringToLocalDateTimeConverter.java
@@ -0,0 +1,20 @@
+package nl.vpro.spring.converters;
+
+import java.time.LocalDateTime;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.springframework.core.convert.converter.Converter;
+
+import nl.vpro.util.TimeUtils;
+
+/**
+ * Converts a {@link String} into an {@link java.time.LocalDateTime} using {@link TimeUtils#parseLocalDateTime(CharSequence)} (CharSequence)}
+ * @author Michiel Meeuwissen
+ * @since 2.19
+ */
+public class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
+    @Override
+    public LocalDateTime convert(@NonNull String s) {
+        return TimeUtils.parseLocalDateTime(s).orElseThrow(() -> new IllegalArgumentException("Cannot parse " + s));
+    }
+}
