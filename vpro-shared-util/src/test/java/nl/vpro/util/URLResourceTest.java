7f756fed92b10e6556260285b76336c1d27da87c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-11-27 14:52:09 +0100

    -> junit5

--- vpro-shared-util/src/test/java/nl/vpro/util/URLResourceTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/URLResourceTest.java
@@ -1,27 +1,27 @@
 package nl.vpro.util;
 
 import lombok.extern.slf4j.Slf4j;
+import ru.lanwen.wiremock.ext.WiremockResolver;
+import ru.lanwen.wiremock.ext.WiremockUriResolver;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URLConnection;
+import java.net.*;
 import java.time.Duration;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpStatus;
-import org.junit.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static nl.vpro.util.URLResource.PROPERTIES;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,31 +31,28 @@ import static org.mockito.Mockito.when;
  * @since 0.37
  */
 @Slf4j
+@ExtendWith({
+    WiremockResolver.class,
+    WiremockUriResolver.class
+})
 public class URLResourceTest {
 
-    @ClassRule
-    public static WireMockClassRule wireMockClassRule = new WireMockClassRule(wireMockConfig().dynamicPort());
-
-    @Rule
-    public WireMockClassRule wireMock = wireMockClassRule;
-
-
-    @Before
-    public  void init() throws IOException {
-        stubFor(get(urlEqualTo("/broadcasters"))
+    @BeforeEach
+    public  void init(@WiremockResolver.Wiremock WireMockServer server) throws IOException {
+        server.stubFor(get(urlEqualTo("/broadcasters"))
             .willReturn(
                 aResponse()
                     .withBody(IOUtils.resourceToByteArray("/broadcasters.properties"))
                     .withHeader("Cache-Control", "public, max-age: 3600")
                     .withHeader("Last-Modified", "Wed, 24 Apr 2019 05:55:21 GMT")
             ));
-        stubFor(get(urlEqualTo("/broadcasters"))
+        server.stubFor(get(urlEqualTo("/broadcasters"))
             .withHeader("If-Modified-Since", equalTo("Wed, 24 Apr 2019 05:55:21 GMT"))
             .willReturn(
                 aResponse()
                     .withStatus(HttpStatus.SC_NOT_MODIFIED)
                 ));
-        stubFor(get(urlEqualTo("/redirect"))
+        server.stubFor(get(urlEqualTo("/redirect"))
             .willReturn(
                 aResponse()
                     .withHeader("Location", "/broadcasters")
@@ -66,16 +63,13 @@ public class URLResourceTest {
 
 
     @Test
-    public void broadcasters() throws InterruptedException {
-
-
-
+    public void broadcasters(@WiremockUriResolver.WiremockUri String uri) throws InterruptedException {
 
         URLResource<Properties> broadcasters =
-            URLResource.properties(URI.create("http://localhost:" + wireMock.port() + "/broadcasters"));
+            URLResource.properties(URI.create(uri + "/broadcasters"));
 
-        assertTrue(broadcasters.get().size() > 0);
-        assertEquals(1, broadcasters.getChangesCount());
+        assertThat(broadcasters.get()).isNotEmpty();
+        assertThat(broadcasters.getChangesCount()).isEqualTo(1);
         assertEquals(0, broadcasters.getNotModifiedCount());
         assertEquals(0, broadcasters.getNotCheckedCount());
 
@@ -109,8 +103,8 @@ public class URLResourceTest {
     }
 
     @Test
-    public void broadcastersRedirect() {
-        URLResource<Properties> broadcasters = URLResource.properties(URI.create("http://localhost:" + wireMock.port() + "/redirect"));
+    public void broadcastersRedirect(@WiremockUriResolver.WiremockUri String uri) {
+        URLResource<Properties> broadcasters = URLResource.properties(URI.create(uri + "/redirect"));
 
         assertTrue(broadcasters.get().size() > 0);
 
@@ -163,7 +157,7 @@ public class URLResourceTest {
 
 
 
-        assertTrue(broadcasters.get().size() == 0);
+        assertThat(broadcasters.get()).hasSize(0);
         assertEquals(0, broadcasters.getChangesCount());
         assertEquals(0, broadcasters.getNotModifiedCount());
         assertEquals(0, broadcasters.getNotCheckedCount());
@@ -189,7 +183,7 @@ public class URLResourceTest {
     }
 
     @Test
-    @Ignore
+    @Disabled
     public void broadcastersReal() {
         URLResource<Properties> broadcasters = URLResource.properties(URI.create("http://localhost:8071/broadcasters/"));
         broadcasters.setMinAge(Duration.ZERO);
@@ -202,5 +196,9 @@ public class URLResourceTest {
         assertEquals(1, broadcasters.getNotModifiedCount());
         assertEquals(0, broadcasters.getNotCheckedCount());
         System.out.println(broadcasters.get());
+    }
+
+    private void assertEquals(int i, long changesCount) {
+        assertThat(changesCount).isEqualTo(i);
     }
 }
