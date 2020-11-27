f87b2125dc71339e4c84eac0a6315a5f735c847a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-11-27 09:38:38 +0100

    Added a few conversions I'll need.

---
+++ vpro-shared-configuration/src/main/java/nl/vpro/spring/converters/StringToInstantConverter.java
@@ -0,0 +1,21 @@
+package nl.vpro.spring.converters;
+
+import java.time.Instant;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.springframework.core.convert.converter.Converter;
+
+import nl.vpro.util.TimeUtils;
+
+/**
+ * Converts a {@link String} into an {@link Instant} using {@link TimeUtils#parse(CharSequence)}
+ * @author Michiel Meeuwissen
+ * @since 2.19
+ */
+public class StringToInstantConverter implements Converter<String, Instant> {
+    @Override
+    public Instant convert(@NonNull String s) {
+        return TimeUtils.parse(s).orElseThrow(() -> new IllegalArgumentException("Cannot parse " + s));
+
+    }
+}
