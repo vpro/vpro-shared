ebb92ccbb520ddf0aa4c73ec17e8dcbb04ac7e8a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-11-27 14:28:01 +0100

    added utility for wildcard query.

--- vpro-shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/Constants.java
+++ vpro-shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/Constants.java
@@ -43,6 +43,7 @@ public class Constants {
         public static final String SHOULD = "should";
         public static final String RANGE = "range";
         public static final String TERM = "term";
+        public static final String WILDCARD = "wildcard";
         public static final String TERMS = "terms";
         public static final String PREFIX = "prefix";
     }
