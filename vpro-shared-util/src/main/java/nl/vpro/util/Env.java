c0838569853bdca886c36f8d12c068fb2f48dc5a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-01-20 12:02:09 +0100

    In preparation of migration to new platform, define constants for such environment, and started using them.

--- vpro-shared-util/src/main/java/nl/vpro/util/Env.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/Env.java
@@ -7,13 +7,23 @@ package nl.vpro.util;
 public enum Env {
     LOCALHOST,
     DEV,
+    DEV_NEW,
     TEST,
     TESTA,
     TESTB,
+    TEST_NEW,
+    TESTA_NEW,
+    TESTB_NEW,
     ACC,
     ACCA,
     ACCB,
+    ACC_NEW,
+    ACCA_NEW,
+    ACCB_NEW,
     PROD,
     PRODA,
-    PRODB
+    PRODB,
+    PROD_NEW,
+    PRODA_NEW,
+    PRODB_NEW
 }
