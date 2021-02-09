211ae102823a42d13d8735226004828f5b958801
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-02-09 17:06:15 +0100

    made it implement Map.Entry

--- vpro-shared-util/src/main/java/nl/vpro/util/Pair.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/Pair.java
@@ -3,6 +3,8 @@ package nl.vpro.util;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Map;
+
 /**
  * @author Michiel Meeuwissen
  * @since 1.63
@@ -10,7 +12,9 @@ import lombok.Data;
 @Data
 @AllArgsConstructor(staticName = "of")
 @lombok.Builder(builderClassName = "Builder")
-public class Pair<F, S> {
+
+public class Pair<F, S> implements Map.Entry<F, S> {
+
     private F first;
     @lombok.Builder.Default
     private String firstDescription = "first";
@@ -21,12 +25,49 @@ public class Pair<F, S> {
 
     public static <F, S> Pair<F, S> of(F first, S second){
         return Pair.<F, S>builder().first(first).second(second).build();
-
     }
 
     @Override
     public String toString() {
         return "(" + firstDescription + "=" + first + ", " + secondDescription + "=" + second + ")";
+    }
+
+    @Override
+    public F getKey() {
+        return getFirst();
+    }
+
+    @Override
+    public S getValue() {
+        return getSecond();
+    }
+
+    @Override
+    public S setValue(S value) {
+        S prev = second;
+        setSecond(value);
+        return prev;
+    }
+
+    public static class Builder<F, S> {
+
+        public Builder<F, S> key(F value) {
+            return descriptedFirst("key", value);
+        }
+
+        public Builder<F, S> value(S value) {
+            return descriptedSecond("value", value);
+        }
+
+
+        public Builder<F, S> descriptedFirst(String description, F value) {
+            return
+                firstDescription(description).first(value);
+        }
+        public Builder<F, S> descriptedSecond(String description, S value) {
+            return
+                secondDescription(description).second(value);
+        }
     }
 }
 
--- vpro-shared-util/src/test/java/nl/vpro/util/PairTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/PairTest.java
@@ -1,5 +1,7 @@
 package nl.vpro.util;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +21,26 @@ public class PairTest {
 
         assertThat(pair.toString()).isEqualTo("(first=a, second=b)");
         assertThat(pair.getFirst()).isEqualTo("a");
+        assertThat(pair.getFirstDescription()).isEqualTo("first");
         assertThat(pair.getSecond()).isEqualTo("b");
+        assertThat(pair.getSecondDescription()).isEqualTo("second");
+
+
+    }
+
+    @Test
+    public void testEntry() {
+        Map.Entry<String, Integer> pair = Pair.<String, Integer>builder()
+            .key("1")
+            .value(2)
+            .build();
+
+        assertThat(pair.toString()).isEqualTo("(key=1, value=2)");
+        assertThat(pair.getKey()).isEqualTo("1");
+        assertThat(pair.getValue()).isEqualTo(2);
+        pair.setValue(3);
+        assertThat(pair.getValue()).isEqualTo(3);
+
 
 
     }
