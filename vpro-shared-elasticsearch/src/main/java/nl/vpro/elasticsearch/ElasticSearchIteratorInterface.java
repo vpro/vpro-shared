b7f4efc78d213346a6424f84e3ecfda42629f3cb
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-02-22 17:11:56 +0100

    spacing

--- vpro-shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/ElasticSearchIteratorInterface.java
+++ vpro-shared-elasticsearch/src/main/java/nl/vpro/elasticsearch/ElasticSearchIteratorInterface.java
@@ -18,19 +18,17 @@ public interface ElasticSearchIteratorInterface<T> extends CountedIterator<T>, E
         return Collections.unmodifiableSet(SCROLL_IDS);
     }
 
-
-
-
     Optional<TotalRelation> getSizeQualifier();
 
     enum TotalRelation {
         EQUAL_TO,
         GREATER_THAN_OR_EQUAL_TO
     }
+
     Instant getStart();
+
+    @Override
     Long getCount();
-
-
 
     default Optional<Instant> getETA() {
         Long count = getCount();
@@ -44,19 +42,19 @@ public interface ElasticSearchIteratorInterface<T> extends CountedIterator<T>, E
             }
         }
         return Optional.empty();
-
     }
-
 
     // Override for JMX
     @Override
     default Date getStartDate() {
         return Date.from(getStart());
     }
+
     @Override
     default Date getETADate() {
         return getETA().map(Date::from).orElse(null);
     }
+
     @Override
     default Long getETASeconds() {
         return getETA().map(i -> (i.toEpochMilli() - getStart().toEpochMilli()) / 1000).orElse(null);
