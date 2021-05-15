c8e8f16d9a2f194de60b66556b0e1e7200cd8076
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-05-15 17:00:40 +0200

    javadoc

--- vpro-shared-util/src/main/java/nl/vpro/util/GranularClock.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/GranularClock.java
@@ -10,42 +10,63 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
- * A clock with limited granularity. It wraps another {@link Clock}, but it will only return (vi {@link #instant()}) values
+ * A clock with limited granularity. It wraps another {@link Clock}, but it will only return (via {@link #instant()}) values
  * that are multiples of the given {@link Duration} {@link #getGranularity()}.
  *
+ * @author Michiel Meeuwissen
  * @since 2.26
  */
-public class GranularClock extends Clock {
-    private final Clock clock;
+public class GranularClock extends Clock  {
+
+    @Getter
+    private final Clock wrapped;
 
     @Getter
     private final Duration granularity;
 
-    Instant granularValue;
+    /**
+     * The last return value of {@link #instant()}. This will be used also as a starting point of the next value to return.
+     */
+    private Instant granularValue;
 
     public static GranularClock of(Duration granularity) {
         return GranularClock.builder().granularity(granularity).build();
     }
 
+    /**
+     * @param clock The clock on which this clock is based. Defaults to {@link Clock#systemDefaultZone()}
+     * @param granularity The granularity of this clock. {@link #instant()} will return the initial value offset with an integer multiple of this.
+     * @param initialValue The value from which returned values will be calculated. Defaults to a truncated (using {@link GranularClock#getOrderOfMagnitude(Duration)}) value of {@link Clock#instant()}. Another sensible value which you might want to supply could e.g. be {@link Instant#EPOCH}
+     */
     @lombok.Builder
     GranularClock(@Nullable Clock clock, @NonNull Duration granularity, @Nullable Instant initialValue) {
-        this.clock = clock == null ? Clock.systemDefaultZone() : clock;
+        this.wrapped = clock == null ? Clock.systemDefaultZone() : clock;
         this.granularity = granularity;
-        this.granularValue = initialValue == null ? this.clock.instant().truncatedTo(getOrderOfMagnitude(granularity)) : initialValue;
+        this.granularValue = initialValue == null ? this.wrapped.instant().truncatedTo(getOrderOfMagnitude(granularity)) : initialValue;
     }
 
     @Override
     public ZoneId getZone() {
-        return clock.getZone();
+        return wrapped.getZone();
     }
 
     @Override
     public Clock withZone(ZoneId zone) {
         return GranularClock.builder()
-            .clock(clock.withZone(zone))
+            .clock(wrapped.withZone(zone))
             .granularity(granularity)
             .initialValue(granularValue)
             .build();
+    }
+
+    @Override
+    public Instant instant() {
+        final Instant now = wrapped.instant();
+        final long diff = Duration.between(granularValue, now).toMillis() / granularity.toMillis();
+        if (diff != 0) {
+            granularValue = granularValue.plus(granularity.multipliedBy(diff));
+        }
+        return granularValue;
     }
 
     static TemporalUnit getOrderOfMagnitude(Duration duration) {
@@ -62,12 +83,4 @@ public class GranularClock extends Clock {
         }
     }
 
-
-    @Override
-    public Instant instant() {
-        Instant now = clock.instant();
-        long diff = Duration.between(granularValue, now).toMillis() / granularity.toMillis();
-        granularValue = granularValue.plus(granularity.multipliedBy(diff));
-        return granularValue;
-    }
 }
