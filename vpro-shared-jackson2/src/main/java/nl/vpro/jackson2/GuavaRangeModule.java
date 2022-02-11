4d63e2b4ae1db52950363992667bd7d41cf36083
Michiel Meeuwissen <git@meeuw.org>
2022-02-11 11:49:01 +0100

    Added a bunch of serialVersionUID's

--- vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/GuavaRangeModule.java
+++ vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/GuavaRangeModule.java
@@ -12,23 +12,23 @@ import com.google.common.collect.Range;
 
 public class GuavaRangeModule extends SimpleModule {
 
+    private static final long serialVersionUID = -8048846883670339246L;
 
     public GuavaRangeModule() {
         super(new Version(1, 29, 1, "", "nl.vpro.shared", "vpro-jackson2"));
-
         addSerializer(Serializer.INSTANCE);
     }
 
     public static class Serializer extends com.fasterxml.jackson.databind.ser.std.StdSerializer<Range<?>> {
 
+        private static final long serialVersionUID = -4394016847732058088L;
         public static Serializer INSTANCE = new Serializer();
 
         protected Serializer() {
             super(new CollectionLikeType(
-            SimpleType.constructUnsafe(Range.class),
-            SimpleType.constructUnsafe(Comparable.class)) {
-
-
+                SimpleType.constructUnsafe(Range.class),
+                SimpleType.constructUnsafe(Comparable.class)) {
+                private static final long serialVersionUID = -2803462566784593946L;
             });
         }
 
