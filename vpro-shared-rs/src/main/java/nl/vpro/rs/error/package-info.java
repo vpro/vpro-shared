d2ef5c2efb2b001669d1089836631fa58ed9ef11
Michiel Meeuwissen <git@meeuw.org>
2022-02-22 22:53:24 +0100

    made it compile

---
+++ vpro-shared-rs/src/main/java/nl/vpro/rs/error/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides a few exception mappers. I'm not sure this is used.
+ *
+ * The wrapped entity {@link nl.vpro.rs.transfer.ErrorResponse} has no namespace
+ */
+
+package nl.vpro.rs.error;
