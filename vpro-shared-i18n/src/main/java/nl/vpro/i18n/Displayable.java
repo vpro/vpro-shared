47a40674cf85e9ba8d760b4351825ad267ac8c3f
Michiel Meeuwissen <git@meeuw.org>
2022-03-25 09:00:18 +0100

    Gave this a better name.

--- vpro-shared-i18n/src/main/java/nl/vpro/i18n/Displayable.java
+++ vpro-shared-i18n/src/main/java/nl/vpro/i18n/Displayable.java
@@ -96,5 +96,4 @@ public interface Displayable {
         return true;
     }
 
-
 }
--- vpro-shared-i18n/src/main/java/nl/vpro/i18n/validation/MustDisplay.java
+++ vpro-shared-i18n/src/main/java/nl/vpro/i18n/validation/MustDisplay.java
@@ -9,14 +9,20 @@ import java.lang.annotation.*;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+import nl.vpro.i18n.Displayable;
+
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+
+/**
+ * Marks that a {@link nl.vpro.i18n.Displayable} values is invalid if its value for {@link Displayable#display()} is not true.
+ */
 @Target({METHOD, FIELD, ANNOTATION_TYPE, TYPE_USE})
 @Retention(RUNTIME)
-@Constraint(validatedBy = DisplayValidator.class)
+@Constraint(validatedBy = DisplayableValidator.class)
 @Documented
-public @interface Display {
+public @interface MustDisplay {
     String message() default "{nl.vpro.constraints.display}";
 
     Class<?>[] groups() default {};
