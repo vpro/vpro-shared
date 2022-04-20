449be67d765b09d5e7e3767f93aa00d433978450
Michiel Meeuwissen <git@meeuw.org>
2022-04-20 08:42:14 +0200

    Test case failure

--- vpro-shared-monitoring/src/test/java/nl/vpro/monitoring/config/MonitoringConfigTest.java
+++ vpro-shared-monitoring/src/test/java/nl/vpro/monitoring/config/MonitoringConfigTest.java
@@ -6,6 +6,7 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,94 +25,102 @@ class MonitoringConfigTest {
     @InjectMocks
     private MonitoringConfig config;
 
+    private int offset;
+
+    @BeforeEach
+    public  void findOffset() {
+        offset = config.globalMeterRegistry().getMeters().size(); // some log4j2 counter are present
+    }
+
+
     @Test
     void empty() {
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).isEmpty();
+        assertThat(registry.getMeters()).hasSize(offset);
     }
 
     @Test
     void classLoader() {
         when(properties.isMeterClassloader()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset + 1);
     }
 
     @Test
     void isMeterEhCache2() {
         when(properties.isMeterEhCache2()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).isEmpty();
+        assertThat(registry.getMeters()).hasSize(offset);
     }
 
     @Test
     void isMeterJvmHeap() {
         when(properties.isMeterJvmHeap()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset + 1);
     }
 
     @Test
     void isMeterJvmGc() {
         when(properties.isMeterJvmGc()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset + 1);
     }
 
     @Test
     void isMeterJvmMemory() {
         when(properties.isMeterJvmMemory()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset + 1);
     }
 
     @Test
     void isMeterJvmThread() {
         when(properties.isMeterJvmThread()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset + 1);
     }
 
     @Test
     void isMeterHibernate() {
         when(properties.isMeterHibernate()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).isEmpty();
+        assertThat(registry.getMeters()).hasSize(offset);
     }
 
     @Test
     void isMeterHibernateQuery() {
         when(properties.isMeterHibernateQuery()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).isEmpty();
+        assertThat(registry.getMeters()).hasSize(offset);
     }
 
     @Test
     void isMeterPostgres() {
         when(properties.isMeterPostgres()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).isEmpty();
+        assertThat(registry.getMeters()).hasSize(offset);
     }
 
     @Test
     void isMeterProcessor() {
         when(properties.isMeterProcessor()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset + 1);
     }
 
     @Test
     void isMeterTomcat() {
         when(properties.isMeterTomcat()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).isEmpty();
+        assertThat(registry.getMeters()).hasSize(offset);
     }
 
     @Test
     void isMeterUptime() {
         when(properties.isMeterUptime()).thenReturn(true);
         final PrometheusMeterRegistry registry = config.globalMeterRegistry();
-        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(1);
+        assertThat(registry.getMeters()).hasSizeGreaterThanOrEqualTo(offset = 1);
     }
 
     @Test
