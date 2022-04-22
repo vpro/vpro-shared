38569a0af4fb7f8d6e1d40c0617e71eb64f89302
Michiel Meeuwissen <git@meeuw.org>
2022-04-22 17:41:42 +0200

    An enum, which we might be using.

---
+++ vpro-shared-util/src/main/java/nl/vpro/util/SchemaType.java
@@ -0,0 +1,12 @@
+package nl.vpro.util;
+
+
+/**
+ * Can be used to communicate what kind of schema is desired. We know of <a href="https://nl.wikipedia.org/wiki/XML_Schema">XML Schema Definitions</a> and of <a href="https://json-schema.org/">Json Schema</a>
+ *
+ * @since 2.30
+ */
+public enum SchemaType {
+    XSD,
+    JSON_SCHEMA
+}
