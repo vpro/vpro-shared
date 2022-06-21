3241fe1e975553155e3a612bd5035721da7903ec
Michiel Meeuwissen <git@meeuw.org>
2022-06-21 16:01:06 +0200

    Added a utility only.

--- vpro-shared-logging/src/main/java/nl/vpro/logging/Log4j2Helper.java
+++ vpro-shared-logging/src/main/java/nl/vpro/logging/Log4j2Helper.java
@@ -1,6 +1,8 @@
 package nl.vpro.logging;
 
 
+import java.time.Duration;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
@@ -18,7 +20,25 @@ public class Log4j2Helper {
         logger.log(info ? Level.INFO : Level.DEBUG, format, argArray);
     }
 
-    public static void log(Logger log, nl.vpro.logging.simple.Level level, CharSequence message) {
-        log.log(Level.valueOf(level.name()), message);
+    public static void log(Logger log, nl.vpro.logging.simple.Level level, String format, Object... argArray) {
+        log.log(Level.valueOf(level.name()), format, argArray);
+    }
+
+    public static void log(Logger log, Duration duration, Duration durationInfo, String format, Object... argArray) {
+        log.log(getLevel(duration, durationInfo.dividedBy(2), durationInfo, durationInfo.multipliedBy(2), durationInfo.multipliedBy(3)), format, argArray);
+    }
+
+    private  static Level getLevel(Duration duration, Duration durationDebug, Duration durationInfo, Duration durationWarn, Duration durationError) {
+        if (duration.compareTo(durationDebug) < 0 ) {
+            return Level.TRACE;
+        } else if (duration.compareTo(durationInfo) < 0) {
+            return Level.DEBUG;
+        } else if (duration.compareTo(durationWarn) < 0) {
+            return Level.INFO;
+        } else if (duration.compareTo(durationError) < 0) {
+            return Level.WARN;
+        } else {
+            return Level.ERROR;
+        }
     }
 }
