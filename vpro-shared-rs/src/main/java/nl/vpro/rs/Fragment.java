80bff49053c8eda06c4571b5422702a139abff38
Michiel Meeuwissen <git@meeuw.org>
2022-08-02 08:55:47 +0200

    javadoc.

--- vpro-shared-rs/src/main/java/nl/vpro/rs/Fragment.java
+++ vpro-shared-rs/src/main/java/nl/vpro/rs/Fragment.java
@@ -1,4 +1,9 @@
 package nl.vpro.rs;
 
+/**
+ * Annotating a resteasy method with this, will cause the produced xml to be a 'fragment', i.e. it will lack
+ * the {@code <?xml } prefix.
+ * @since 2.33.0
+ */
 public @interface Fragment {
 }
