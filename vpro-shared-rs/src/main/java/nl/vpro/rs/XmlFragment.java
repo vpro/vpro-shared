45c7f8b5d9f3eea366aca17febbc9ad03e9a0c24
Michiel Meeuwissen <git@meeuw.org>
2022-08-04 15:40:37 +0200

    explicit retention

--- vpro-shared-rs/src/main/java/nl/vpro/rs/XmlFragment.java
+++ vpro-shared-rs/src/main/java/nl/vpro/rs/XmlFragment.java
@@ -1,9 +1,13 @@
 package nl.vpro.rs;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Annotating a resteasy method with this, will cause the produced xml to be a 'fragment', i.e. it will lack
  * the {@code <?xml } prefix.
  * @since 2.33.0
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface XmlFragment {
 }
