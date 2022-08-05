5da9450218fb5f3be22aed7ef469ac04ba4ab417
Michiel Meeuwissen <git@meeuw.org>
2022-08-05 11:17:20 +0200

    measure duration of prometheus call itself.

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/web/PrometheusController.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/web/PrometheusController.java
@@ -2,13 +2,17 @@ package nl.vpro.monitoring.web;
 
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.time.Duration;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.meeuw.math.statistics.StatisticalLong;
+import org.meeuw.math.windowed.WindowedStatisticalLong;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +21,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/prometheus", produces = TextFormat.CONTENT_TYPE_004)
 @Slf4j
 public class PrometheusController {
+
+    @Getter
+    private final  WindowedStatisticalLong duration = WindowedStatisticalLong.builder()
+        .mode(StatisticalLong.Mode.DURATION)
+        .bucketCount(10)
+        .bucketDuration(Duration.ofMinutes(5))
+        .build();
 
     private final PrometheusMeterRegistry registry;
 
@@ -30,11 +41,14 @@ public class PrometheusController {
 
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(TextFormat.CONTENT_TYPE_004);
-
+        long start = System.nanoTime();
         try (Writer writer = response.getWriter()) {
             registry.scrape(writer);
             writer.flush();
+        } finally {
+            duration.accept(Duration.ofNanos(System.nanoTime() - start));
         }
+
     }
 
 }
