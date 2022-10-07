3226fb8618983a6db3deeb5c7d091b6b7811beed
Michiel Meeuwissen <git@meeuw.org>
2022-10-07 15:01:35 +0200

    Made it compile with java >= 11

--- vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/Jackson2Mapper.java
+++ vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/Jackson2Mapper.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
