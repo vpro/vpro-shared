c38546245b3116507fe613ed5bbdfff5591ca136
Michiel Meeuwissen <git@meeuw.org>
2023-08-02 08:19:32 +0200

    Improving perfomance of findByCrid. Added tests.

---
+++ vpro-shared-test/src/main/java/nl/vpro/test/psql/PostgresqlContainerSupport.java
@@ -0,0 +1,46 @@
+package nl.vpro.test.psql;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Configuration
+@Log4j2
+public class PostgresqlContainerSupport {
+
+    @Bean("psqlcontainer")
+    public PostgreSQLContainer<?> getPostgresqlContainer() {
+        PostgreSQLContainer<?> postgresDBContainer = new PostgreSQLContainer<>("postgres:13")
+            //.withStartupTimeout(Duration.ofSeconds(180L))
+            ;
+        postgresDBContainer.start();
+        return postgresDBContainer;
+    }
+
+    @Bean("dataSource")
+    @Inject
+    public DataSource getDataSource(PostgreSQLContainer<?> postgresDBContainer) {
+        PGSimpleDataSource source = new PGSimpleDataSource();
+        source.setURL(postgresDBContainer.getJdbcUrl());
+        source.setPassword(postgresDBContainer.getPassword());
+        source.setUser(postgresDBContainer.getUsername());
+        log.info("{} : {} ({})", postgresDBContainer.getUsername(), postgresDBContainer.getPassword(), postgresDBContainer.getJdbcUrl());
+
+
+        Flyway flyway = Flyway.configure()
+            .table("schema_version")
+            .dataSource(source)
+            .validateMigrationNaming(true)
+            .load();
+        flyway.migrate();
+        return source;
+
+    }
+}
