1eb9b1450f6b750cb538c18a6f84f5eb10e546af
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-11-30 11:45:43 +0100

    More testing.

--- vpro-shared-util/src/test/java/nl/vpro/util/WrappedReadableByteChannelTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/WrappedReadableByteChannelTest.java
@@ -8,9 +8,10 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.Random;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.output.NullOutputStream;
+import org.apache.commons.io.output.CountingOutputStream;
 import org.junit.jupiter.api.Test;
 
+import static java.io.OutputStream.nullOutputStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Log4j2
@@ -21,17 +22,39 @@ class WrappedReadableByteChannelTest {
         Random random = new Random();
         int size = random.nextInt(1_000_000) + 500_000;
         ReadableByteChannel channel = Channels.newChannel(new RandomStream(random, size));
+        CountingOutputStream outputStream = new CountingOutputStream(nullOutputStream());
+
         try (WrappedReadableByteChannel wrapped = WrappedReadableByteChannel.builder()
             .delegate(channel)
             .batchSize(100_000L)
             .consumer((l) -> log.info("{}", () -> FileSizeFormatter.DEFAULT.format(l)))
             .build()) {
-            IOUtils.copy(Channels.newInputStream(wrapped), NullOutputStream.nullOutputStream());
+            IOUtils.copy(Channels.newInputStream(wrapped),outputStream);
 
             assertThat(wrapped.getTotal()).isEqualTo(size);
             assertThat(wrapped.isOpen()).isTrue();
         }
         assertThat(channel.isOpen()).isFalse();
+        assertThat(outputStream.getCount()).isEqualTo(size);
+
+    }
+
+    @Test
+    public void noConsumer() throws IOException {
+        Random random = new Random();
+        int size = random.nextInt(1_000_000) + 500_000;
+        ReadableByteChannel channel = Channels.newChannel(new RandomStream(random, size));
+        CountingOutputStream outputStream = new CountingOutputStream(nullOutputStream());
+        try (WrappedReadableByteChannel wrapped = WrappedReadableByteChannel.builder()
+            .delegate(channel)
+            .build()) {
+            IOUtils.copy(Channels.newInputStream(wrapped), outputStream);
+
+            assertThat(wrapped.getTotal()).isEqualTo(size);
+            assertThat(wrapped.isOpen()).isTrue();
+        }
+        assertThat(channel.isOpen()).isFalse();
+        assertThat(outputStream.getCount()).isEqualTo(size);
     }
 
 }
