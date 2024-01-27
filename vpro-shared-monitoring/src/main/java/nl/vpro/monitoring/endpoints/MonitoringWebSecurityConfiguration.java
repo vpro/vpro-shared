471ba9e4c0b289fda478add85e111a2cb6913c18
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-01-27 09:48:48 +0100

    resolving deprecation.

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringWebSecurityConfiguration.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringWebSecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,30 +31,23 @@ public class MonitoringWebSecurityConfiguration { // deprecated damn.
 
     @Bean
     public SecurityFilterChain monitoringSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) -> {
-                if (properties.isHealthPermitAll()) {
-                    authz.requestMatchers(antMatcher("/manage/health")).permitAll();
-                }
-                try {
-                    authz.requestMatchers(antMatcher("/manage/**"))
-                        .hasRole("MANAGER")
-                        .and()
-                        .httpBasic()
-                        .realmName("management")
-                    ;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+        if (properties.isHealthPermitAll()) {
+            http.authorizeHttpRequests((authz) -> {
+                authz.requestMatchers(
+                    antMatcher("/manage/health")
+                ).permitAll();
             });
+        }
+        http.authorizeHttpRequests((authz) -> {
+            authz.
+                requestMatchers(antMatcher("/manage/**"))
+                .hasRole("MANAGER");
+        }).httpBasic(
+            httpBasic -> httpBasic.realmName("manager"));
+
         return http.build();
     }
 
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(antMatcher("/resources/**"));
-    }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
