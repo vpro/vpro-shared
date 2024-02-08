ebde71c6b2152cacacaf98069f73ba2ea57f2b06
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-02-08 16:56:06 +0100

    Ported

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringWebSecurityConfiguration.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringWebSecurityConfiguration.java
@@ -4,11 +4,11 @@ import javax.inject.Inject;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,9 +17,9 @@ import nl.vpro.monitoring.config.MonitoringProperties;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @EnableWebSecurity
-@Order(-1)
-public class MonitoringWebSecurityConfiguration { // deprecated damn.
+public class MonitoringWebSecurityConfiguration {
 
     private final MonitoringProperties properties;
 
@@ -29,32 +29,26 @@ public class MonitoringWebSecurityConfiguration { // deprecated damn.
         configure(auth);
     }
 
+
     @Bean
-    public SecurityFilterChain monitoringSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) -> {
-                if (properties.isHealthPermitAll()) {
-                    authz.requestMatchers(antMatcher("/manage/health")).permitAll();
-                }
-                try {
-                    authz.requestMatchers(antMatcher("/manage/**"))
-                        .hasRole("MANAGER")
-                        .and()
-                        .httpBasic()
-                        .realmName("management")
-                    ;
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(antMatcher("/manage/**"));
+        if (properties.isHealthPermitAll()) {
+            http.authorizeHttpRequests((authz) -> {
+                authz.requestMatchers(
+                    antMatcher("/manage/health")
+                ).permitAll();
             });
+        }
+        http.authorizeHttpRequests((authz) -> {
+            authz.
+                requestMatchers(antMatcher("/manage/**"))
+                .hasRole("MANAGER");
+        }).httpBasic(
+            httpBasic -> httpBasic.realmName("manager"));
         return http.build();
     }
 
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(antMatcher("/resources/**"));
-    }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
