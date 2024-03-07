3633d116fe947e953fcd19987c19a6e389fe51e9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-03-07 11:27:21 +0100

    Covnerted to record.

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/domain/Health.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/domain/Health.java
@@ -1,17 +1,17 @@
 package nl.vpro.monitoring.domain;
 
-import lombok.Data;
+import lombok.Builder;
 
 import java.time.Duration;
 import java.time.Instant;
 
-@Data
-@lombok.Builder
-public class Health {
-    private final int status;
-    private final String message;
-    private final Instant startTime;
-    private final Duration upTime;
-    private final Duration prometheusCallDuration;
-    private final Long prometheusDownCount;
+
+@Builder
+public record Health(
+    int status,
+    String message,
+    Instant startTime,
+    Duration upTime,
+    Duration prometheusCallDuration,
+    Long prometheusDownCount) {
 }
