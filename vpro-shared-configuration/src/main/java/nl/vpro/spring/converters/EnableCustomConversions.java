1fd80da0941b0ca693ba3b316dc80b636dc1d28f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-04-02 10:20:55 +0200

    javadoc.

--- vpro-shared-configuration/src/main/java/nl/vpro/spring/converters/EnableCustomConversions.java
+++ vpro-shared-configuration/src/main/java/nl/vpro/spring/converters/EnableCustomConversions.java
@@ -9,8 +9,11 @@ import com.google.common.annotations.Beta;
 
 
 /**
-
+ * Enabled a bunch of custom versions by VPRO, mainly related to converting to Durations and so on.
+ *
  * @since 5.0
+ * @see CustomConversion
+ * @see nl.vpro.spring.converters
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
