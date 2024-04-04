e864f9411f1c30a5699f3b5c116c7af2189aa0e3
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-04-04 13:13:48 +0200

    Simplified.

--- vpro-shared-configuration/src/main/java/nl/vpro/spring/converters/EnableCustomConversions.java
+++ vpro-shared-configuration/src/main/java/nl/vpro/spring/converters/EnableCustomConversions.java
@@ -3,9 +3,6 @@ package nl.vpro.spring.converters;
 import java.lang.annotation.*;
 
 import org.springframework.context.annotation.Import;
-import org.springframework.core.annotation.Order;
-
-import com.google.common.annotations.Beta;
 
 
 /**
@@ -18,7 +15,5 @@ import com.google.common.annotations.Beta;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Import({CustomConversion.class})
-@Beta
-@Order(1)
 public @interface EnableCustomConversions {
 }
