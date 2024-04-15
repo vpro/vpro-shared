bb499c81ecb61f20a1e65131691c0a25c6e7e94b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-04-15 10:02:24 +0200

    test entity.

---
+++ vpro-shared-hibernate-search6/src/test/java/nl/vpro/hibernate/search6/TestEntity.java
@@ -0,0 +1,19 @@
+package nl.vpro.hibernate.search6;
+
+import javax.persistence.*;
+
+@Entity
+public class TestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
