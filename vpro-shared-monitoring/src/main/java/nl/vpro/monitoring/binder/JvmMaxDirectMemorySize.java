ad0fc95fc71eaafb6162193b15830fc8040cadb7
Rico Jansen <rico.jansen@gmail.com>
2024-05-14 13:20:11 +0200

    Fix memory reference.

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/binder/JvmMaxDirectMemorySize.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/binder/JvmMaxDirectMemorySize.java
@@ -1,12 +1,12 @@
 package nl.vpro.monitoring.binder;
 
+import io.micrometer.common.lang.NonNull;
 import io.micrometer.core.instrument.*;
 import io.micrometer.core.instrument.binder.BaseUnits;
 import io.micrometer.core.instrument.binder.MeterBinder;
 
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.atomic.AtomicLong;
-
 
 import com.sun.management.HotSpotDiagnosticMXBean;
 import com.sun.management.VMOption;
@@ -17,6 +17,7 @@ public class JvmMaxDirectMemorySize implements MeterBinder {
 
     private static final String MAX_DIRECT_MEMORY_SIZE = "MaxDirectMemorySize";
     private final Iterable<Tag> tags;
+    private final AtomicLong maxDirectMemorySize = new AtomicLong();
 
     public JvmMaxDirectMemorySize() {
         this(emptyList());
@@ -28,20 +29,20 @@ public class JvmMaxDirectMemorySize implements MeterBinder {
 
 
     @Override
-    public void bindTo(MeterRegistry registry) {
-        AtomicLong size = getMaxDirectMemorySize();
+    public void bindTo(@NonNull MeterRegistry registry) {
+        setMaxDirectMemorySize();
         Iterable<Tag> tagsWithId = Tags.concat(tags, "id", "direct");
 
-        Gauge.builder("jvm.buffer.memory.max.bytes", size, AtomicLong::get)
+        Gauge.builder("jvm.buffer.memory.max.bytes", maxDirectMemorySize, AtomicLong::get)
             .tags(tagsWithId)
             .description("The maximum number of bytes that can be allocated to direct memory")
             .baseUnit(BaseUnits.BYTES)
+            .strongReference(true)
             .register(registry);
 
     }
 
-    private AtomicLong getMaxDirectMemorySize() {
-        AtomicLong maxDirectMemorySize = new AtomicLong();
+    private void setMaxDirectMemorySize() {
 
         HotSpotDiagnosticMXBean diagnosticMXBean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
         maxDirectMemorySize.set(0);
@@ -57,6 +58,5 @@ public class JvmMaxDirectMemorySize implements MeterBinder {
         if (maxDirectMemorySize.get() == 0) {
             maxDirectMemorySize.set(Runtime.getRuntime().maxMemory());
         }
-        return maxDirectMemorySize;
     }
 }
