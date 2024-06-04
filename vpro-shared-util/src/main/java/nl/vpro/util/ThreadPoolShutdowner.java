629d928236ae86b21052ab5854690c385257617e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-04 23:04:28 +0200

    lets also shut down commonPool

--- vpro-shared-util/src/main/java/nl/vpro/util/ThreadPoolShutdowner.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/ThreadPoolShutdowner.java
@@ -1,13 +1,22 @@
 package nl.vpro.util;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
 
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * A junit TestExecutionListener that just makes sure that all ThreadPools are shutdown after the tests.
+ */
+@Log4j2
 public class ThreadPoolShutdowner implements TestExecutionListener {
 
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
         ThreadPools.shutdown();
+        log.info("Shutting down ForkJoinPool.commonPool() too");
+        ForkJoinPool.commonPool().shutdown();
     }
 
 }
