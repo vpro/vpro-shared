d467df0708990f9870599121d5faaf15097f9325
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-24 12:03:15 +0200

    Better file watching.

--- vpro-shared-util/src/main/java/nl/vpro/util/DirectoryWatcher.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/DirectoryWatcher.java
@@ -1,11 +1,13 @@
 package nl.vpro.util;
 
-import lombok.NonNull;
+
 import lombok.SneakyThrows;
+import lombok.With;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.time.Clock;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.*;
@@ -17,6 +19,7 @@ import java.util.stream.Stream;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import nl.vpro.jmx.MBeans;
@@ -38,8 +41,7 @@ public class DirectoryWatcher implements AutoCloseable {
     private final Path directory;
     final WatchService watcher;
 
-    private final Consumer<Path> consumer;
-
+    private final Consumer<WatcherEvent> consumer;
     private final Predicate<Path> filter;
 
     private final Future<?> future;
@@ -50,32 +52,52 @@ public class DirectoryWatcher implements AutoCloseable {
     final AtomicInteger counter = new AtomicInteger();
     private Instant last = null;
 
+    private final Clock clock;
 
-    public DirectoryWatcher(@NonNull Path directory, @NonNull Consumer<Path> consumer, @Nullable Predicate<Path> filter) throws IOException {
+
+    /**
+     * @param directory The directory to watch
+     * @param eventConsumer What should happen if a certain file in it changes
+     * @param pathConsumer What should happen if a certain file in it changes
+     * @param filter An optional filter on files in this directory
+     */
+    @lombok.Builder
+    private DirectoryWatcher(
+        @NonNull Path directory,
+        @Nullable Consumer<WatcherEvent> eventConsumer,
+        @Nullable Consumer<Path> pathConsumer,
+        @Nullable Predicate<Path> filter,
+        @Nullable Clock clock) throws IOException {
         this.directory = directory;
         this.watcher = directory.getFileSystem().newWatchService();
-
-        this.consumer = consumer;
+        if (eventConsumer == null && pathConsumer == null) {
+            throw new IllegalArgumentException("Either eventConsumer or pathConsumer should be set");
+        }
+        this.consumer = eventConsumer != null ? eventConsumer :
+            (we) -> pathConsumer.accept((we.resolved));
         this.filter = filter == null ? (p) -> true : filter;
-        this.future = useWatchService();
-
-
+        this.clock = clock == null ? Clock.systemUTC() : clock;
+        this.future = watchService();
 
 
         try {
-            MBeans.registerBean(new ObjectName("nl.vpro:name=watcher,directory="  + directory), new Admin());
-        } catch (MalformedObjectNameException mfoe) {
+            MBeans.registerBean(
+                new ObjectName("nl.vpro:name=watcher,directory="  + directory),
+                new Admin()
+            );
+        } catch (MalformedObjectNameException ignored) {
             // ignored, the objectname _is_ not malformed
         }
-
     }
 
     private WatchKey register(Path path, WatchService watcher) throws IOException {
         return path.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
     }
 
+
+
     @SneakyThrows
-    private Future<?> useWatchService() {
+    private Future<?> watchService() {
         register(directory, watcher);
 
         // check initial set of file for existing symlinks to follow the targets of.
@@ -83,64 +105,138 @@ public class DirectoryWatcher implements AutoCloseable {
             p.forEach(file -> checkSymlink(file, StandardWatchEventKinds.ENTRY_CREATE));
         }
 
-        Callable<Void> callable = () -> {
-            log.info("Watching {}", directory);
-            Thread.currentThread().setName("Watcher for " + directory);
-            while (true) {
+        Callable<Void> callable = () -> {this.watchLoop(); return null;};
+        return executorService.submit(callable);
+    }
+
+    /**
+     * An event in the watched directory
+     * @param resolved The resolved path of the event (which may be the target of a symlink)
+     *                 The relevant file will always be the key of a map.
+     * @param type The type of event
+     * @param instant Associated time.
+     */
+    public record WatcherEvent(@NonNull Path file, @NonNull Path resolved, @With @NonNull WatcherEventType type, @NonNull Instant instant) {
+
+
+    }
+
+
+    private Instant instant(Path file1, Path file2) throws IOException {
+        Instant prop = null;
+        if (Files.exists(file1)) {
+            prop = Files.getLastModifiedTime(file1).toInstant();
+        }
+        if (Files.exists(file2)) {
+            Instant prop2 = Files.getLastModifiedTime(file2).toInstant();
+            if (prop == null || prop2.isAfter(prop)) {
+                prop = prop2;
+            }
+        }
+        return prop != null ? prop : clock.instant();
+    }
+
+    public enum WatcherEventType {
+        CREATE,
+        MODIFY,
+        DELETE,
+        RELINKED;
+
+        public static WatcherEventType of(WatchEvent.Kind<?> type) {
+            if (type ==  ENTRY_CREATE) {
+                return CREATE;
+            } else if (type == ENTRY_MODIFY) {
+                return MODIFY;
+            } else if (type == ENTRY_DELETE) {
+                return DELETE;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private void watchLoop() {
+        log.info("Watching {}", directory);
+        Thread.currentThread().setName("Watcher for " + directory);
+        while (true) {
+            try {
+                WatchKey key = watcher.take();
+                // collect events, so we could report
+                List<WatcherEvent> events = new ArrayList<>();
                 try {
-                    WatchKey key = watcher.take();
-                    List<Path> events = new ArrayList<>();
-                    try {
-                        if (key.watchable() instanceof Path d) {
-                            for (WatchEvent<?> event : key.pollEvents()) {
-                                if (event.count() > 1) {
-                                    log.info("Repeated event {}", event);
-                                    continue;
-                                }
-                                if (event.context() instanceof Path eventPath) {
-                                    Path file = d.resolve(eventPath);
-                                    if (d.equals(directory)) {
-                                        log.info("{}", file);
-                                        if (filter.test(file)) {
-                                            log.info(event.kind() + " " + event.context());
-                                            checkSymlink(file, event.kind());
-                                            events.add(file);
-                                        } else {
-                                            log.info("Ignored {} {}", event.kind(), event.context());
+                    if (key.watchable() instanceof Path d) {
+                        for (WatchEvent<?> event : key.pollEvents()) {
+                            if (event.count() > 1) {
+                                log.info("Repeated event {}", event);
+                                continue;
+                            }
+                            if (event.context() instanceof Path eventPath) {
+                                Path file = d.resolve(eventPath);
+                                if (d.equals(directory)) {
+                                    log.debug("{}", file);
+                                    if (filter.test(file)) {
+                                        var watcherEvent = new WatcherEvent(file, file, WatcherEventType.of(event.kind()), instant(file, file));
+                                        log.info(event.kind() + " " + event.context());
+                                        events.add(watcherEvent);
+                                        checkSymlink(file, event.kind());
+                                        if (event.kind() == ENTRY_DELETE) {
+                                            lastModified.remove(file);
+
                                         }
                                     } else {
-                                        Path resolved = watchedTargetFiles.get(file);
-                                        if (resolved != null) {
-                                            events.add(resolved);
-                                        } else {
-                                            log.info("Not a watched file {}", file);
-                                        }
+                                        log.debug("Ignored {} {}", event.kind(), event.context());
                                     }
                                 } else {
-                                    log.info("Not a path {}", event.context());
+                                    Path resolved = watchedTargetFiles.get(file);
+                                    if (resolved != null) {
+                                        events.add(new WatcherEvent(file, resolved, WatcherEventType.of(event.kind()), instant(file, resolved)));
+                                        if (event.kind() == ENTRY_DELETE) {
+                                            lastModified.remove(file);
+                                        }
+                                    } else {
+                                        log.debug("Not a watched file {}", resolved);
+                                    }
                                 }
+                            } else {
+                                log.info("Not a path {}", event.context());
                             }
-                        } else {
-                            log.info("Not a path {}", key.watchable());
                         }
-                        for (Path p : events) {
-                            counter.incrementAndGet();
-                            last = Instant.now();
-                            consumer.accept(p);
-                        }
-
-                    } finally {
-                        key.reset();
+                    } else {
+                        log.info("Not a path {}", key.watchable());
                     }
-                } catch (InterruptedException e) {
-                    log.info("Interrupted watcher for " + directory);
-                    Thread.currentThread().interrupt();
-                    break;
+
+
+                    for (WatcherEvent e : events) {
+                        final Instant previous = lastModified.getOrDefault(e.resolved, Instant.EPOCH);
+                        final Instant time = e.instant;
+                        if (time.isAfter(previous)) {
+                            handleEvent(e);
+
+                        } else {
+                            log.info("Ignoring {} because {} <= {}", e, time, previous);
+                        }
+                    }
+
+                } catch (IOException e) {
+                    log.warn(e.getMessage(), e);
+                } finally {
+                    key.reset();
                 }
+            } catch (InterruptedException e) {
+                log.info("Interrupted watcher for " + directory);
+                Thread.currentThread().interrupt();
+                break;
             }
-            return null;
-        };
-        return executorService.submit(callable);
+        }
+    }
+
+    private void handleEvent(WatcherEvent e) {
+       counter.incrementAndGet();
+        last = clock.instant();
+        consumer.accept(e);
+        if (e.type != WatcherEventType.DELETE) {
+            lastModified.put(e.resolved, e.instant);
+        }
     }
 
     @SneakyThrows
@@ -148,17 +244,27 @@ public class DirectoryWatcher implements AutoCloseable {
         if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
             if (watchedTargetFiles.containsValue(file)) {
                 if (watchedTargetFiles.entrySet().removeIf(e -> e.getValue().equals(file))) {
-                    log.info("Removed {}", file);
+                    log.info("Removed source {}", file);
                 }
             }
-        }
+            // if it was a symlink, then stop watching the target too.
+            Path target = watchedTargetFiles.remove(file);
+            if (target != null) {
+                log.info("Removed target {}", target);
+            }
+            if (lastModified.remove(file) != null) {
+                log.info("Removed lastModified {}", file);
 
+            }
+        }
         if (Files.isSymbolicLink(file)) {
             Path resolve = Files.readSymbolicLink(file);
             if (! resolve.isAbsolute()) {
                 resolve = file.getParent().resolve(resolve);
             }
+            watchedTargetFiles.entrySet().removeIf(e -> e.getValue().equals(file));
             watchedTargetFiles.put(resolve, file);
+
             if (!watchedTargetDirectories.contains(resolve.getParent())) {
                 register(resolve.getParent(), watcher);
                 watchedTargetDirectories.add(resolve.getParent());
@@ -177,6 +283,10 @@ public class DirectoryWatcher implements AutoCloseable {
 
     public Set<Path> getWatchedTargetDirectories() {
         return Collections.unmodifiableSet(watchedTargetDirectories);
+    }
+
+    public Map<Path, Instant> getWatchedLastModifieds() {
+        return Collections.unmodifiableMap(lastModified);
     }
 
     public class Admin implements AdminMXBean {
@@ -202,8 +312,11 @@ public class DirectoryWatcher implements AutoCloseable {
             return String.valueOf(last);
         }
     }
+
     public interface AdminMXBean {
+
         int getWatchedTargetFiles();
+
         int getWatchedTargetDirectories();
 
         int getCount();
--- vpro-shared-util/src/test/java/nl/vpro/util/DirectoryWatcherTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/DirectoryWatcherTest.java
@@ -1,5 +1,6 @@
 package nl.vpro.util;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
@@ -7,96 +8,156 @@ import java.nio.file.*;
 import java.nio.file.attribute.FileTime;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
-import org.junit.jupiter.api.Test;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.junit.jupiter.api.*;
 
+import static nl.vpro.logging.Log4j2Helper.debugOrInfo;
 import static org.assertj.core.api.Assertions.assertThat;
 @Log4j2
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DirectoryWatcherTest {
 
     private final Path dir = Files.createTempDirectory(DirectoryWatcherTest.class.getSimpleName());
     private final Path subDir = Files.createDirectory(dir.resolve("subdir"));
 
-    final Queue<Path> events = new ArrayDeque<>();
+    final Path f1 = Files.createFile(dir.resolve("test-1.xml"));
+    final Path tarf5 = Files.createFile(subDir.resolve("test-5-target.xml"));
+    final Path symf5 = Files.createSymbolicLink(dir.resolve("test-5.xml"), Path.of("subdir/test-5-target.xml"));// relative!
+    final Path tarf3 = Files.createFile(subDir.resolve("test-3-target.xml"));
+    final Path tarf3_2 = Files.createFile(subDir.resolve("test-3-target-2.xml"));
+    Path f2;
+    Path symf3;
+    Path symf6;
+
+    final Queue<DirectoryWatcher.@NonNull WatcherEvent> events = new ArrayDeque<>();
+
+    DirectoryWatcher watcher;
 
     DirectoryWatcherTest() throws IOException {
     }
 
-
-    @Test
-    public void test() throws IOException {
-        Path f1 = Files.createFile(dir.resolve("test-1.xml"));
-        Path tarf5 = Files.createFile(subDir.resolve("test-5-target.xml"));
-        Path symf5 = Files.createSymbolicLink(dir.resolve("test-5.xml"), Path.of("subdir/test-5-target.xml"));// relative!
-        Path tarf3 = Files.createFile(subDir.resolve("test-3-target.xml"));
-        Path tarf3_2 = Files.createFile(subDir.resolve("test-3-target-2.xml"));
-
-        try (DirectoryWatcher watcher = new DirectoryWatcher(
-            dir,
+    @BeforeAll
+    public void setup() throws IOException {
+        watcher = DirectoryWatcher.builder()
+            .directory(dir)
+            .eventConsumer(
             path -> {
                 events.add(path);
                 synchronized (dir) {
                     dir.notifyAll();
                 }
-            },
-            path -> path.getFileName().toString().endsWith(".xml")
-
-        )) {
-
-            assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf5);
-            //assertThat(watcher.getWatchedTargetDirectories()).containsExactlyInAnyOrder(subDir);
-
-            Path f2 = Files.createFile(dir.resolve("test-2.xml"));
-            Path symf3 = Files.createSymbolicLink(dir.resolve("test-3.xml"), tarf3);
-
-            wait(f2, symf3);
-
-            Thread.sleep(100);
-            assertThat(events).isEmpty();
-
-            Files.setLastModifiedTime(f1, FileTime.from(Instant.now()));
-
-            wait(f1);
-
-            Files.setLastModifiedTime(tarf3, FileTime.from(Instant.now()));
-
-            wait(symf3);
+            })
+            .filter(
+                path -> path.getFileName().toString().endsWith(".xml")
+            ).build();
 
 
-            Path symf6 = Files.createSymbolicLink(dir.resolve("test-6.xml"), tarf5);
+        assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf5);
+        assertThat(watcher.getWatchedTargetDirectories()).containsExactlyInAnyOrder(subDir);
+    }
 
-            wait(symf6);
-
-            assertThat(events).isEmpty();
-
-            assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf3, tarf5);
-
-            Files.delete(symf3);
-
-            wait(symf3);
-
-            symf3 = Files.createSymbolicLink(dir.resolve("test-3.xml"), tarf3_2);
-
-            wait(symf3);
-
-            assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf3_2, tarf5);
+    @Test
+    @Order(2)
+    public void waitForNewFileAnNewSymlink() throws IOException, InterruptedException {
 
 
-            for (int i = 0; i < 20; i++) {
-                log.info("Checking for subsequent events {}", i);
-                Thread.sleep(Duration.ofSeconds(1).toMillis());
-                assertThat(events).isEmpty();
-            }
+        f2 = Files.createFile(dir.resolve("test-2.xml"));
+        symf3 = Files.createSymbolicLink(dir.resolve("test-3.xml"), tarf3);
 
-        } catch (InterruptedException e) {
-            log.warn(e.getMessage(), e);
-        }
+        wait(f2, symf3);
+
+        Thread.sleep(100);
+        assertThat(events).isEmpty();
+    }
+
+    @Test
+    @Order(3)
+    public void touchFile() throws IOException, InterruptedException {
+        tickAndTouch(f1);
+
+        wait(f1);
+    }
+    @Test
+    @Order(3)
+    public void touchTargetOfSymlink() throws IOException, InterruptedException {
+
+        tickAndTouch(tarf3);
+
+        wait(symf3);
+    }
+
+    @Test
+    @Order(4)
+    public void createSymlink() throws IOException, InterruptedException {
+        symf6 = Files.createSymbolicLink(dir.resolve("test-6.xml"), tarf5);
+
+        wait(symf6);
+
+        assertThat(events).isEmpty();
+        assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf3, tarf5);
+    }
+
+    @Test
+    @Order(5)
+    public void deleteSymlink() throws IOException, InterruptedException {
+        Files.delete(symf3);
+
+        wait(symf3);
+
+        assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf5);
+
+        // symf3 has been deleted.
+        assertThat(watcher.getWatchedLastModifieds().keySet()).containsExactlyInAnyOrder(f1, f2, tarf5, symf6, symf5);
+
 
     }
 
     @Test
+    @Order(5)
+    public void newDestinationOfSymlink() throws IOException, InterruptedException {
+        tick();
+
+        Files.createSymbolicLink(symf3, tarf3_2);
+
+        wait(symf3);
+
+        assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf3_2, tarf5);
+    }
+
+
+    @Test
+    @Order(6)
+    public void newDestinationOfSymlink2() throws IOException, InterruptedException {
+        tick();
+
+        Files.delete(symf3);
+        Files.createSymbolicLink(symf3, tarf3);
+
+        wait(symf3);
+
+        assertThat(watcher.getWatchedTargetFiles().keySet()).containsExactlyInAnyOrder(tarf3, tarf5);
+
+
+    }
+
+    @Test
+    @Order(20)
+    public void noSubsequentEvents() throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+            log.info("Checking for subsequent events {}", i);
+            Thread.sleep(Duration.ofSeconds(1).toMillis());
+            assertThat(events).isEmpty();
+        }
+
+    }
+
+
+    @Test
+    @Disabled
     public void tryout() throws IOException {
         Path dir = Files.createTempDirectory(DirectoryWatcherTest.class.getSimpleName());
 
@@ -110,13 +171,23 @@ class DirectoryWatcherTest {
     private void wait(Path... paths) throws InterruptedException {
         int size = paths.length;
         final long start = System.currentTimeMillis();
+        log.info("Waiting for {}", Arrays.asList(paths));
         synchronized (dir) {
+            int prevSize = 0;
+            Instant prevLog = Instant.EPOCH;
             while (events.size() < size && (System.currentTimeMillis() - start) < Duration.ofSeconds(1000).toMillis()) {
+                boolean info =  prevSize != events.size() ||
+                    Duration.between(prevLog, Instant.now()).compareTo(Duration.ofSeconds(5)) > 0;
+                debugOrInfo(log, info, "Waiting for " + size + " events, got " + events.size() + " " + events);
+                prevSize = events.size();
+                if (info) {
+                    prevLog = Instant.now();
+                }
                 dir.wait(100);
             }
         }
         if (events.size() != size) {
-            throw new AssertionError("Expected " + size + " events, but got " + events.size() + "( " + Duration.ofMillis(System.currentTimeMillis() - start) + ")");
+            throw new AssertionError("Expected " + size + " events, but got " + events.size() + "( " + Duration.ofMillis(System.currentTimeMillis() - start) + "):" + events);
         }
 
         for (Path p : paths) {
