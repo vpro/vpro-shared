875c745c4609e8684e136e08766b40fc75a5ba1f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-26 11:53:58 +0200

    Details.

---
+++ vpro-shared-configuration/src/main/java/nl/vpro/configuration/util/PostfixingMetadataNamingStrategy.java
@@ -0,0 +1,29 @@
+package nl.vpro.configuration.util;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.springframework.jmx.export.naming.MetadataNamingStrategy;
+import org.springframework.jmx.support.ObjectNameManager;
+import org.springframework.lang.Nullable;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since ...
+ */
+public class PostfixingMetadataNamingStrategy extends MetadataNamingStrategy {
+
+    private final String postfix;
+
+    public PostfixingMetadataNamingStrategy(String prefix) {
+        this.postfix = prefix;
+    }
+
+    @NonNull
+    @Override
+	public ObjectName getObjectName(Object managedBean, @Nullable String beanKey) throws MalformedObjectNameException {
+        ObjectName name = super.getObjectName(managedBean, beanKey);
+        return ObjectNameManager.getInstance(name.getDomain() + "." + postfix, name.getKeyPropertyList());
+    }
+}
--- vpro-shared-configuration/src/main/java/nl/vpro/configuration/util/PrefixingMetadataNamingStrategy.java
+++ vpro-shared-configuration/src/main/java/nl/vpro/configuration/util/PrefixingMetadataNamingStrategy.java
@@ -1,29 +1,8 @@
 package nl.vpro.configuration.util;
 
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.springframework.jmx.export.naming.MetadataNamingStrategy;
-import org.springframework.jmx.support.ObjectNameManager;
-import org.springframework.lang.Nullable;
-
-/**
- * @author Michiel Meeuwissen
- * @since ...
- */
-public class PrefixingMetadataNamingStrategy extends MetadataNamingStrategy {
-
-    private final String postfix;
-
+@Deprecated
+public class PrefixingMetadataNamingStrategy extends PostfixingMetadataNamingStrategy {
     public PrefixingMetadataNamingStrategy(String prefix) {
-        this.postfix = prefix;
-    }
-
-    @NonNull
-    @Override
-	public ObjectName getObjectName(Object managedBean, @Nullable String beanKey) throws MalformedObjectNameException {
-        ObjectName name = super.getObjectName(managedBean, beanKey);
-        return ObjectNameManager.getInstance(name.getDomain() + "." + postfix, name.getKeyPropertyList());
+        super(prefix);
     }
 }
