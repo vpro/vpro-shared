03ad8e4f10356882d8b83bfcd6cea7d32c233cf0
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-06-26 10:48:45 +0200

    More javadoc.

--- vpro-shared-configuration/src/main/java/nl/vpro/configuration/spring/converters/EnableCustomConversions.java
+++ vpro-shared-configuration/src/main/java/nl/vpro/configuration/spring/converters/EnableCustomConversions.java
@@ -10,13 +10,23 @@ import org.springframework.context.annotation.Import;
  * <p>
  * E.g. when this is effective, this will work:
  * <pre>
- *     {@literal @}Value("${nep.itemizer.ratelimiter.unit:PT1H}") Duration unit,
+ *     {@literal @}Value("${nep.itemizer.ratelimiter.unit:1H}") Duration unit, // defaults to 1 hour
  * </pre>
- * This triggers {@link StringToDurationConverter}.
+ * In this example {@link StringToDurationConverter} is triggered.
+ * <p>
+ * Typically, you would put this annotation on a class with is also annotated with {@link org.springframework.context.annotation.Configuration}. E.g. like this:
+ * <pre>
+ * {@literal @}Configuration
+ * {@literal @}EnableCustomConversions
+ * {@literal @}Import({OpenApiConfig.class, AOPConfig.class, JPAConfig.class, SecurityConfiguration.class})
+ * {@literal @}ImportResource(locations = "classpath*:META-INF/vpro/*-context.xml")
+ *  public class AppConfig  {
+ * ..
+ * </pre>
  *
  * @since 5.0
  * @see CustomConversion
- * @see nl.vpro.configuration.spring.converters all converters in this pacakge are available then
+ * @see nl.vpro.configuration.spring.converters all converters in this package are available then
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
