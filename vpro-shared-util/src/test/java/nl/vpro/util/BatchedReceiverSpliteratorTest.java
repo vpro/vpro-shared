af26c68160e1950c75a48182bec864a1d134b0d8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-09-26 21:49:56 +0200

    details.

--- vpro-shared-util/src/test/java/nl/vpro/util/BatchedReceiverSpliteratorTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/BatchedReceiverSpliteratorTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -16,69 +17,68 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Disabled("work in progress")
 public class BatchedReceiverSpliteratorTest {
 
+    @Test
+   public void test() {
+      final List<String> result = new ArrayList<>();
+      for (int i = 0; i < 23; i++) {
+         result.add("a" + i);
+      }
+      BatchedReceiverSpliterator<String> i =
+          BatchedReceiverSpliterator.<String>builder()._build().<String>builder()
+              .batchGetter((offset, max) ->
+                  result.subList(
+                        Math.min(offset.intValue(), result.size()),
+                      Math.min(offset.intValue() + max, result.size())).iterator())
+            .batchSize(6)
+              .build();
 
-	@Test
-	public void test() {
-		final List<String> result = new ArrayList<>();
-		for (int i = 0; i < 23; i++) {
-			result.add("a" + i);
-		}
-		BatchedReceiverSpliterator<String> i =
-				BatchedReceiverSpliterator.<String>builder()._build().<String>builder()
-				.batchGetter((offset, max) ->
-						result.subList(
-								Math.min(offset.intValue(), result.size()),
-								Math.min(offset.intValue() + max, result.size())).iterator())
-				.batchSize(6)
-				.build();
+        assertThat(i).asList().containsExactly(result.toArray(new String[0]));
 
-		assertThat(i).asList().containsExactly(result.toArray(new String[0]));
-
-	}
+   }
 
 
-	@Test
-	public void testWithOffset() {
-		final List<String> result = new ArrayList<>();
-		for (int i = 0; i < 23; i++) {
-			result.add("a" + i);
-		}
-		BatchedReceiverSpliterator<String> i =
-			BatchedReceiverSpliterator.<String>builder()
-				.batchGetter((offset, max) ->
-					result.subList(
-						Math.min(offset.intValue(), result.size()),
-						Math.min(offset.intValue() + max, result.size())).iterator())
-				.batchSize(6)
+    @Test
+    public void testWithOffset() {
+        final List<String> result = new ArrayList<>();
+        for (int i = 0; i < 23; i++) {
+            result.add("a" + i);
+        }
+        BatchedReceiverSpliterator<String> i =
+            BatchedReceiverSpliterator.<String>builder()
+                .batchGetter((offset, max) ->
+                    result.subList(
+                        Math.min(offset.intValue(), result.size()),
+                        Math.min(offset.intValue() + max, result.size())).iterator())
+                .batchSize(6)
                 .offset(10L)
-				.build();
+                .build();
 
-		assertThat(i)
-			.asList().containsExactly(result.subList(10, result.size()).toArray(new String[result.size() -10]));
+        assertThat(i)
+            .asInstanceOf(InstanceOfAssertFactories.LIST)
+            .containsExactly((Object[]) result.subList(10, result.size()).toArray(new String[result.size() -10]));
 
-	}
+    }
 
 
 
-	@Test
-	public void testWithoutBatch() {
-		final List<String> result = new ArrayList<>();
-		for (int i = 0; i < 23; i++) {
-			result.add("a" + i);
-		}
-		AtomicInteger offset = new AtomicInteger(0);
-		BatchedReceiverSpliterator<String> i =
-			BatchedReceiverSpliterator.<String>builder()
-				.batchGetter(() -> {
-					int of = offset.getAndAdd(5);
-					return result.subList(
-						Math.min(of, result.size()),
-						Math.min(of + 5, result.size())).iterator();
-				})
-				.build();
+    @Test
+    public void testWithoutBatch() {
+        final List<String> result = new ArrayList<>();
+        for (int i = 0; i < 23; i++) {
+            result.add("a" + i);
+        }
+        AtomicInteger offset = new AtomicInteger(0);
+        BatchedReceiverSpliterator<String> i =
+            BatchedReceiverSpliterator.<String>builder()
+                .batchGetter(() -> {
+                    int of = offset.getAndAdd(5);
+                    return result.subList(
+                        Math.min(of, result.size()),
+                        Math.min(of + 5, result.size())).iterator();
+                })
+                .build();
 
-		//assertThat(i).extracting().containsExactly(result.toArray(new String[0]));
+        //assertThat(i).extracting().containsExactly(result.toArray(new String[0]));
 
-	}
-
+    }
 }
