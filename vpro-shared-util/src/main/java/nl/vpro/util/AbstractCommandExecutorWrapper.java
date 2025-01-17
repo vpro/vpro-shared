a86dfe6fb821d6c992bd593a366a2ddcd7c4ac1d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-01-17 18:07:55 +0100

    Some support for access log parsing, and then reporting that to prometheus.

---
+++ vpro-shared-util/src/main/java/nl/vpro/util/AbstractCommandExecutorWrapper.java
@@ -0,0 +1,41 @@
+package nl.vpro.util;
+
+import java.util.function.Supplier;
+
+import nl.vpro.logging.simple.SimpleLogger;
+
+/**
+ * Implementation of {@link CommandExecutor} wrapping another one. The idea is that this can be
+ * extended to add useful extra methods related to the wrapped command, while the command itself
+ * can be instantiated with {@link CommandExecutorImpl#builder()}
+ * @since 5.4
+ */
+public abstract class AbstractCommandExecutorWrapper implements CommandExecutor {
+
+
+    public AbstractCommandExecutorWrapper() {
+
+    }
+
+    protected abstract CommandExecutor getWrapped();
+
+    @Override
+    public int execute(String... args) {
+        return getWrapped().execute(args);
+    }
+
+    @Override
+    public int execute(Parameters parameters) {
+        return getWrapped().execute(parameters);
+    }
+
+    @Override
+    public SimpleLogger getLogger() {
+        return getWrapped().getLogger();
+    }
+
+    @Override
+    public Supplier<String> getBinary() {
+        return getWrapped().getBinary();
+    }
+}
--- vpro-shared-util/src/main/java/nl/vpro/util/CommandExecutorWrapper.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/CommandExecutorWrapper.java
@@ -1,8 +1,6 @@
 package nl.vpro.util;
 
-import java.util.function.Supplier;
-
-import nl.vpro.logging.simple.SimpleLogger;
+import lombok.Getter;
 
 /**
  * Implementation of {@link CommandExecutor} wrapping another one. The idea is that this can be
@@ -10,31 +8,12 @@ import nl.vpro.logging.simple.SimpleLogger;
  * can be instantiated with {@link CommandExecutorImpl#builder()}
  * @since 5.4
  */
-public class CommandExecutorWrapper implements CommandExecutor {
+@Getter
+public class CommandExecutorWrapper extends AbstractCommandExecutorWrapper {
 
     final CommandExecutor wrapped;
 
     public CommandExecutorWrapper(CommandExecutor wrapped) {
         this.wrapped = wrapped;
-    }
-
-    @Override
-    public int execute(String... args) {
-        return wrapped.execute(args);
-    }
-
-    @Override
-    public int execute(Parameters parameters) {
-        return wrapped.execute(parameters);
-    }
-
-    @Override
-    public SimpleLogger getLogger() {
-        return wrapped.getLogger();
-    }
-
-    @Override
-    public Supplier<String> getBinary() {
-        return wrapped.getBinary();
     }
 }
