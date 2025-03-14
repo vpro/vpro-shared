ac533d4a16d969e73245bce604dec21de551779e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-03-14 10:43:59 +0100

    Added a small utility builder method.

--- vpro-shared-util/src/main/java/nl/vpro/util/BatchedReceiver.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/BatchedReceiver.java
@@ -5,15 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 /**
  * Given some API which supplies only 'batched' retrieval (so with offset and max/batchsize parameters),
  * access such an API as an iterator to visit all elements.
- *
+ * <p>
  * If an API provides access to huge set of elements, they often do it with some paging mechanism, or by some 'resumption token' formalism. With {@link BatchedReceiver} this can be morphed into a simple {@link java.util.Iterator}.
-
+ *
  *
  * <h3>Paging</h3>
  * The 'batchGetter' argument should be a {@link java.util.function.BiFunction}, returning an iterator for the page described by given offset and batch size
@@ -59,121 +58,144 @@ import java.util.function.Supplier;
 @Slf4j
 public class BatchedReceiver<T> implements Iterator<T> {
 
-	final Integer batchSize;
+    final Integer batchSize;
 
-	final BiFunction<Long, Integer, Iterator<T>> batchGetter;
+    final BiFunction<Long, Integer, Iterator<T>> batchGetter;
 
-	long subCount = 0;
-	long offset;
-	Iterator<T> subIterator;
-	Boolean hasNext;
-	T next;
-
-
-	@lombok.Builder(builderClassName = "Builder", buildMethodName = "_build")
-	public BatchedReceiver(
-			Integer batchSize,
-			Long offset,
-			BiFunction<Long, Integer, Iterator<T>> _batchGetter) {
-		this.batchSize = batchSize;
-		this.batchGetter = _batchGetter;
-		this.offset = offset == null ? 0L : offset;
-	}
-
-	private enum BatchType {
-		BIFUNCTION,
-		SUPPLIER
-	}
+    long subCount = 0;
+    long offset;
+    Iterator<T> subIterator;
+    Boolean hasNext;
+    T next;
 
 
-	public static class Builder<T>  {
+    @lombok.Builder(builderClassName = "Builder", buildMethodName = "_build")
+    public BatchedReceiver(
+        Integer batchSize,
+        Long offset,
+        BiFunction<Long, Integer, Iterator<T>> _batchGetter) {
+        this.batchSize = batchSize;
+        this.batchGetter = _batchGetter;
+        this.offset = offset == null ? 0L : offset;
+    }
 
-		private BatchType batchType = null;
+    private enum BatchType {
+        BIFUNCTION,
+        SUPPLIER
+    }
+
+
+    public static class Builder<T> {
+
+        private BatchType batchType = null;
+
+
+        public <X> Builder<T> initialAndResumption(
+            Supplier<X> initial,
+            UnaryOperator<X> resumption,
+            Function<X, Iterator<T>> getter) {
+            return batchGetter(new Supplier<Iterator<T>>() {
+                X x = null;
+
+                @Override
+                public Iterator<T> get() {
+                    if (x == null) {
+                        x = initial.get();
+                    } else {
+                        x = resumption.apply(x);
+                        if (x == null) {
+                            return null;
+                        }
+                    }
+                    return  getter.apply(x);
+                }
+            });
+        }
+
 
         /**
-         *
          * @param batchGetter A function to get the next batch, the parameters are the current necessary offset, and batch size
          */
-	    public Builder<T> batchGetter(BiFunction<Long, Integer, Iterator<T>> batchGetter) {
-	    	batchType = BatchType.BIFUNCTION;
-	        return _batchGetter(batchGetter);
+        public Builder<T> batchGetter(BiFunction<Long, Integer, Iterator<T>> batchGetter) {
+            batchType = BatchType.BIFUNCTION;
+            return _batchGetter(batchGetter);
         }
 
         /**
          * @param batchGetter For 'resumption token' like functionality, the offset and max argument can be irrelevant.
          */
         public Builder<T> batchGetter(final Supplier<Iterator<T>> batchGetter) {
-        	batchType = BatchType.SUPPLIER;
+            batchType = BatchType.SUPPLIER;
             return _batchGetter((offset, max) -> batchGetter.get());
         }
 
         public BatchedReceiver<T> build() {
-        	if (_batchGetter == null) {
-        		throw new IllegalStateException("No batch getter defined");
-			}
-        	if (batchType == BatchType.BIFUNCTION && batchSize == null) {
-				log.debug("Specified a bifunction, and nobatch size. The batch size is implicetely set to 100");
-        		batchSize(100);
-			}
-        	if (batchType == BatchType.SUPPLIER && batchSize != null) {
-        		log.warn("Specified a supplier, and a batch size. The batch size is ignored", new Exception());
-        		batchSize(null);
-			}
-        	return _build();
-		}
+            if (_batchGetter == null) {
+                throw new IllegalStateException("No batch getter defined");
+            }
+            if (batchType == BatchType.BIFUNCTION && batchSize == null) {
+                log.debug("Specified a bifunction, and nobatch size. The batch size is implicetely set to 100");
+                batchSize(100);
+            }
+            if (batchType == BatchType.SUPPLIER && batchSize != null) {
+                log.warn("Specified a supplier, and a batch size. The batch size is ignored", new Exception());
+                batchSize(null);
+            }
+            return _build();
+        }
 
     }
 
 
-	@Override
-	public boolean hasNext () {
-		findNext();
-		return hasNext;
-	}
+    @Override
+    public boolean hasNext() {
+        findNext();
+        return hasNext;
+    }
 
-	@Override
-	public T next () {
-		findNext();
-		if (!hasNext) {
-			throw new NoSuchElementException();
-		}
-		hasNext = null;
-		return next;
-	}
+    @Override
+    public T next() {
+        findNext();
+        if (!hasNext) {
+            throw new NoSuchElementException();
+        }
+        hasNext = null;
+        return next;
+    }
 
-	protected void findNext() {
-		if (hasNext == null) {
-			if (subIterator == null) {
-				subIterator = batchGetter.apply(offset, batchSize);
+    protected void findNext() {
+        if (hasNext == null) {
+            if (subIterator == null) {
+                subIterator = batchGetter.apply(offset, batchSize);
                 subCount = 0;
                 if (subIterator == null) {
                     hasNext = false;
                     return;
                 }
-			}
-			if (subIterator.hasNext()) {
-				next = subIterator.next();
+            }
+            if (subIterator.hasNext()) {
+                next = subIterator.next();
                 subCount++;
-				hasNext = true;
-			} else {
-				offset += subCount;
-				if (batchSize == null || subCount == batchSize) {
+                hasNext = true;
+            } else {
+                offset += subCount;
+                if (batchSize == null || subCount == batchSize) {
                     subIterator = batchGetter.apply(offset, batchSize);
                 } else {
-				    subIterator = null;
+                    subIterator = null;
                 }
                 subCount = 0;
                 if (subIterator == null) {
                     hasNext = false;
                     return;
                 }
-				hasNext = subIterator.hasNext();
-				if (hasNext) {
-					next = subIterator.next();
+                hasNext = subIterator.hasNext();
+                if (hasNext) {
+                    next = subIterator.next();
                     subCount++;
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
 }
--- vpro-shared-util/src/test/java/nl/vpro/util/BatchedReceiverTest.java
+++ vpro-shared-util/src/test/java/nl/vpro/util/BatchedReceiverTest.java
@@ -1,7 +1,9 @@
 package nl.vpro.util;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
@@ -12,72 +14,115 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Michiel Meeuwissen
  * @since 1.68
  */
+@Log4j2
 public class BatchedReceiverTest {
 
 
-	@Test
-	public void test() {
-		final List<String> result = new ArrayList<>();
-		for (int i = 0; i < 23; i++) {
-			result.add("a" + i);
-		}
-		BatchedReceiver<String> i =
-				BatchedReceiver.<String>builder()
-				.batchGetter((offset, max) ->
-						result.subList(
-								Math.min(offset.intValue(), result.size()),
-								Math.min(offset.intValue() + max, result.size())).iterator())
-				.batchSize(6)
-				.build();
+    @Test
+    public void test() {
+        final List<String> result = new ArrayList<>();
+        for (int i = 0; i < 23; i++) {
+            result.add("a" + i);
+        }
+        BatchedReceiver<String> i =
+            BatchedReceiver.<String>builder()
+                .batchGetter((offset, max) ->
+                    result.subList(
+                        Math.min(offset.intValue(), result.size()),
+                        Math.min(offset.intValue() + max, result.size())).iterator())
+                .batchSize(6)
+                .build();
 
-		assertThat(i).toIterable().containsExactly(result.toArray(new String[0]));
+        assertThat(i).toIterable().containsExactly(result.toArray(new String[0]));
 
-	}
+    }
 
 
-	@Test
-	public void testWithOffset() {
-		final List<String> result = new ArrayList<>();
-		for (int i = 0; i < 23; i++) {
-			result.add("a" + i);
-		}
-		BatchedReceiver<String> i =
-			BatchedReceiver.<String>builder()
-				.batchGetter((offset, max) ->
-					result.subList(
-						Math.min(offset.intValue(), result.size()),
-						Math.min(offset.intValue() + max, result.size())).iterator())
-				.batchSize(6)
+    @Test
+    public void testWithOffset() {
+        final List<String> result = new ArrayList<>();
+        for (int i = 0; i < 23; i++) {
+            result.add("a" + i);
+        }
+        BatchedReceiver<String> i =
+            BatchedReceiver.<String>builder()
+                .batchGetter((offset, max) ->
+                    result.subList(
+                        Math.min(offset.intValue(), result.size()),
+                        Math.min(offset.intValue() + max, result.size())).iterator())
+                .batchSize(6)
                 .offset(10L)
-				.build();
+                .build();
 
-		assertThat(i)
-			.toIterable().containsExactly(result.subList(10, result.size()).toArray(new String[result.size() -10]));
+        assertThat(i)
+            .toIterable().containsExactly(result.subList(10, result.size()).toArray(new String[result.size() - 10]));
 
-	}
+    }
+
+
+    @Test
+    public void testWithoutBatch() {
+        final List<String> result = new ArrayList<>();
+        for (int i = 0; i < 23; i++) {
+            result.add("a" + i);
+        }
+        AtomicInteger offset = new AtomicInteger(0);
+        BatchedReceiver<String> i =
+            BatchedReceiver.<String>builder()
+                .batchGetter(() -> {
+                    int of = offset.getAndAdd(5);
+                    return result.subList(
+                        Math.min(of, result.size()),
+                        Math.min(of + 5, result.size())).iterator();
+                })
+                .build();
+
+        assertThat(i)
+            .toIterable().containsExactly(result.toArray(new String[0]));
+
+    }
+
+    @Getter
+    static class WithToken {
+        final List<String> result;
+        final Integer token;
+
+        WithToken(List<String> result, Integer token) {
+            this.result = result;
+            this.token = token;
+        }
+
+        static WithToken forToken(Integer token) {
+            if (token == null) {
+                return new WithToken(List.of("0", "a"), 1);
+            } else {
+                if (token < 10) {
+                    return new WithToken(List.of("a" + token , "b" + token), token + 1);
+                } else {
+                    return null;
+                }
+
+            }
+        }
+    }
 
 
 
-	@Test
-	public void testWithoutBatch() {
-		final List<String> result = new ArrayList<>();
-		for (int i = 0; i < 23; i++) {
-			result.add("a" + i);
-		}
-		AtomicInteger offset = new AtomicInteger(0);
-		BatchedReceiver<String> i =
-			BatchedReceiver.<String>builder()
-				.batchGetter(() -> {
-					int of = offset.getAndAdd(5);
-					return result.subList(
-						Math.min(of, result.size()),
-						Math.min(of + 5, result.size())).iterator();
-				})
-				.build();
+     @Test
+    public void testWithTokens() {
+        AtomicInteger offset = new AtomicInteger(0);
+        BatchedReceiver<String> i =
+            BatchedReceiver.<String>builder()
+                .<WithToken>initialAndResumption(
+                    () -> WithToken.forToken(null),
+                    (withToken) -> WithToken.forToken(withToken.token),
+                    (withToken) -> withToken.result.iterator())
+                .build();
 
-		assertThat(i)
-			.toIterable().containsExactly(result.toArray(new String[0]));
+        assertThat(i).toIterable().containsExactly("0", "a", "a1", "b1", "a2", "b2", "a3", "b3", "a4", "b4", "a5", "b5", "a6", "b6", "a7", "b7", "a8", "b8", "a9", "b9");
 
-	}
+
+
+    }
 
 }
