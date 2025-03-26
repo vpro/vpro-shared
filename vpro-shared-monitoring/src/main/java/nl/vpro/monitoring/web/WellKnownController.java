b3739d5765f312aa2c34817d28d5cc40e85c5c9f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-03-26 16:38:01 +0100

    support '${REQUEST}' in security.txt

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/web/WellKnownController.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/web/WellKnownController.java
@@ -5,7 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
+import jakarta.inject.Inject;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.apache.commons.text.StringSubstitutor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatusCode;
@@ -26,6 +31,8 @@ import org.springframework.web.server.ResponseStatusException;
 public class WellKnownController {
 
 
+    @Inject
+    HttpServletRequest request;
     //if you need to test in macos, use /etc/synthetic.conf (/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util -t)
 
     Path dir = Path.of("/well-known");
@@ -39,12 +46,58 @@ public class WellKnownController {
             throw new BadRequestException();
         }
         if (Files.isReadable(file)) {
-            return Files.readString(file);
+            var placeHolders = Map.of(
+                "REQUEST", getOriginalRequestURL(request)
+            );
+            StringSubstitutor substitutor = new StringSubstitutor(placeHolders);
+            return substitutor.replace(Files.readString(file));
         }
 
         throw new ResponseStatusException(
             HttpStatusCode.valueOf(404),
             "No %s found".formatted(fileName));
 
+    }
+
+
+    private static  String getOriginalRequestURL(HttpServletRequest request) {
+
+        // Get scheme - check forwarded headers first
+        String scheme = request.getHeader("X-Forwarded-Proto");
+        if (scheme == null) {
+        scheme = request.getScheme();
+        }
+
+        // Get host
+        String host = request.getHeader("X-Forwarded-Host");
+        if (host == null) {
+            host = request.getServerName();
+        }
+
+        // Get port
+        int port = request.getServerPort();
+        String portHeader = request.getHeader("X-Forwarded-Port");
+        if (portHeader != null) {
+            try {
+                port = Integer.parseInt(portHeader);
+            } catch (NumberFormatException e) {
+                // Use default port if header is invalid
+            }
+        }
+
+        // Build URL with port only if non-standard
+        StringBuilder url = new StringBuilder();
+        url.append(scheme).append("://").append(host);
+        if (!((scheme.equals("http") && port == 80) || (scheme.equals("https") && port == 443))) {
+            url.append(":").append(port);
+        }
+
+        // Add path and query string
+        url.append(request.getRequestURI());
+        if (request.getQueryString() != null) {
+            url.append("?").append(request.getQueryString());
+    }
+
+        return url.toString();
     }
 }
