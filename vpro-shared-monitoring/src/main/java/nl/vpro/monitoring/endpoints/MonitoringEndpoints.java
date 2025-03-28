60cfcfeaa3571ab51f4a2103d67083fa2f74f2af
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-03-28 18:55:27 +0100

    Look out, otherwise spring security it required.

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringEndpoints.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringEndpoints.java
@@ -8,20 +8,17 @@ import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import nl.vpro.jackson2.DateModule;
 import nl.vpro.jackson2.Jackson2Mapper;
-import nl.vpro.monitoring.config.*;
+import nl.vpro.monitoring.config.MonitoringProperties;
 import nl.vpro.monitoring.web.HealthController;
 import nl.vpro.monitoring.web.PrometheusController;
 
 @Configuration
-@EnableWebSecurity
-@EnableMonitoringWebSecurity
 public class MonitoringEndpoints {
 
 
