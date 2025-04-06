5acb8e6c5fa21e18d55db7b7c2f50082cba98514
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-04-06 12:17:28 +0200

    Improved EnumAdapter a bit, recognizing @XmlEnumValue.

--- vpro-shared-xml/src/main/java/nl/vpro/xml/bind/EnumAdapter.java
+++ vpro-shared-xml/src/main/java/nl/vpro/xml/bind/EnumAdapter.java
@@ -1,7 +1,15 @@
 package nl.vpro.xml.bind;
 
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
 import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.annotation.XmlEnumValue;
 import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * This is a nice idea. The sad thing however, is that when using this, the generated XSD will not anymore be an enum.
@@ -11,14 +19,60 @@ import jakarta.xml.bind.annotation.adapters.XmlAdapter;
  */
 public abstract class EnumAdapter<T extends Enum<T>> extends XmlAdapter<String,  T> {
 
-    private final Class<T> enumClass;
+    private final Map<String, T> map;
+    private final boolean ignoreCase;
 
     protected EnumAdapter(Class<T> enumClass) {
-        this.enumClass = enumClass;
+        this.map = map(enumClass, true);
+        this.ignoreCase = true;
     }
 
-    protected T valueOf(String v) {
-         return Enum.valueOf(enumClass, v.trim());
+    @SneakyThrows
+    protected static <T extends Enum<T>> Map<String, T> map(Class<T> enumClass, boolean ignoreCase) {
+        Map<String, T> result = new HashMap<>();
+        for (Field f : enumClass.getDeclaredFields()) {
+            if (f.isEnumConstant()) {
+                XmlEnumValue value = f.getAnnotation(XmlEnumValue.class);
+                T constant = (T) f.get(null);
+                String key;
+                if (value != null) {
+                    key = ignoreCase ? value.value().toUpperCase() : value.value();
+                } else {
+                    key = ignoreCase ? f.getName().toUpperCase() : f.getName();
+                }
+                result.put(key, constant);
+            }
+        }
+        for (Field f : enumClass.getDeclaredFields()) {
+            if (f.isEnumConstant()) {
+                XmlEnumValue value = f.getAnnotation(XmlEnumValue.class);
+                T constant = (T) f.get(null);
+                String key;
+                if (value != null) {
+                    key = ignoreCase ? f.getName().toUpperCase() : f.getName();
+                    if (!result.containsKey(key)) {
+                        result.put(key, constant);
+                    }
+                }
+            }
+        }
+        return Collections.unmodifiableMap(result);
+    }
+
+    protected T valueOf(@NonNull String v) {
+        if (v == null) {
+            throw new NullPointerException("v must not be null");
+        }
+        if (ignoreCase) {
+            v = v.toUpperCase();
+        }
+        v = v.trim();
+        T result = map.get(v);
+        if (result == null) {
+            throw new IllegalArgumentException("No enum constant " + v);
+
+        }
+        return result;
     }
 
     @Override
