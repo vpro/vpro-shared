3b588d3b640106ad55d72cf4a183ea56b746e33f
Michiel Meeuwissen <michiel meeuwissen>
2025-05-22 10:16:17 +0200

    javadoc.

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringWebSecurityConfiguration.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/endpoints/MonitoringWebSecurityConfiguration.java
@@ -1,7 +1,14 @@
 package nl.vpro.monitoring.endpoints;
 
+import nl.vpro.monitoring.web.Authentication;
+
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * @deprecated Not defining any beans anymore. Authentication is now arranged without spring security, just from the calls in {@link nl.vpro.monitoring.web.HealthController} and {@link nl.vpro.monitoring.web.PrometheusController} themselves.
+ * @see Authentication
+ */
 @Configuration
 @Deprecated
 public class MonitoringWebSecurityConfiguration {
