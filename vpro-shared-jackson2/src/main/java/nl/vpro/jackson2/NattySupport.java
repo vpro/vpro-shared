c3cde1b491f8d211d645751043ef84d2568983f7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-16 08:50:46 +0200

    I guess this is never used, but added some logging to make sure.

--- vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/NattySupport.java
+++ vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/NattySupport.java
@@ -1,5 +1,7 @@
 package nl.vpro.jackson2;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.Instant;
 import java.util.*;
 
@@ -15,6 +17,7 @@ import nl.vpro.util.DateUtils;
  * @since 2.14
  */
 
+@Slf4j
 class NattySupport {
     private static final Parser PARSER = new Parser(TimeZone.getTimeZone(BindingUtils.DEFAULT_ZONE));
 
@@ -22,6 +25,7 @@ class NattySupport {
     static Optional<Instant> parseDate(String value) {
         List<DateGroup> groups = PARSER.parse(value);
         if (groups.size() == 1) {
+            log.info("Parsed date '{}' to {}", value, groups.get(0).getDates());
             return Optional.ofNullable(DateUtils.toInstant(groups.get(0).getDates().get(0)));
         }
         return Optional.empty();
