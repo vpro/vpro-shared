858823f86182971f46743f6bfb8a14254a50515b
Michiel Meeuwissen <michiel meeuwissen>
2025-10-09 09:23:38 +0200

    unused implort

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/config/MonitoringWebSecurityConfiguration.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/config/MonitoringWebSecurityConfiguration.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
 @EnableWebSecurity
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
