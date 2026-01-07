f4f6b43a6d2bb9224f708be57368f82fa4bfdd32
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-07 20:30:35 +0100

    Small new feature, that makes log4j2-test superflous.

---
+++ vpro-shared-logging/src/main/java/nl/vpro/logging/log4j2/CaptureListFromLogger.java
@@ -0,0 +1,33 @@
+package nl.vpro.logging.log4j2;
+
+import lombok.Getter;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+import org.apache.logging.log4j.core.LogEvent;
+
+/**
+ *
+ * @author Michiel Meeuwissen
+ * @since 5.14
+ */
+public class CaptureListFromLogger extends AbstractCaptureLogger implements Supplier<List<LogEvent>> {
+
+    @Getter
+    private final List<LogEvent> events = new ArrayList<>();
+
+    CaptureListFromLogger(UUID uuid, boolean currentThreadOnly) {
+        super(uuid, currentThreadOnly);
+    }
+
+    @Override
+    public List<LogEvent> get() {
+        return getEvents();
+    }
+
+    @Override
+    protected void accept(LogEvent event) {
+        events.add(event.toImmutable());
+    }
+}
