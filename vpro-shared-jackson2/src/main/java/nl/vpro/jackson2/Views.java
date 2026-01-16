4cc0a0186d9da522b6cd003a68ccf484e3b2fa19
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-16 13:36:36 +0100

    Better backwards compatigblity.

--- vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/Views.java
+++ vpro-shared-jackson2/src/main/java/nl/vpro/jackson2/Views.java
@@ -2,4 +2,16 @@ package nl.vpro.jackson2;
 
 @Deprecated(forRemoval = true, since = "use nl.vpro.jackson.Views instead")
 public class Views extends nl.vpro.jackson.Views {
+
+    public interface Normal extends nl.vpro.jackson.Views.Normal {
+    }
+
+    public interface Forward extends nl.vpro.jackson.Views.Forward {
+    }
+
+    public interface Publisher extends nl.vpro.jackson.Views.Publisher {
+    }
+
+    public interface ForwardPublisher extends nl.vpro.jackson.Views.ForwardPublisher, Publisher, Forward {
+    }
 }
