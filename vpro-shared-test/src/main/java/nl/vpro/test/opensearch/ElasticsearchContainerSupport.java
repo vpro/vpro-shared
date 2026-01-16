77f72b8cede49b3de74c1989965124a71790ad4e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-16 11:05:50 +0100

    Increased timeout a bit, (with the reason)

--- vpro-shared-test/src/main/java/nl/vpro/test/opensearch/ElasticsearchContainerSupport.java
+++ vpro-shared-test/src/main/java/nl/vpro/test/opensearch/ElasticsearchContainerSupport.java
@@ -2,6 +2,8 @@ package nl.vpro.test.opensearch;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,10 +31,15 @@ public class ElasticsearchContainerSupport {
     public static final String OPENSEARCH_IMAGE = "ghcr.io/npo-poms/opensearch:opendistro";
 
 
-
+    /**
+     * Using a longer then default startup timeout, because there is no arm image, so on m1/m2 macs it has to emulate, and the start up may be quite clow.
+     * @return A new ElasticsearchContainer with extended startup timeout, and started.
+     */
     @Bean("elasticsearchContainer")
     public  ElasticsearchContainer getOpensearchContainer() {
-        ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer(true);
+        ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer(false)
+            .withStartupTimeout(Duration.ofMinutes(5));
+        elasticsearchContainer.start();
         return elasticsearchContainer;
     }
 
