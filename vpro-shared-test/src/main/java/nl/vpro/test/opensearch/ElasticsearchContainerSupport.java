6484df9972e19c156d34883f86857543fa1f9b48
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-16 14:20:40 +0100

    Added note about rosetta.

--- vpro-shared-test/src/main/java/nl/vpro/test/opensearch/ElasticsearchContainerSupport.java
+++ vpro-shared-test/src/main/java/nl/vpro/test/opensearch/ElasticsearchContainerSupport.java
@@ -23,6 +23,21 @@ import org.springframework.context.annotation.Configuration;
  *}
  * }
  * </pre>
+ *
+ * See also https://osxhub.com/running-x86-docker-images-apple-silicon-m1-m2-m3-m4/
+ *<p>
+ * Leveraging Rosetta 2 for Better Performanc
+ * Apple’s Rosetta 2 technology provides dynamic binary translation, allowing x86 applications to run on Apple Silicon with significantly better performance than traditional emulation.
+ * <p>
+ * Enabling Rosetta in Docker Desktop
+ * To enable Rosetta for Docker on your Mac:
+ * <p>
+ * Open Docker Desktop
+ * Navigate to Settings (gear icon)
+ * Go to the “General” tab
+ * Enable “Use Virtualization framework”
+ * Check “Use Rosetta for x86/amd64 emulation on Apple Silicon”
+ * Restart Docker Desktop
  */
 @Configuration
 @Slf4j
