6382f6cf060bd3983a240d2e1759bb384635ad77
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-25 15:37:17 +0100

    Corrected MXBean name

--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/binder/ScriptMeterBinder.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/binder/ScriptMeterBinder.java
@@ -20,7 +20,7 @@ import nl.vpro.jmx.MBeans;
 import nl.vpro.util.*;
 
 @Slf4j
-public class ScriptMeterBinder implements MeterBinder, Runnable, ScriptMeterMXBean {
+public class ScriptMeterBinder implements MeterBinder, Runnable, ScriptMeterBinderMXBean {
 
 
     private final Map<String, AtomicDouble> cache = new ConcurrentHashMap<>();
--- vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/binder/ScriptMeterBinderMXBean.java
+++ vpro-shared-monitoring/src/main/java/nl/vpro/monitoring/binder/ScriptMeterBinderMXBean.java
@@ -2,7 +2,7 @@ package nl.vpro.monitoring.binder;
 
 import nl.vpro.jmx.Description;
 
-public interface ScriptMeterMXBean {
+public interface ScriptMeterBinderMXBean {
 
     @Description("run")
     void run();
