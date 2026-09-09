8f78a767e8fe2dc771709690766d5201582b03e2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-09-09 16:47:03 +0200

    javadoc.

--- vpro-shared-util/src/main/java/nl/vpro/util/Strings.java
+++ vpro-shared-util/src/main/java/nl/vpro/util/Strings.java
@@ -12,6 +12,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 /**
  * @author Michiel Meeuwissen
  * @since 1.64
+ * @see TextUtil
  */
 @Slf4j
 public class Strings {
@@ -19,6 +20,14 @@ public class Strings {
     private Strings() {
     }
 
+    /**
+     * Creates a stream from comma- or newline-separated values, expanding readable file names into their lines.
+     *
+     * <p>The returned stream must be closed when it contains values read from a file.</p>
+     *
+     * @param strings values or readable file names to expand
+     * @return a stream containing the supplied values or the lines of the referenced files
+     */
     public static Stream<String> strings(String... strings) {
         return Arrays
             .stream(strings)
@@ -29,6 +38,12 @@ public class Strings {
             ;
     }
 
+    /**
+     * Returns the lines of a readable file, or the supplied value when it is not a readable file.
+     *
+     * @param s a possible file name
+     * @return a stream of file lines, or a single-element stream containing {@code s}
+     */
     private static Stream<String> fromFile(String s) {
         File file = new File(s);
         if (!file.canRead()) {
